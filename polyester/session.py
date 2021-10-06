44a87a686f4d621620498dee7b324e1def2bf4a9
Erik Bernhardsson <mail@erikbern.com>
2021-10-06 16:03:55 -0400

    delete the old deprecated session, let's gooooooo

--- polyester/session.py
+++ polyester/session.py
@@ -1,7 +1,7 @@
 import asyncio
 import contextlib
 
-from .async_utils import infinite_loop, retry, synchronizer
+from .async_utils import infinite_loop, retry, synchronizer, asynccontextmanager, TaskContext
 from .client import Client
 from .config import logger
 from .ctx_mgr_utils import CtxMgr
@@ -10,52 +10,6 @@ from .image import base_image
 from .object import Object
 from .proto import api_pb2
 from .utils import print_logs
-
-
-@synchronizer
-class DeprecatedSession(CtxMgr):
-    def __init__(self, client, stdout=None, stderr=None):
-        self.client = client
-        self.objects_by_tag = {}
-        # log queues
-        self._stdout = stdout
-        self._stderr = stderr
-
-    @classmethod
-    async def _create(cls):
-        client = await Client.current()
-        return DeprecatedSession(client)
-
-    async def _get_logs(self, draining=False, timeout=BLOCKING_REQUEST_TIMEOUT):
-        request = api_pb2.SessionGetLogsRequest(session_id=self.session_id, timeout=timeout, draining=draining)
-        async for log_entry in self.client.stub.SessionGetLogs(request, timeout=timeout + GRPC_REQUEST_TIME_BUFFER):
-            if log_entry.done:
-                logger.info("No more logs")
-                break
-            else:
-                print_logs(log_entry.data, log_entry.fd, self._stdout, self._stderr)
-
-    async def _start(self):
-        req = api_pb2.SessionCreateRequest(client_id=self.client.client_id)
-        resp = await self.client.stub.SessionCreate(req)
-        self.session_id = resp.session_id
-
-        # See comment about heartbeats task, same thing applies here
-        self._logs_task = infinite_loop(self._get_logs)
-
-    async def _stop(self, hard):
-        # Stop session (this causes the server to kill any running task)
-        logger.debug("Stopping the session server-side")
-        req = api_pb2.SessionStopRequest(session_id=self.session_id)
-        await self.client.stub.SessionStop(req)
-
-        # Kill the existing log loop
-        self._logs_task.cancel()
-
-        # Fetch any straggling logs
-        if not hard:
-            logger.debug("Draining logs")
-            await self._get_logs(draining=True, timeout=10.0)
 
 
 @synchronizer
@@ -84,7 +38,17 @@ class Session(Object):
         self._functions.append(fun)
         return fun
 
-    async def _start(self, client=None):
+    async def _get_logs(self, draining=False, timeout=BLOCKING_REQUEST_TIMEOUT):
+        request = api_pb2.SessionGetLogsRequest(session_id=self.session_id, timeout=timeout, draining=draining)
+        async for log_entry in self.client.stub.SessionGetLogs(request, timeout=timeout + GRPC_REQUEST_TIME_BUFFER):
+            if log_entry.done:
+                logger.info("No more logs")
+                break
+            else:
+                print_logs(log_entry.data, log_entry.fd, self._stdout, self._stderr)
+
+    @asynccontextmanager
+    async def run(self, client=None):
         if client is None:
             client = await Client.current()
 
@@ -109,12 +73,16 @@ class Session(Object):
         for tag, obj in objects.items():
             await self.create_or_get(obj, tag)
 
-    async def _stop(self):
-        pass
+        # Start tracking logs and yield context
+        async with TaskContext() as tc:
+            tc.create_task(infinite_loop(self._get_logs))
+            yield
 
-    @contextlib.asynccontextmanager
-    async def run(self, client=None):
-        # TODO: support sync, and return_copy
-        await self._start(client)
-        yield
-        await self._stop()
+        # Stop session (this causes the server to kill any running task)
+        logger.debug("Stopping the session server-side")
+        req = api_pb2.SessionStopRequest(session_id=self.session_id)
+        await self.client.stub.SessionStop(req)
+
+        # Fetch any straggling logs
+        logger.debug("Draining logs")
+        await self._get_logs(draining=True, timeout=10.0)
