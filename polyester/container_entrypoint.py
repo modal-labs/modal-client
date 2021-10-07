cdb653c6903c22d06eab8833ea60431fb2115bc0
Akshat Bubna <akshatb42@gmail.com>
2021-10-07 16:47:17 -0700

    generator support

--- polyester/container_entrypoint.py
+++ polyester/container_entrypoint.py
@@ -1,9 +1,12 @@
 import asyncio
 import inspect
 import sys
+import threading
 import traceback
 import typing
 import uuid
+
+from aiostream import stream
 
 from .async_utils import synchronizer
 from .buffer_utils import buffered_read_all, buffered_write_all
@@ -45,19 +48,47 @@ class FunctionContext:
         await buffered_write_all(self.client.stub.FunctionOutput, requests)
 
 
+def make_output_request(input_id, output_buffer_id, **kwargs):
+    result = api_pb2.GenericResult(**kwargs)
+    item = pack_output_buffer_item(result)
+    buffer_req = api_pb2.BufferWriteRequest(item=item, buffer_id=output_buffer_id)
+
+    return api_pb2.FunctionOutputRequest(input_id=input_id, buffer_req=buffer_req)
+
+
 async def call_function(
     function: typing.Callable,
-    args: any,
-    kwargs: any,
+    buffer_item: api_pb2.BufferItem,
     serializer: typing.Callable[[typing.Any], bytes],
-) -> api_pb2.GenericResult:
+    deserializer: typing.Callable[[bytes], typing.Any],
+) -> (str, api_pb2.GenericResult):
+    input_id = buffer_item.item_id
+
+    input = unpack_input_buffer_item(buffer_item)
+    args = deserializer(input.args)
+    kwargs = deserializer(input.kwargs)
+    output_buffer_id = input.output_buffer_id
+
     try:
         res = function(*args, **kwargs)
-        # TODO: handle generators etc
+
         if inspect.iscoroutine(res):
             res = await res
 
-        return api_pb2.GenericResult(
+        if inspect.isgenerator(res):
+            for value in res:
+                yield make_output_request(
+                    input_id,
+                    output_buffer_id,
+                    status=api_pb2.GenericResult.Status.SUCCESS,
+                    data=serializer(value),
+                    incomplete=True,
+                )
+            return
+
+        yield make_output_request(
+            input_id,
+            output_buffer_id,
             status=api_pb2.GenericResult.Status.SUCCESS,
             data=serializer(res),
         )
@@ -65,7 +96,9 @@ async def call_function(
     except Exception as exc:
         # Note that we have to stringify the exception/traceback since
         # it isn't always possible to unpickle on the client side
-        return api_pb2.GenericResult(
+        yield make_output_request(
+            input_id,
+            output_buffer_id,
             status=api_pb2.GenericResult.Status.FAILURE,
             exception=repr(exc),
             traceback=traceback.format_exc(),
@@ -81,32 +114,26 @@ def main(task_id, function_id, input_buffer_id, module_name, function_name, clie
     function_context = FunctionContext(client, task_id, function_id, input_buffer_id, module_name, function_name)
     function = function_context.get_function()
 
-    async def generate_output_requests():
-        async for buffer_item in function_context.get_inputs():
-            if buffer_item.EOF:
-                break
+    async def generate_outputs():
+        try:
+            # Create a stream for each call_function; needed so we can multiplex generator outputs.
+            generators = []
 
-            input_id = buffer_item.item_id
+            async for buffer_item in function_context.get_inputs():
+                gen = call_function(function, buffer_item, client.serialize, client.deserialize)
+                generators.append(gen)
 
-            input = unpack_input_buffer_item(buffer_item)
-            args = client.deserialize(input.args)
-            kwargs = client.deserialize(input.kwargs)
-            output_buffer_id = input.output_buffer_id
+            # TODO: draining the input iterator should also happen async.
+            interleaved = stream.merge(*generators)
 
-            # function
-            output = await call_function(
-                function,
-                args,
-                kwargs,
-                client.serialize,
-            )
+            async with interleaved.stream() as streamer:
+                async for output in streamer:
+                    yield output
 
-            item = pack_output_buffer_item(output)
-            buffer_req = api_pb2.BufferWriteRequest(item=item, buffer_id=output_buffer_id)
-            request = api_pb2.FunctionOutputRequest(input_id=input_id, buffer_req=buffer_req)
-            yield request
+        except Exception as exc:
+            logger.exception(f"Failed generating outputs: {repr(exc)}")
 
-    function_context.stream_outputs(generate_output_requests())
+    function_context.stream_outputs(generate_outputs())
 
 
 if __name__ == "__main__":
