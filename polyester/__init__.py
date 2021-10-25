0ed88079eb3f8ce728c5b732c3b01edb13c81f55
Erik Bernhardsson <mail@erikbern.com>
2021-10-25 17:26:48 -0400

    Layer -> Image

--- polyester/__init__.py
+++ polyester/__init__.py
@@ -1,6 +1,6 @@
 from .client import Client
 from .function import RemoteException
 from .dict import Dict
-from .image import EnvDict, Layer, base_image
+from .image import EnvDict, Image, base_image
 from .queue import Queue
 from .session import Session
--- polyester/image.py
+++ polyester/image.py
@@ -17,11 +17,11 @@ def _make_bytes(s):
     return s.encode("ascii") if type(s) is str else s
 
 
-class Layer(Object):
+class Image(Object):
     def __init__(
         self,
         tag=None,
-        base_layers={},
+        base_images={},
         dockerfile_commands=[],
         context_files={},
         must_create=False,
@@ -31,8 +31,8 @@ class Layer(Object):
 
         # Construct the local id
         local_id_args = []
-        for docker_tag, layer in base_layers.items():
-            local_id_args.append("b:%s:(%s)" % (docker_tag, layer.args.local_id))
+        for docker_tag, image in base_images.items():
+            local_id_args.append("b:%s:(%s)" % (docker_tag, image.args.local_id))
         local_id_args.append("c:%s" % get_sha256_hex_from_content(b"\n".join(dockerfile_commands)))
         for filename, content in context_files.items():
             local_id_args.append("f:%s:%s" % (filename, get_sha256_hex_from_content(content)))
@@ -41,7 +41,7 @@ class Layer(Object):
             args=dict(
                 local_id=",".join(local_id_args),
                 tag=tag,
-                base_layers=base_layers,
+                base_images=base_images,
                 dockerfile_commands=dockerfile_commands,
                 context_files=context_files,
                 must_create=must_create,
@@ -51,49 +51,49 @@ class Layer(Object):
 
     async def create_or_get(self):
         if self.args.tag:
-            req = api_pb2.LayerGetByTagRequest(tag=self.args.tag)
-            resp = await self.client.stub.LayerGetByTag(req)
-            layer_id = resp.layer_id
+            req = api_pb2.ImageGetByTagRequest(tag=self.args.tag)
+            resp = await self.client.stub.ImageGetByTag(req)
+            image_id = resp.image_id
 
         else:
-            # Recursively build base layers
-            base_layer_objs = await asyncio.gather(
-                *(self.session.create_or_get_object(layer) for layer in self.args.base_layers.values())
+            # Recursively build base images
+            base_image_objs = await asyncio.gather(
+                *(self.session.create_or_get_object(image) for image in self.args.base_images.values())
             )
-            base_layers_pb2s = [
-                api_pb2.BaseLayer(docker_tag=docker_tag, layer_id=layer.object_id)
-                for docker_tag, layer in zip(self.args.base_layers.keys(), base_layer_objs)
+            base_images_pb2s = [
+                api_pb2.BaseImage(docker_tag=docker_tag, image_id=image.object_id)
+                for docker_tag, image in zip(self.args.base_images.keys(), base_image_objs)
             ]
 
             context_file_pb2s = [
-                api_pb2.LayerContextFile(filename=filename, data=data)
+                api_pb2.ImageContextFile(filename=filename, data=data)
                 for filename, data in self.args.context_files.items()
             ]
 
-            layer_definition = api_pb2.Layer(
-                base_layers=base_layers_pb2s,
+            image_definition = api_pb2.Image(
+                base_images=base_images_pb2s,
                 dockerfile_commands=self.args.dockerfile_commands,
                 context_files=context_file_pb2s,
                 local_id=self.args.local_id,
                 local=self.args.local,
             )
 
-            req = api_pb2.LayerGetOrCreateRequest(
+            req = api_pb2.ImageGetOrCreateRequest(
                 session_id=self.session.session_id,
-                layer=layer_definition,
+                image=image_definition,
                 must_create=self.args.must_create,
             )
-            resp = await self.client.stub.LayerGetOrCreate(req)
-            layer_id = resp.layer_id
+            resp = await self.client.stub.ImageGetOrCreate(req)
+            image_id = resp.image_id
 
-        logger.debug("Waiting for layer %s" % layer_id)
+        logger.debug("Waiting for image %s" % image_id)
         while True:
-            request = api_pb2.LayerJoinRequest(
-                layer_id=layer_id,
+            request = api_pb2.ImageJoinRequest(
+                image_id=image_id,
                 timeout=BLOCKING_REQUEST_TIMEOUT,
                 session_id=self.session.session_id,
             )
-            response = await retry(self.client.stub.LayerJoin)(request, timeout=GRPC_REQUEST_TIMEOUT)
+            response = await retry(self.client.stub.ImageJoin)(request, timeout=GRPC_REQUEST_TIMEOUT)
             if not response.result.status:
                 continue
             elif response.result.status == api_pb2.GenericResult.Status.FAILURE:
@@ -103,12 +103,12 @@ class Layer(Object):
             else:
                 raise RemoteException("Unknown status %s!" % response.result.status)
 
-        return layer_id
+        return image_id
 
     @requires_create
     async def set_tag(self, tag):
-        req = api_pb2.LayerSetTagRequest(layer_id=self.object_id, tag=tag)
-        await self.client.stub.LayerSetTag(req)
+        req = api_pb2.ImageSetTagRequest(image_id=self.object_id, tag=tag)
+        await self.client.stub.ImageSetTag(req)
 
     def is_inside(self):
         # This is used from inside of containers to know whether this container is active or not
@@ -133,7 +133,7 @@ def get_python_version():
     return config["image_python_version"] or "%d.%d.%d" % sys.version_info[:3]
 
 
-class DebianSlim(Layer):
+class DebianSlim(Image):
     def __init__(self, python_version=None):
         if python_version is None:
             python_version = get_python_version()
@@ -144,10 +144,10 @@ class DebianSlim(Layer):
     def add_python_packages(self, python_packages, find_links=None):
         find_links_arg = f"-f {find_links}" if find_links else ""
 
-        layer = Layer(
-            base_layers={
+        image = Image(
+            base_images={
                 "base": self,
-                "builder": Layer(tag="python-%s-slim-buster-builder" % self.python_version),
+                "builder": Image(tag="python-%s-slim-buster-builder" % self.python_version),
             },
             dockerfile_commands=[
                 "FROM builder as builder-vehicle",
@@ -158,14 +158,14 @@ class DebianSlim(Layer):
                 "RUN rm -rf /tmp/wheels",
             ],
         )
-        return layer
+        return image
 
     def run_commands(self, commands):
-        layer = Layer(
-            base_layers={"base": self},
+        image = Image(
+            base_images={"base": self},
             dockerfile_commands=["FROM base"] + ["RUN " + command for command in commands],
         )
-        return layer
+        return image
 
 
 debian_slim = DebianSlim()
