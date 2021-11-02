c3f58fb539becd5ddfab67696629f58b0ddc3863
Erik Bernhardsson <mail@erikbern.com>
2021-11-02 17:49:24 -0400

    Fix __init__.py thing

--- polyester/__init__.py
+++ polyester/__init__.py
@@ -1,6 +1,6 @@
 from .client import Client
-from .function import RemoteException
 from .dict import Dict
+from .exception import RemoteError
 from .image import EnvDict, Image, base_image
 from .queue import Queue
 from .session import Session
