c5cd498d120bc95e5107323c192d3f250db39d9a
Erik Bernhardsson <mail@erikbern.com>
2021-11-08 11:27:11 -0500

    Break out EnvDict into its own module

--- polyester/__init__.py
+++ polyester/__init__.py
@@ -1,6 +1,7 @@
 from .client import Client
 from .dict import Dict
+from .env_dict import EnvDict
 from .exception import RemoteError
-from .image import EnvDict, Image, base_image
+from .image import Image, base_image
 from .queue import Queue
 from .session import Session
---
+++ polyester/env_dict.py
@@ -0,0 +1,13 @@
+from .object import Object
+from .proto import api_pb2
+
+
+class EnvDict(Object):
+    def __init__(self, env_dict):
+        super().__init__(tag=None)
+        self.env_dict = env_dict
+
+    async def _create_impl(self, session):
+        req = api_pb2.EnvDictCreateRequest(session_id=session.session_id, env_dict=self.env_dict)
+        resp = await session.client.stub.EnvDictCreate(req)
+        return resp.env_dict_id
