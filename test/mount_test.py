b84da2fb1a4c26dce490d57851f6b7f25f03720d
Erik Bernhardsson <mail@erikbern.com>
2021-12-01 14:57:54 -0500

    Fix test

--- test/mount_test.py
+++ test/mount_test.py
@@ -8,17 +8,9 @@ from polyester.mount import get_files
 
 @pytest.mark.asyncio
 async def test_get_files():
-    q = asyncio.Queue()
-    n_sentinels = 10
-    await get_files(os.path.dirname(__file__), lambda fn: fn.endswith(".py"), True, q, n_sentinels)
     files = {}
-    while not q.empty():
-        z = await q.get()
-        if z is None:
-            n_sentinels -= 1
-        else:
-            filename, rel_filename, sha256_hex = z
+    async for tup in get_files(os.path.dirname(__file__), lambda fn: fn.endswith(".py"), True):
+        filename, rel_filename, sha256_hex = tup
         files[filename] = sha256_hex
 
-    assert n_sentinels == 0
     assert __file__ in files
