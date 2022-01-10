d9498cc805d26abc38461d75afae541151f1a3ee
Erik Bernhardsson <mail@erikbern.com>
2022-01-10 14:24:01 -0500

    comment explaining the setup.py hack

--- setup.py
+++ setup.py
@@ -1,6 +1,15 @@
 from setuptools import setup
 
-# Parse version data
+# PEP 396 prescribes setting a __version__ on the package,
+# but there's no obvious way for setup.py to access it.
+# Since __init__.py imports various third-party packages,
+# we can't access __version__ at this point.
+# Instead, we're using the using the approach suggested in
+# https://stackoverflow.com/questions/458550/standard-way-to-embed-version-into-python-package
+# and set the version info by executing a Python module.
+# TODO: might want to look at pbr:
+# https://docs.openstack.org/pbr/latest/user/index.html
+
 about = {}
 with open("modal/version.py") as f:
     exec(f.read(), about)
