d899c66ca2d0d52562f54609fe16496c555980f8
Erik Bernhardsson <mail@erikbern.com>
2022-01-15 08:18:36 -0500

    add failing test reproducing the integration test

---
+++ test/serialization_test.py
@@ -0,0 +1,27 @@
+import pytest
+
+from modal import Queue, Session
+
+session = Session()
+
+
+@Queue.factory(session=session)
+async def qf():
+    return await Queue.create()
+
+
+@pytest.mark.asyncio
+async def test_persistent_object(servicer, client):
+    async with session.run(client=client):
+        # Serialize "dynamic" object and deserialize
+        q = await Queue.create(session=session)
+        data = session.serialize(q)
+        q_roundtrip = session.deserialize(data)
+        assert isinstance(q_roundtrip, Queue)
+        assert q.object_id == q_roundtrip.object_id
+
+        # Serialize factory object and deserialize
+        data = session.serialize(qf)
+        qf_roundtrip = session.deserialize(data)
+        assert isinstance(qf_roundtrip, Queue)
+        assert qf.object_id == qf_roundtrip.object_id
