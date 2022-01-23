7c13ebd325f0d3bf35381ac8210a6f418d808d8d
Elias Freider <elias@freider.se>
2022-01-23 15:17:08 +0100

    Utilities for file descriptor level stdout/stderr capture
    Async context manager that starts a thread which redirects
    an outstream into a pipe and then reads line by line from
    the pipe and forwards it into a callback.

---
+++ modal/_output_capture.py
@@ -0,0 +1,42 @@
+import asyncio
+import io
+import os
+from asyncio import TimeoutError
+from typing import Callable
+
+from modal.config import logger
+
+from ._async_utils import synchronizer
+
+
+@synchronizer.asynccontextmanager
+async def thread_capture(stream: io.IOBase, callback: Callable[[str, io.TextIOBase], None]):
+    """Intercept writes on a stream (typically stderr or stdout)"""
+    fd = stream.fileno()
+    dup_fd = os.dup(fd)
+    orig_writer = os.fdopen(dup_fd, "w")
+    read_fd, write_fd = os.pipe()
+    os.dup2(write_fd, fd)
+    read_file = os.fdopen(read_fd, "r")
+
+    def capture_thread():
+        while 1:
+            line = read_file.readline()
+            if not line:
+                return
+            callback(line, orig_writer)
+
+    # start thread but don't await it
+    print_task = asyncio.get_event_loop().run_in_executor(None, capture_thread)
+    try:
+        yield orig_writer
+    finally:
+        stream.flush()  # flush any remaining writes on fake output
+        os.close(write_fd)  # this should trigger eof in the capture thread
+        os.dup2(dup_fd, fd)  # restore stdout
+        try:
+            await asyncio.wait_for(print_task, 3)  # wait for thread to empty the read buffer
+        except TimeoutError:
+            # TODO: this doesn't actually kill the thread, but since the pipe is closed it shouldn't
+            #       capture more user output and eventually end when eof is reached on the read pipe
+            logger.warn("Could not empty user output buffer. Some user output might be missing at this time")
---
+++ test/output_capture_test.py
@@ -0,0 +1,111 @@
+import subprocess
+import sys
+import time
+from contextlib import contextmanager
+
+import pytest
+
+from modal._async_utils import synchronizer
+from modal._output_capture import thread_capture
+
+
+@pytest.fixture(scope="class")
+def suspend_capture(pytestconfig):
+    # Pytest itself does global capture on a file descriptor level which
+    # for some reason interacts with our capture, so we need to suspend
+    # pytest capture when running the subprocess test below
+
+    # Code from:
+    # https://github.com/pytest-dev/pytest/issues/1599
+
+    @contextmanager
+    def suspend_block():
+        capman = pytestconfig.pluginmanager.getplugin("capturemanager")
+        capman.suspend_global_capture(in_=True)
+        try:
+            yield
+        finally:
+            capman.suspend_global_capture(in_=True)
+
+    return suspend_block()
+
+
+@pytest.fixture
+def capture_stdout_as_list(suspend_capture):
+    @synchronizer.asynccontextmanager
+    async def ctx():
+        caps = []
+
+        def callback(line, _):
+            caps.append(line)
+
+        with suspend_capture:
+            async with thread_capture(sys.stdout, callback):
+                yield caps
+
+    return ctx
+
+
+@pytest.mark.asyncio
+async def test_capture_prints(capture_stdout_as_list):
+    async with capture_stdout_as_list() as caps:
+        # in case line buffering is turned off for sys.stdout in the test env
+        print("foo", flush=True)
+        time.sleep(0.1)  # TODO: can we remove this
+        # test that we actually capture continually and not just at end of block
+        assert caps == ["foo\n"]
+        print("bar")
+
+    # intentionally no wait here - all results should be captured when capture block ends
+    assert caps == ["foo\n", "bar\n"]
+
+
+@pytest.mark.asyncio
+async def test_capture_partial_line(capture_stdout_as_list):
+    async with capture_stdout_as_list() as caps:
+        # in case line buffering is turned off for sys.stdout in the test env
+        sys.stdout.write("no newline")
+
+    # intentionally no wait here - all results should be captured when capture block ends
+    assert caps == ["no newline"]
+
+
+@pytest.mark.asyncio
+async def test_capture_empty_line(capture_stdout_as_list):
+    async with capture_stdout_as_list() as caps:
+        # in case line buffering is turned off for sys.stdout in the test env
+        sys.stdout.write("\n")
+
+    # intentionally no wait here - all results should be captured when capture block ends
+    assert caps == ["\n"]
+
+
+@pytest.mark.asyncio
+async def test_error_bubbles_through_and_shuts_down_thread(suspend_capture, capsys):
+    caps = []
+
+    def callback(line, original_stream):
+        caps.append(line)
+
+    class DummyException(Exception):
+        pass
+
+    with capsys.disabled():
+        with suspend_capture:
+            with pytest.raises(DummyException):
+                async with thread_capture(sys.stdout, callback):
+                    # in case line buffering is turned off for sys.stdout in the test env
+                    print("foo")
+                    raise DummyException()
+
+    print("bar")
+    out, err = capsys.readouterr()
+    assert out == "bar\n"
+
+
+@pytest.mark.asyncio
+async def test_capture_subprocess(capture_stdout_as_list):
+    async with capture_stdout_as_list() as caps:
+        subprocess.call(["echo", "foo"])
+
+    assert caps == ["foo\n"]
