a90eac26dded017af873189f08a082f85f511667
Richard Gong <richardgong0@gmail.com>
2022-01-27 14:34:54 -0500

    server rejects invalid ScheduleCreate requests (with tests)

--- modal/schedule.py
+++ modal/schedule.py
@@ -6,11 +6,11 @@ class Schedule(Object, type_prefix="sc"):
     """A schedule of specific times at which registered functions are triggered"""
 
     @classmethod
-    async def create(cls, period="", cron_expression="", session=None):
+    async def create(cls, period="", cron_string="", session=None):
         session = cls._get_session(session)
 
-        seconds = None
         if period:
+            seconds = None
             try:
                 if period[-1] == "d":
                     seconds = int(period[:-1]) * 24 * 3600
@@ -20,21 +20,31 @@ class Schedule(Object, type_prefix="sc"):
                     seconds = int(period[:-1]) * 60
                 elif period[-1] == "s":
                     seconds = int(period[:-1])
+                else:
+                    raise
             except:
-                raise Exception(f"Failed to parse period during construction of Schedule: {period}")
-
-        if cron_expression:
-            raise Exception(f"Cron expression construction of Schedule not suppported yet.")
+                raise Exception(f"Failed to parse period while creating Schedule: {period}")
+            req = api_pb2.ScheduleCreateRequest(
+                session_id=session.session_id,
+                period=seconds,
+                args=session.serialize([]),
+                kwargs=session.serialize({}),
+            )
+        elif cron_string:
+            req = api_pb2.ScheduleCreateRequest(
+                session_id=session.session_id,
+                cron_string=cron_string,
+                args=session.serialize([]),
+                kwargs=session.serialize({}),
+            )
 
         # TODO: remove args/kwargs placeholders, which represent arguments to scheduled function
         # This would involve requesting an output buffer here similar to _Invocation
-        req = api_pb2.ScheduleCreateRequest(
-            session_id=session.session_id,
-            period=seconds,
-            args=session.serialize([]),
-            kwargs=session.serialize({}),
-        )
-        resp = await session.client.stub.ScheduleCreate(req)
-        schedule_id = resp.schedule_id
 
+        resp = await session.client.stub.ScheduleCreate(req)
+
+        if resp.error_message:
+            raise Exception(f"Failed to create Schedule: {resp.error_message}")
+
+        schedule_id = resp.schedule_id
         return cls._create_object_instance(schedule_id, session)
