3559663386efb1d457b33510fd2167ea6797c2e0
Erik Bernhardsson <mail@erikbern.com>
2022-02-16 17:21:43 -0500

    Revert "Remove skipped test_create_factory, cc @erikbern"
    This reverts commit e22b0b9369dcf9d34497c445a164d6baa721f1c5.

---
+++ test/server_test.py
@@ -0,0 +1,19 @@
+from modal._server_connection import GRPCConnectionFactory
+
+
+async def test_create_factory():
+    cf = GRPCConnectionFactory("http://localhost:1234")
+    assert cf.target == "localhost:1234"
+    assert cf.credentials is None
+
+    cf = GRPCConnectionFactory("http://localhost:1234", credentials=("foo", "bar"))
+    assert cf.target == "localhost:1234"
+    assert cf.credentials is not None
+
+    cf = GRPCConnectionFactory("http://host.docker.internal:1234", credentials=("foo", "bar"))
+    assert cf.target == "host.docker.internal:1234"
+    assert cf.credentials is None  # This is a special case where we have to remove credentails
+
+    cf = GRPCConnectionFactory("https://api.modal.com", credentials=("foo", "bar"))
+    assert cf.target == "api.modal.com"
+    assert cf.credentials is not None
