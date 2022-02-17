e19aaa3b4d4503d740d6d41a2c5a06a0cda71f01
Erik Bernhardsson <mail@erikbern.com>
2022-02-16 23:16:41 -0500

    Fix notebooks

--- modal/extensions/ipython.py
+++ modal/extensions/ipython.py
@@ -2,38 +2,38 @@ import atexit
 import logging
 import sys
 
-from modal import Session
+from modal import App
+from modal._app_singleton import set_default_app
 from modal._async_utils import run_coro_blocking
-from modal._session_singleton import set_default_session
 from modal.config import config, logger
 
 
 def load_ipython_extension(ipython):
-    global session_ctx
+    global app_ctx
 
     # Set logger for notebook sys.stdout
     logger.addHandler(logging.StreamHandler(stream=sys.stdout))
     logger.setLevel(config["loglevel"])
 
-    # Create a session and provide it in the IPython session
-    session = Session(blocking_late_creation_ok=True)
-    ipython.push({"session": session})
-    set_default_session(session)
+    # Create a app and provide it in the IPython app
+    app = App(blocking_late_creation_ok=True)
+    ipython.push({"app": app})
+    set_default_app(app)
 
-    session_ctx = session.run()
+    app_ctx = app.run()
 
     # Notebooks have an event loop present, but we want this function
     # to be blocking. This is fairly hacky.
-    run_coro_blocking(session_ctx.__aenter__())
+    run_coro_blocking(app_ctx.__aenter__())
 
-    def exit_session(self):
-        print("Exiting modal session")
-        run_coro_blocking(session_ctx.__aexit__(None, None, None))
+    def exit_app(self):
+        print("Exiting modal app")
+        run_coro_blocking(app_ctx.__aexit__(None, None, None))
 
-    atexit.register(exit_session)
+    atexit.register(exit_app)
 
 
 def unload_ipython_extension(ipython):
-    global session_ctx
+    global app_ctx
 
-    run_coro_blocking(session_ctx.__aexit__(None, None, None))
+    run_coro_blocking(app_ctx.__aexit__(None, None, None))
