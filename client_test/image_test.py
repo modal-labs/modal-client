4948c07dc06be62298e1c5694c1189c21c3c1af6
Akshat Bubna <akshatb42@gmail.com>
2022-02-25 18:23:58 +0530

    distinguish test module names

--- client_test/image_test.py
+++ client_test/image_test.py
@@ -19,12 +19,12 @@ def my_image(pkg="python-numpy"):
 def test_image_factory():
     assert isinstance(my_image, Image)
     assert isinstance(my_image.tag, str)
-    assert my_image.tag == "test.image_test.my_image"
+    assert my_image.tag == "client_test.image_test.my_image"
 
     my_image_2 = my_image(pkg="python-scipy")
     assert isinstance(my_image_2, Image)
     assert isinstance(my_image_2.tag, str)
-    assert my_image_2.tag == 'test.image_test.my_image("python-scipy")'
+    assert my_image_2.tag == 'client_test.image_test.my_image("python-scipy")'
 
 
 def test_debian_slim():
--- client_test/package_utils_test.py
+++ client_test/package_utils_test.py
@@ -2,10 +2,6 @@ from modal._package_utils import get_package_deps_mount_info
 
 
 def test_get_package_deps_mount_info():
-    res = get_package_deps_mount_info("modal")
-    assert len(res) == 1
-    assert res[0][0] == "modal"
-
     res = get_package_deps_mount_info("asyncio")
     assert len(res) == 1
     assert res[0][0] == "asyncio"
