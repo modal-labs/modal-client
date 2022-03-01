888fb2ec03fef265cbe228dbad66b76997872aa5
Akshat Bubna <akshatb42@gmail.com>
2022-03-01 15:07:57 +0530

    remove partial line logic from _logging

--- modal/_logging.py
+++ modal/_logging.py
@@ -25,22 +25,10 @@ class LogPrinter:
             raise Exception(f"Weird file descriptor {log.file_descriptor} for log output")
 
         if buf.isatty():
-            if self.add_newline:
-                # HACK: to make partial line outputs (like when using a progress bar that uses
-                # ANSI escape chars) work. If the last log line doesn't end with a newline,
-                # add one manually, and take it back the next time we print something.
-                # TODO: this can cause problems if there are partial lines being printed as logs, and the user is also
-                # printing to stdout directly. Can be solved if we can print directly here and rely on the redirection
-                # (without calling suspend()), and then add the newline logic to `write_callback`.
-                stdout_buf.write("\033[A\r")
-            self.add_newline = not log.data.endswith("\n")
-
             buf.write(color)
 
         buf.write(log.data)
 
         if buf.isatty():
             buf.write(colorama.Style.RESET_ALL)
-            if self.add_newline:
-                stdout_buf.write("\n")
             buf.flush()
