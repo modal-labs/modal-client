41a336f3219c9d413c7b35e880a2727ddd5a530b
Elias Freider <freider@users.noreply.github.com>
2022-03-01 21:13:33 +0100

    Secrets documentation (#948)
    * Add secrets documentation
    * Revert to singular secret in cases where multiple are not needed

--- modal/secret.py
+++ modal/secret.py
@@ -3,7 +3,60 @@ from .proto import api_pb2
 
 
 class Secret(Object, type_prefix="st"):
-    """A dictionary of environment variables for images"""
+    """A dictionary of environment variables for images
+
+    Secrets are a good and secure way to add credentials and other sensitive information
+    to the containers your functions run in. You can create and edit secrets from [modal.com/secrets](https://modal.com/secrets)
+    or programatically from your Python code.
+
+    ## Using secrets
+    To inject secrets into the container running your function, you add the `secret=` or `secrets=[...]` argument
+    to your `modal.function` annotation. For deployed secrets (e.g. secrets defined on the Modal website) you
+    can refer to your secrets using `Secret.include(secret_name)`:
+    ```python
+    import modal
+
+    @modal.function(secret=modal.Secret.include("my-secret-name"))
+    def some_function():
+        ...
+
+    @modal.function(secrets=[
+        modal.Secret.include("my-secret-name"),
+        modal.Secret.include("other-secret"),
+    ])
+    def other_function():
+        ...
+    ```
+    ## Programmatic creation of secrets
+    You can programatically create a secret and send it along to your function using `Secret.create`:
+
+    ```python
+    import os
+    import modal
+
+    @modal.Secret.factory
+    def my_local_secret():
+        return Secret.create({"FOO": os.environ["LOCAL_FOO"]})
+
+    @modal.function(secret=my_local_secret)
+    def some_function():
+        print(os.environ["FOO"])
+    ```
+
+    ## Deploying secrets
+    Sometimes, it can be convenient to not have to go through the website to save or update secrets.
+    You can then *deploy* secrets similar to how you deploy other objects to Modal, which has the
+    same effect as publishing a secret on the web page:
+
+    ```python
+    import modal
+
+    if __name__ == "__main__":
+        with modal.run() as app:
+            app.deploy("my-secret-name", Secret.create({"FOO": "BAR"}))
+
+    The secrets deployed this way will also show up on [modal.com/secrets](https://modal.com/secrets)
+    """
 
     @classmethod
     async def create(cls, env_dict={}, app=None):
