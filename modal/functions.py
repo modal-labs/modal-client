3429b5d2e3e4e77b45084406c2adfeac810899a6
Erik Bernhardsson <mail@erikbern.com>
2022-03-02 17:13:57 -0500

    fix bug caused by rebase

--- modal/functions.py
+++ modal/functions.py
@@ -277,7 +277,7 @@ class Function(Object, Factory, type_prefix="fu"):
         request = api_pb2.FunctionCreateRequest(
             app_id=app.app_id,
             function=function_definition,
-            schedule=self.schedule.proto_message,
+            schedule=self.schedule.proto_message if self.schedule is not None else None,
         )
         response = await app.client.stub.FunctionCreate(request)
 
