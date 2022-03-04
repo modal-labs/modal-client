00aba83d4bcda240101311f775876433bcbaf643
Akshat Bubna <akshatb42@gmail.com>
2022-03-04 17:49:13 +0530

    add BlobGet and blob_download

--- modal/_blob_utils.py
+++ modal/_blob_utils.py
@@ -21,6 +21,8 @@ async def blob_upload(payload, client):
 
     req = api_pb2.BlobCreateRequest(content_md5=content_md5)
     resp = await client.stub.BlobCreate(req)
+
+    blob_id = resp.blob_id
     target = resp.presigned_url
 
     async with aiohttp.ClientSession() as session:
@@ -30,3 +32,20 @@ async def blob_upload(payload, client):
             if resp.status != 200:
                 text = await resp.text()
                 raise Exception(f"Put to {target} failed with status {resp.status}: {text}")
+
+    return blob_id
+
+
+async def blob_download(blob_id, client):
+    req = api_pb2.BlobGetRequest(blob_id=blob_id)
+    resp = await client.stub.BlobGet(req)
+    target = resp.presigned_url
+
+    async with aiohttp.ClientSession() as session:
+        async with session.get(target) as resp:
+            if resp.status != 200:
+                text = await resp.text()
+                raise Exception(f"Get from {target} failed with status {resp.status}: {text}")
+
+            data = await resp.read()
+            return data
