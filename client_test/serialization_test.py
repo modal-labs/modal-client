4e11f9e9db0a505184297e1e97d58b2aa733dc4f
Erik Bernhardsson <mail@erikbern.com>
2022-03-15 11:57:40 -0400

    fix serialization test

--- client_test/serialization_test.py
+++ client_test/serialization_test.py
@@ -1,28 +1,28 @@
 import pytest
 
-from modal import App, Queue
+from modal.aio import AioApp, AioQueue
 
-app = App()
+app = AioApp()
 
 
-@Queue.factory
+@AioQueue.factory
 async def qf():
-    return await Queue.create()
+    return await AioQueue.create()
 
 
 @pytest.mark.asyncio
 async def test_persistent_object(servicer, client):
     async with app.run(client=client):
         # Serialize "dynamic" object and deserialize
-        q = await Queue.create(app=app)
+        q = await AioQueue.create(app=app)
         data = app._serialize(q)
         q_roundtrip = app._deserialize(data)
-        assert isinstance(q_roundtrip, Queue)
+        assert isinstance(q_roundtrip, AioQueue)
         assert q.object_id == q_roundtrip.object_id
 
         # Serialize factory object and deserialize
         await app.create_object(qf)
         data = app._serialize(qf)
         qf_roundtrip = app._deserialize(data)
-        assert isinstance(qf_roundtrip, Queue)
+        assert isinstance(qf_roundtrip, AioQueue)
         assert qf.object_id == qf_roundtrip.object_id
