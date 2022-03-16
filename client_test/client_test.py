701e33fdd71f6e6896ab8a2bb35cf895f8c5fa4f
Erik Bernhardsson <mail@erikbern.com>
2022-03-16 14:58:54 -0400

    Fix client_test.py

--- client_test/client_test.py
+++ client_test/client_test.py
@@ -2,7 +2,7 @@ import asyncio
 import pytest
 
 import modal.exception
-from modal.client import _Client
+from modal.client import AioClient
 from modal.exception import ConnectionError, VersionError
 from modal_proto import api_pb2
 
@@ -28,20 +28,22 @@ async def test_container_client(servicer, container_client):
 @pytest.mark.asyncio
 async def test_client_connection_failure():
     with pytest.raises(ConnectionError):
-        async with _Client("https://xyz.invalid", api_pb2.CLIENT_TYPE_CLIENT, None):
+        async with AioClient("https://xyz.invalid", api_pb2.CLIENT_TYPE_CLIENT, None):
             pass
 
 
 @pytest.mark.asyncio
 async def test_client_old_version(servicer):
     with pytest.raises(VersionError):
-        async with _Client(servicer.remote_addr, api_pb2.CLIENT_TYPE_CLIENT, ("foo-id", "foo-secret"), version="0.0.0"):
+        async with AioClient(
+            servicer.remote_addr, api_pb2.CLIENT_TYPE_CLIENT, ("foo-id", "foo-secret"), version="0.0.0"
+        ):
             pass
 
 
 @pytest.mark.asyncio
 async def test_server_client_gone_disconnects_client(servicer):
-    async with Client(servicer.remote_addr, api_pb2.CLIENT_TYPE_CLIENT, ("foo-id", "foo-secret")) as client:
+    async with AioClient(servicer.remote_addr, api_pb2.CLIENT_TYPE_CLIENT, ("foo-id", "foo-secret")) as client:
         servicer.heartbeat_return_client_gone = True
         await client._heartbeat()
         await asyncio.sleep(0)  # let event loop take care of cleanup
