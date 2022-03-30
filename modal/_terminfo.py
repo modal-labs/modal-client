6e31780922163a6044e774a551d45c82fe767975
Akshat Bubna <akshatb42@gmail.com>
2022-03-29 20:06:58 -0400

    add fallback for terminfo sequences

--- modal/_terminfo.py
+++ modal/_terminfo.py
@@ -1,6 +1,22 @@
-import curses
+from typing import Callable, Dict
 
-_initialized_terminfo = False
+_initialized_terminfo = None
+
+FALLBACK_SEQUENCES = {
+    "cuu": lambda i: f"\r\033[{i}A".encode("ascii"),
+    "cr": lambda: b"\r",
+    "ed": lambda: b"\033[J",
+    "el": lambda: b"\033[K",
+    "civis": lambda: b"\033[?25l",
+    "cvvis": lambda: b"\033[?25h",
+}
+
+
+def fallback_term_seq(capname, *args):
+    if capname not in FALLBACK_SEQUENCES:
+        raise Exception(f"Could not find capability {capname}")
+
+    return FALLBACK_SEQUENCES[capname](*args)
 
 
 def term_seq(capname, *args):
@@ -8,15 +24,25 @@ def term_seq(capname, *args):
 
     https://www.gnu.org/software/termutils/manual/termutils-2.0/html_chapter/tput_1.html
     """
-    global _initialized_terminfo
-    if not _initialized_terminfo:
-        curses.setupterm()
-        _initialized_terminfo = True
+    global _initialized_terminfo, curses
 
-    capstr = curses.tigetstr(capname)
-    if capstr is None:
-        raise Exception(f"Could not find capability {capname}")
-    return curses.tparm(capstr, *args)
+    if _initialized_terminfo is None:
+        try:
+            import curses
+
+            curses.setupterm()
+            _initialized_terminfo = True
+        except Exception:
+            _initialized_terminfo = False
+
+    if _initialized_terminfo:
+        capstr = curses.tigetstr(capname)
+        if capstr is None:
+            return fallback_term_seq(capname, *args)
+        else:
+            return curses.tparm(capstr, *args)
+    else:
+        return fallback_term_seq(capname, *args)
 
 
 def term_seq_str(capname, *args):
