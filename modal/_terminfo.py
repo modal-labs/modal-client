ebe048fb91c8b40af9a42399637e7f4955a4561c
Akshat Bubna <akshatb42@gmail.com>
2022-03-29 21:48:57 -0400

    fix mypy for fallback sequences

--- modal/_terminfo.py
+++ modal/_terminfo.py
@@ -1,8 +1,14 @@
 from typing import Callable, Dict
 
-_initialized_terminfo = None
+try:
+    import curses
 
-FALLBACK_SEQUENCES = {
+    curses.setupterm()
+    _using_terminfo = True
+except Exception:
+    _using_terminfo = False
+
+FALLBACK_SEQUENCES: Dict[str, Callable[..., bytes]] = {
     "cuu": lambda i: f"\r\033[{i}A".encode("ascii"),
     "cr": lambda: b"\r",
     "ed": lambda: b"\033[J",
@@ -24,18 +30,8 @@ def term_seq(capname, *args):
 
     https://www.gnu.org/software/termutils/manual/termutils-2.0/html_chapter/tput_1.html
     """
-    global _initialized_terminfo, curses
 
-    if _initialized_terminfo is None:
-        try:
-            import curses
-
-            curses.setupterm()
-            _initialized_terminfo = True
-        except Exception:
-            _initialized_terminfo = False
-
-    if _initialized_terminfo:
+    if _using_terminfo:
         capstr = curses.tigetstr(capname)
         if capstr is None:
             return fallback_term_seq(capname, *args)
