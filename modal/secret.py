94a9f927ecfaf6b28b512e0f3da2d442eb5a1c6c
Elias Freider <freider@users.noreply.github.com>
2022-04-05 14:05:02 +0200

    Move secrets guide from reference to Guide section (#1212)
    * Move secrets guide from reference to Guide section
    
    * Update guide

--- modal/secret.py
+++ modal/secret.py
@@ -9,59 +9,9 @@ class _Secret(Object, type_prefix="st"):
 
     Secrets are a secure way to add credentials and other sensitive information
     to the containers your functions run in. You can create and edit secrets on
-    [the dashboard](https://modal.com/secrets), or programatically from Python code.
+    [the dashboard](/secrets), or programmatically from Python code.
 
-    ### Using secrets
-    To inject secrets into the container running your function, you add the `secret=` or `secrets=[...]` argument
-    to your `modal.function` annotation. For deployed secrets (e.g., secrets defined on the Modal website) you
-    can refer to your secrets using `Secret.include(app, secret_name)`:
-
-    ```python
-    import modal
-
-    @app.function(secret=modal.Secret.include(app, "my-secret-name"))
-    def some_function():
-        ...
-
-    @app.function(secrets=[
-        modal.Secret.include(app, "my-secret-name"),
-        modal.Secret.include(app, "other-secret"),
-    ])
-    def other_function():
-        ...
-    ```
-
-    ### Programmatic creation of secrets
-    You can programatically create a secret and send it along to your function using `Secret.create`:
-
-    ```python
-    import os
-    import modal
-
-    @modal.Secret.factory
-    def my_local_secret():
-        return Secret.create({"FOO": os.environ["LOCAL_FOO"]})
-
-    @app.function(secret=my_local_secret)
-    def some_function():
-        print(os.environ["FOO"])
-    ```
-
-    ### Deploying secrets
-    Sometimes, it can be convenient to not have to go through the website to save or update secrets.
-    You can then *deploy* secrets similar to how you deploy other objects to Modal, which has the
-    same effect as publishing a secret on the web page:
-
-    ```python
-    import modal
-    app = modal.App()
-
-    if __name__ == "__main__":
-        with app.run() as app:
-            app.deploy("my-secret-name", Secret.create({"FOO": "BAR"}))
-    ```
-
-    The secrets deployed this way will also show up on [the dashboard](https://modal.com/secrets).
+    See [The guide](/docs/guide/secrets) for more information.
     """
 
     def __init__(self, app, env_dict={}, template_type=""):
