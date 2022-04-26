71e0238cf49e6c372e0b807c7ee297ac1e4c444e
Akshat Bubna <akshatb42@gmail.com>
2022-04-26 21:36:15 +0000

    simple fastapi example

--- modal/_asgi.py
+++ modal/_asgi.py
@@ -2,11 +2,14 @@ async def asgi_app_wrapper(asgi_app, scope, body=None):
     messages = []
 
     async def send(message):
+        print("SENT", message)
         messages.append(message)
 
     async def receive():
+        print("RECEIVED", body)
         return {"type": "http.request", "body": body}
 
+    print("INSIDE WRAPPER!", scope)
     await asgi_app(scope, receive, send)
 
     return messages
