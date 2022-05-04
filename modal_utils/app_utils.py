a604f24f02f0da06f38fe19e294cf36b0e7c12e4
Akshat Bubna <akshatb42@gmail.com>
2022-05-04 20:05:03 +0000

    create & validate username for new users

--- modal_utils/app_utils.py
+++ modal_utils/app_utils.py
@@ -1,4 +1,12 @@
+import re
 import urllib.parse
+
+# https://www.rfc-editor.org/rfc/rfc1035
+subdomain_regex = re.compile("^(?![0-9]+$)(?!-)[a-z0-9-]{,63}(?<!-)$")
+
+
+def is_valid_subdomain_label(label: str):
+    return subdomain_regex.match(label) is not None
 
 
 def is_valid_deployment_name(name):
