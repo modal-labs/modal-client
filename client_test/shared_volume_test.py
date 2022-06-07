030c82efaaba1d8fcd7cfcc0eea2b664437b1c0d
Erik Bernhardsson <mail@erikbern.com>
2022-06-06 21:37:27 -0400

    Fix client tests after rebase

--- client_test/shared_volume_test.py
+++ client_test/shared_volume_test.py
@@ -6,28 +6,28 @@ from modal.exception import InvalidError
 
 
 def test_shared_volume_files(client, test_dir, servicer):
-    app = modal.App()
+    stub = modal.Stub()
 
-    @app.function(
+    @stub.function(
         shared_volumes={"/root/foo": modal.SharedVolume()},
     )
     def f():
         pass
 
-    with app.run(client=client):
+    with stub.run(client=client):
         f()
 
 
 @pytest.mark.skipif(platform.system() == "Windows", reason="TODO: implement client-side path check on Windows.")
 def test_shared_volume_bad_path(client, test_dir, servicer):
-    app = modal.App()
+    stub = modal.Stub()
 
-    @app.function(
+    @stub.function(
         shared_volumes={"/root/../../foo": modal.SharedVolume()},
     )
     def f():
         pass
 
     with pytest.raises(InvalidError):
-        with app.run(client=client):
+        with stub.run(client=client):
             f()
