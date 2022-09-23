0679ec847394c6672a1465cc3ab57cef457d0f44
Richard Gong <richard@modal.com>
2022-09-23 13:29:05 -0400

    set correct task on input finish, show webhook execution breakdown (#3335)
    * have correct final task id on the input
    
    * fix tests
    
    * fix issue with task finished notifier
    
    * webhook handling had wrong typing

--- modal/_container_entrypoint.py
+++ modal/_container_entrypoint.py
@@ -181,6 +181,7 @@ class _FunctionIOManager:
 
         output = api_pb2.FunctionPutOutputsItem(
             input_id=input_id,
+            task_id=self.task_id,
             input_started_at=self.input_started_at,
             output_created_at=time.time(),
             result=api_pb2.GenericResult(**kwargs),
