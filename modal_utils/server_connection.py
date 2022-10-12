23a89da018e508ff374d2f329180949f48f5f45d
Luis Capelo <953118+luiscape@users.noreply.github.com>
2022-10-11 20:17:25 -0400

    Adds NGINX as local proxy for containers (#3584)
    This runs `nginx` as a daemon in our host nodes then forces clients running inside containers to use nginx as a proxy for the external server. Clients connect to nginx using a socket, skipping individual TLS negotiation and DNS lookups.

--- modal_utils/server_connection.py
+++ modal_utils/server_connection.py
@@ -37,20 +37,29 @@ class GRPCConnectionFactory:
             logger.exception(f"failed to parse server url: {server_url}")
             raise
 
-        self.target = o.netloc
-        self.is_tls = o.scheme.endswith("s")
+        # Determine if the url is a socket.
+        self.scheme = o.scheme
+        self.is_socket = self.scheme == "unix"
+        if self.is_socket:
+            self.target = o.path
+        else:
+            self.target = o.netloc
+            self.is_tls = o.scheme.endswith("s")
 
         self.metadata = auth_metadata(client_type, credentials)
         logger.debug(f"Connecting to {self.target} using scheme {o.scheme}")
 
     def create(self) -> Channel:
-        parts = self.target.split(":")
-        assert len(parts) <= 2, "Invalid target location: " + self.target
-        channel = Channel(
-            host=parts[0],
-            port=parts[1] if len(parts) == 2 else 443 if self.is_tls else 80,
-            ssl=self.is_tls,
-        )
+        if self.is_socket:
+            channel = Channel(path=self.target)
+        else:
+            parts = self.target.split(":")
+            assert len(parts) <= 2, "Invalid target location: " + self.target
+            channel = Channel(
+                host=parts[0],
+                port=parts[1] if len(parts) == 2 else 443 if self.is_tls else 80,
+                ssl=self.is_tls,
+            )
 
         # Inject metadata for the client.
         async def send_request(event: grpclib.events.SendRequest) -> None:
