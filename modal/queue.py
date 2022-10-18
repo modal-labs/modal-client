fb41811e5acffd4691c9542f8b698b9c954a4713
Akshat Bubna <akshatb42@gmail.com>
2022-10-18 14:27:44 -0400

    Queue updates (#3739)

--- modal/queue.py
+++ modal/queue.py
@@ -1,13 +1,13 @@
 import queue  # The system library
-import uuid
-from typing import Any, List
+import time
+import warnings
+from typing import Any, List, Optional
 
 from modal_proto import api_pb2
 from modal_utils.async_utils import synchronize_apis
 from modal_utils.grpc_utils import retry_transient_errors
 
 from ._serialization import deserialize, serialize
-from .config import logger
 from .object import Handle, Provider
 
 
@@ -23,46 +23,104 @@ class _QueueHandle(Handle, type_prefix="qu"):
     ```
     """
 
-    async def _get(self, block, timeout, n_values):
-        while timeout is None or timeout > 0:
+    async def _get_nonblocking(self, n_values: int) -> List[Any]:
+        request = api_pb2.QueueGetRequest(
+            queue_id=self.object_id,
+            timeout=0,
+            n_values=n_values,
+        )
+
+        response = await retry_transient_errors(self._client.stub.QueueGet, request)
+        if response.values:
+            return [deserialize(value, self._client) for value in response.values]
+        else:
+            return []
+
+    async def _get_blocking(self, timeout: Optional[float], n_values: int) -> List[Any]:
+        if timeout is not None:
+            deadline = time.time() + timeout
+        else:
+            deadline = None
+
+        while True:
             request_timeout = 50.0  # We prevent longer ones in order to keep the connection alive
-            if timeout is not None:
-                request_timeout = min(request_timeout, timeout)
-                timeout -= request_timeout
+
+            if deadline is not None:
+                request_timeout = min(request_timeout, deadline - time.time())
+
             request = api_pb2.QueueGetRequest(
                 queue_id=self.object_id,
-                block=block,
                 timeout=request_timeout,
                 n_values=n_values,
-                idempotency_key=str(uuid.uuid4()),
             )
+
             response = await retry_transient_errors(self._client.stub.QueueGet, request)
+
             if response.values:
                 return [deserialize(value, self._client) for value in response.values]
-            logger.debug("Queue get for %s had empty results, trying again" % self.object_id)
+
+            if deadline is not None and time.time() > deadline:
+                break
+
         raise queue.Empty()
 
-    async def get(self, block=True, timeout=None) -> Any:
-        """Get and pop the next object."""
-        values = await self._get(block, timeout, 1)
-        return values[0]
+    async def get(self, block: bool = True, timeout: Optional[float] = None) -> Optional[Any]:
+        """Remove and return the next object in the queue.
 
-    async def get_many(self, n_values: int, block=True, timeout=None) -> List[Any]:
-        """Get multiple objects, up to `n_values`."""
-        return await self._get(block, timeout, n_values)
+        If `block` is `True` (the default) and the queue is empty, `get` will wait indefinitely for
+        an object, or until `timeout` if a `timeout` is specified. Raises the native Python `queue.Empty`
+        exception if the `timeout` is reached.
+
+        If `block` is `False`, `get` returns `None` immediately if the queue is empty. The `timeout` is
+        ignored in this case.
+        """
+
+        if block:
+            values = await self._get_blocking(timeout, 1)
+        else:
+            if timeout is not None:
+                warnings.warn("Timeout is ignored for non-blocking get.")
+            values = await self._get_nonblocking(1)
+
+        if values:
+            return values[0]
+        else:
+            return None
+
+    async def get_many(self, n_values: int, block: bool = True, timeout: Optional[float] = None) -> List[Any]:
+        """Remove and return up to `n_values` objects from the queue.
+
+        If `block` is `True` (the default) and the queue is empty, `get` will wait indefinitely for
+        the next object, or until `timeout` if a `timeout` is specified. Raises the native Python `queue.Empty`
+        exception if the `timeout` is reached. Returns as many objects as are available (less then `n_values`)
+        as soon as the queue becomes non-empty.
+
+        If `block` is `False`, `get` returns `None` immediately if the queue is empty. The `timeout` is
+        ignored in this case.
+        """
+
+        if block:
+            return await self._get_blocking(timeout, n_values)
+        else:
+            if timeout is not None:
+                warnings.warn("Timeout is ignored for non-blocking get.")
+            return await self._get_nonblocking(n_values)
 
     async def put(self, v: Any) -> None:
-        """Add a single object to the queue."""
+        """Add an object to the end of the queue."""
+
         await self.put_many([v])
 
     async def put_many(self, vs: List[Any]) -> None:
-        """Add several objects to the queue."""
+        """Add several objects to the end of the queue."""
+
         vs_encoded = [serialize(v) for v in vs]
+
         request = api_pb2.QueuePutRequest(
             queue_id=self.object_id,
             values=vs_encoded,
-            idempotency_key=str(uuid.uuid4()),
         )
+
         await retry_transient_errors(self._client.stub.QueuePut, request)
 
 
@@ -78,7 +136,6 @@ class _Queue(Provider[_QueueHandle]):
     async def _load(self, client, app_id, loader, message_callback, existing_object_id):
         request = api_pb2.QueueCreateRequest(app_id=app_id, existing_queue_id=existing_object_id)
         response = await client.stub.QueueCreate(request)
-        logger.debug("Created queue with id %s" % response.queue_id)
         return _QueueHandle(client, response.queue_id)
 
 
