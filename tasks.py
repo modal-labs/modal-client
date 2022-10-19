d6c6fe11f9d15dc693ef98d362077fc8331d1766
Erik Bernhardsson <mail@erikbern.com>
2022-10-19 13:32:23 -0400

    grpclib

--- tasks.py
+++ tasks.py
@@ -8,4 +8,5 @@ def protoc(ctx):
         f"{sys.executable} -m grpc_tools.protoc"
         + " --python_out=. --grpclib_python_out=. --grpc_python_out=. --mypy_out=. --mypy_grpc_out=."
     )
+    print(py_protoc)
     ctx.run(f"{py_protoc} -I . modal_proto/api.proto")
