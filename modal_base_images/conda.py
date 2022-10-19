32262673cc3169509024d65ed457952d8b08e489
Erik Bernhardsson <mail@erikbern.com>
2022-10-19 14:20:47 -0400

    modal_base_images

---
+++ modal_base_images/conda.py
@@ -0,0 +1,13 @@
+import asyncio
+
+from modal.aio import AioConda, AioStub
+
+
+async def main(client=None):
+    stub = AioStub(image=AioConda())
+    async with stub.run(client=client):
+        pass
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
