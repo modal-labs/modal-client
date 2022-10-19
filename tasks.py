1790699083c4ba0a3cfbd4840762905c36be48c4
Erik Bernhardsson <mail@erikbern.com>
2022-10-19 13:19:04 -0400

    Basic shell for CI/CD (WIP)

---
+++ tasks.py
@@ -0,0 +1,10 @@
+import sys
+from invoke import task
+
+@task
+def protoc(ctx):
+    py_protoc = (
+        f"{sys.executable} -m grpc_tools.protoc"
+        + " --python_out=. --grpclib_python_out=. --grpc_python_out=. --mypy_out=. --mypy_grpc_out=."
+    )
+    ctx.run(f"{py_protoc} -I . modal_proto/api.proto")
