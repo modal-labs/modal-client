85dc93b46d105eda3acf3cfdb36d4a92401cffd6
Erik Bernhardsson <mail@erikbern.com>
2022-10-20 15:20:25 -0400

    Fix deprecated syntax (#19)

--- modal_base_images/conda.py
+++ modal_base_images/conda.py
@@ -1,11 +1,11 @@
 # Copyright Modal Labs 2022
 import asyncio
 
-from modal.aio import AioConda, AioStub
+from modal.aio import AioImage, AioStub
 
 
 async def main(client=None):
-    stub = AioStub(image=AioConda())
+    stub = AioStub(image=AioImage.conda())
     async with stub.run(client=client):
         pass
 
