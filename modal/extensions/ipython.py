1a61ca36591426de4490a3eb83b537652738c4a5
Elias Freider <elias@freider.se>
2022-10-26 14:49:06 +0200

    Indent

--- modal/extensions/ipython.py
+++ modal/extensions/ipython.py
@@ -64,28 +64,28 @@ else:
         local_ns.update(res)
 
 
-    def load_ipython_extension(ipython):
-        global app_ctx
+def load_ipython_extension(ipython):
+    global app_ctx
 
-        # Set logger for notebook sys.stdout
-        logger.addHandler(logging.StreamHandler(stream=sys.stdout))
-        logger.setLevel(config["loglevel"])
+    # Set logger for notebook sys.stdout
+    logger.addHandler(logging.StreamHandler(stream=sys.stdout))
+    logger.setLevel(config["loglevel"])
 
-        # Create a app and provide it in the IPython app
-        stub = Stub(blocking_late_creation_ok=True)
-        ipython.push({"stub": stub})
+    # Create a app and provide it in the IPython app
+    stub = Stub(blocking_late_creation_ok=True)
+    ipython.push({"stub": stub})
 
-        app_ctx = stub.run()
+    app_ctx = stub.run()
 
-        # Notebooks have an event loop present, but we want this function
-        # to be blocking. This is fairly hacky.
-        run_coro_blocking(app_ctx.__aenter__())
+    # Notebooks have an event loop present, but we want this function
+    # to be blocking. This is fairly hacky.
+    run_coro_blocking(app_ctx.__aenter__())
 
-        def exit_app():
-            print("Exiting modal app")
-            run_coro_blocking(app_ctx.__aexit__(None, None, None))
+    def exit_app():
+        print("Exiting modal app")
+        run_coro_blocking(app_ctx.__aexit__(None, None, None))
 
-        atexit.register(exit_app)
+    atexit.register(exit_app)
 
 
 def unload_ipython_extension(ipython):
