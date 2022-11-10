092b07d307e5443f0e286093b2efae5e58fb9d41
Erik Bernhardsson <mail@erikbern.com>
2022-11-09 21:44:44 -0500

    Make it possible to invoke a decorated function directly (#59)
    * Make it possible to invoke a decorated function directly
    
    * Add decorator test
    
    * copyright, remove double definition (ironic)

--- modal_utils/decorator_utils.py
+++ modal_utils/decorator_utils.py
@@ -15,13 +15,15 @@ def decorator_with_options(dec_fun):
 
     @functools.wraps(dec_fun)
     def wrapper(*args, **kwargs):
-        if args and not kwargs and inspect.isfunction(args[-1]):
-            # The decorator is invoked without arguments
-            # Return a closure that consumes the function
-            return dec_fun(*args)
+        # Note: if the def_fun is a method, then args will contain the object the method is bound to.
+        if args and inspect.isfunction(args[-1]):
+            # The decorator is invoked with a function as its first argument
+            # Call the decorator function directly
+            return dec_fun(*args, **kwargs)
         else:
             # The function is called with arguments
             # bind those arguments to the function and decorate the next token
+            # args is only nonempty if it's the object the method is bound to
             return functools.partial(dec_fun, *args, **kwargs)
 
     return wrapper
