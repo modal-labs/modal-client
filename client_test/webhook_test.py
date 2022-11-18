8a7f729b35ffe5ecfc1b2a95146140bb75068d59
Eric Zhang <ekzhang1@gmail.com>
2022-11-18 06:16:35 +0000

    Add test for webhook CORS support

--- client_test/webhook_test.py
+++ client_test/webhook_test.py
@@ -15,3 +15,26 @@ async def f(x):
 async def test_webhook(servicer, aio_client):
     async with stub.run(client=aio_client):
         assert f.web_url
+
+
+def test_webhook_cors():
+    from fastapi.testclient import TestClient
+
+    from modal._asgi import webhook_asgi_app
+
+    def handler():
+        return {"message": "Hello, World!"}
+
+    app = webhook_asgi_app(handler, method="GET")
+    client = TestClient(app)
+    resp = client.options(
+        "/",
+        headers={
+            "Origin": "http://example.com",
+            "Access-Control-Request-Method": "POST",
+        },
+    )
+    assert resp.headers["Access-Control-Allow-Origin"] == "http://example.com"
+
+    assert client.get("/").json() == {"message": "Hello, World!"}
+    assert client.post("/").status_code == 405  # Method Not Allowed
