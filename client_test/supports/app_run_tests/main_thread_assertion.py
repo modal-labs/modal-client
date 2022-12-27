f6a000cee12b902e75af6fda20e964859e0eeb19
Elias Freider <elias@freider.se>
2022-12-27 10:18:00 +0100

    Copyright

--- client_test/supports/app_run_tests/main_thread_assertion.py
+++ client_test/supports/app_run_tests/main_thread_assertion.py
@@ -1,10 +1,13 @@
+# Copyright Modal Labs 2022
 import pytest
 import threading
 
 import modal
 
 assert threading.current_thread() == threading.main_thread()
-pytest._did_load_main_thread_assertion = True  # can be checked to ensure module is loaded at all
+pytest._did_load_main_thread_assertion = (
+    True  # can be checked to ensure module is loaded at all
+)
 
 stub = modal.Stub()
 
