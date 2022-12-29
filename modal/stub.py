4fee15f15d0c45b56b114d19a40591334d2aa6f7
Elias Freider <elias@freider.se>
2022-12-29 16:26:21 +0100

    Adds proto fields for specifying new async modes for webhooks

--- modal/stub.py
+++ modal/stub.py
@@ -681,6 +681,9 @@ class _Stub:
                 method=method,
                 wait_for_response=wait_for_response,
                 requested_suffix=label,
+                async_mode=api_pb2.WEBHOOK_ASYNC_MODE_DISABLED
+                if wait_for_response
+                else api_pb2.WEBHOOK_ASYNC_MODE_TRIGGER,
             ),
             cpu=cpu,
             memory=memory,
@@ -742,7 +745,12 @@ class _Stub:
             mounts=mounts,
             shared_volumes=shared_volumes,
             webhook_config=api_pb2.WebhookConfig(
-                type=_webhook_type, wait_for_response=wait_for_response, requested_suffix=label
+                type=_webhook_type,
+                wait_for_response=wait_for_response,
+                requested_suffix=label,
+                async_mode=api_pb2.WEBHOOK_ASYNC_MODE_DISABLED
+                if wait_for_response
+                else api_pb2.WEBHOOK_ASYNC_MODE_TRIGGER,
             ),
             cpu=cpu,
             memory=memory,
