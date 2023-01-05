705736cf56a297c4a32830277422f534d476a304
Elias Freider <freider@users.noreply.github.com>
2023-01-05 21:56:23 +0100

    Update modal/stub.py
    Co-authored-by: Jonathon Belotti <jonathon@modal.com>

--- modal/stub.py
+++ modal/stub.py
@@ -757,7 +757,7 @@ class _Stub:
         [guide on web endpoints](https://modal.com/docs/guide/webhooks).
 
         The two `wait_for_response` modes for webhooks are as follows:
-        * wait_for_response=True - tries to fulfill the request on the original URL, but returns a 302 rediect after ~150s to a result url (original url with an added __modal_function_id=... query parameter)
+        * wait_for_response=True - tries to fulfill the request on the original URL, but returns a 302 redirect after ~150s to a result URL (original URL with an added `__modal_function_id=fc-1234abcd` query parameter)
         * wait_for_response=False - immediately returns a 202 ACCEPTED response with a json payload: {"result_url": "..."} containing the result "redirect" url from above (which in turn redirects to itself every 150s)
         """
         if image is None:
