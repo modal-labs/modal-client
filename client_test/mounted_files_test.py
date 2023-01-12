ba27e0f8a674c2ba5cd9c7612f1dcbaf9601c4f6
Akshat Bubna <akshatb42@gmail.com>
2023-01-12 16:40:01 +0530

    Automount packages for serialized functions as well (#192)

--- client_test/mounted_files_test.py
+++ client_test/mounted_files_test.py
@@ -52,6 +52,41 @@ def test_mounted_files_script(test_dir):
     assert not any(["k.py" in f for f in files])
 
 
+serialized_fn_path = "pkg_a/serialized_fn.py"
+
+
+def test_mounted_files_serialized(test_dir):
+    p = subprocess.run(
+        [sys.executable, str(serialized_fn_path)],
+        capture_output=True,
+        cwd=test_dir / Path("supports"),
+        env={**os.environ, "PYTHONPATH": str(test_dir / Path("supports"))},
+    )
+    assert p.returncode == 0
+    stdout = p.stdout.decode("utf-8")
+    stderr = p.stderr.decode("utf-8")
+    print("stdout: ", stdout)
+    print("stderr: ", stderr)
+    files = set(stdout.splitlines())
+
+    assert len(files) == 7
+    # Assert everything from `pkg_a` is in the output.
+    assert any(["a.py" in f for f in files])
+    assert any(["c.py" in f for f in files])
+    assert not any(["d.py" in f for f in files])
+    assert any(["e.py" in f for f in files])
+    assert any(["serialized_fn.py" in f for f in files])
+
+    # Assert everything from `pkg_b` is in the output.
+    assert any(["__init__.py" in f for f in files])
+    assert any(["f.py" in f for f in files])
+    assert any(["h.py" in f for f in files])
+
+    # Assert nothing from `pkg_c` is in the output.
+    assert not any(["i.py" in f for f in files])
+    assert not any(["k.py" in f for f in files])
+
+
 def test_mounted_files_package(test_dir):
     p = subprocess.run([sys.executable, "-m", "pkg_a.package"], cwd=test_dir / Path("supports"), capture_output=True)
     assert p.returncode == 0
@@ -61,7 +96,7 @@ def test_mounted_files_package(test_dir):
     print("stderr: ", stderr)
     files = set(stdout.splitlines())
 
-    assert len(files) == 9
+    assert len(files) == 10
 
     # Assert everything from `pkg_a` is in the output.
     assert any(["a.py" in f for f in files])
---
+++ client_test/supports/pkg_a/serialized_fn.py
@@ -0,0 +1,27 @@
+# Copyright Modal Labs 2022
+import asyncio
+
+import a  # noqa
+import b  # noqa
+import b.c  # noqa
+import pkg_b  # noqa
+import six  # noqa
+
+import modal  # noqa
+from modal._function_utils import FunctionInfo
+
+
+def f():
+    pass
+
+
+async def get_files():
+    fn_info = FunctionInfo(f, serialized=True)
+
+    for _, mount in fn_info.get_mounts().items():
+        async for file_info in mount._get_files():
+            print(file_info.rel_filename)
+
+
+if __name__ == "__main__":
+    asyncio.run(get_files())
