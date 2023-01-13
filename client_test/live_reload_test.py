8b7798d3cb7ca8e4065f5e01e97ae72df0acd7a4
Jonathon Belotti <jonathon@modal.com>
2023-01-13 10:47:20 -0500

    live-reload: restarting process based solution, with tests (#197)

---
+++ client_test/live_reload_test.py
@@ -0,0 +1,75 @@
+# Copyright Modal Labs 2023
+import asyncio
+import os
+import pytest
+
+try:
+    from unittest.mock import AsyncMock
+except ImportError:
+    # Support Python 3.7
+    from unittest.mock import MagicMock
+
+    class AsyncMock(MagicMock):  # type: ignore
+        async def __call__(self, *args, **kwargs):
+            return super(AsyncMock, self).__call__(*args, **kwargs)  # type: ignore
+
+
+from watchfiles import Change
+
+from modal import Stub
+from modal._live_reload import MODAL_AUTORELOAD_ENV
+from modal._watcher import AppChange
+from modal.aio import AioStub
+
+
+def dummy():
+    pass
+
+
+class FakeProcess:
+    def send_signal(self, signal):
+        pass
+
+    def terminate(self):
+        pass
+
+
+def test_file_changes_trigger_reloads(client, monkeypatch, servicer, test_dir):
+    async def fake_watch(stub, output_mgr, timeout):
+        yield AppChange.START
+        yield {(Change.modified, "fake-test-data.py")}
+        yield {(Change.modified, "fake-test-data.py")}
+        yield {(Change.added, "another-file.py")}
+        yield AppChange.TIMEOUT
+
+    stub = Stub()
+    stub.webhook(dummy)
+
+    mock_create_subprocess_exec = AsyncMock(return_value=FakeProcess())
+    monkeypatch.setattr("modal.stub.asyncio.create_subprocess_exec", mock_create_subprocess_exec)
+    monkeypatch.setattr("modal._watcher.watch", fake_watch)
+
+    stub.serve(client=client, timeout=None)
+    assert mock_create_subprocess_exec.call_count == 3
+
+
+@pytest.mark.asyncio
+async def test_reloadable_serve_ignores_file_changes(client, monkeypatch, servicer, test_dir):
+    async def fake_watch(stub, output_mgr, timeout):
+        yield AppChange.START
+        yield AppChange.TIMEOUT
+
+    stub = AioStub()
+    stub.webhook(dummy)
+
+    mock_create_subprocess_exec = AsyncMock(return_value=FakeProcess())
+    monkeypatch.setattr("modal.stub.asyncio.create_subprocess_exec", mock_create_subprocess_exec)
+    monkeypatch.setattr("modal._watcher.watch", fake_watch)
+
+    # The app should not react to AppChange.TIMEOUT, and instead need
+    # the wait_for to cancel it.
+    monkeypatch.setattr(os, "environ", {MODAL_AUTORELOAD_ENV: "ap-12345"})
+    with pytest.raises(asyncio.TimeoutError):
+        await asyncio.wait_for(stub.serve(client=client), timeout=1.0)
+
+    assert mock_create_subprocess_exec.call_count == 0
