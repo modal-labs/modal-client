e8a48a9bee88cf7c04437ecea346f1b0dd359678
Erik Bernhardsson <mail@erikbern.com>
2023-01-16 14:09:41 -0500

    Add a basic test calling a function that was looked up

--- client_test/lookup_test.py
+++ client_test/lookup_test.py
@@ -7,17 +7,32 @@ from modal.exception import InvalidError, NotFoundError
 
 @pytest.mark.asyncio
 async def test_persistent_object(servicer, aio_client):
-    stub_1 = AioStub()
-    stub_1["q_1"] = AioQueue()
-    await stub_1.deploy("my-queue", client=aio_client)
+    stub = AioStub()
+    stub["q_1"] = AioQueue()
+    await stub.deploy("my-queue", client=aio_client)
 
-    stub_2 = AioStub()
-    async with stub_2.run(client=aio_client) as app_2:
-        assert isinstance(app_2, AioApp)
+    q = await aio_lookup("my-queue", client=aio_client)
+    # assert isinstance(q_3, AioQueue)  # TODO(erikbern): it's a AioQueueHandler
+    assert q.object_id == "qu-1"
 
-        q_3 = await aio_lookup("my-queue", client=aio_client)
-        # assert isinstance(q_3, AioQueue)  # TODO(erikbern): it's a AioQueueHandler
-        assert q_3.object_id == "qu-1"
+    with pytest.raises(NotFoundError):
+        await aio_lookup("bazbazbaz", client=aio_client)
 
-        with pytest.raises(NotFoundError):
-            await aio_lookup("bazbazbaz", client=aio_client)
+
+def square(x):
+    # This function isn't deployed anyway
+    pass
+            
+
+@pytest.mark.asyncio
+async def test_lookup_function(servicer, aio_client):
+    stub = AioStub()
+
+    stub.function(square)
+    await stub.deploy("my-function", client=aio_client)
+
+    f = await aio_lookup("my-function", client=aio_client)
+    assert f.object_id == "fu-1"
+
+    # Make sure we can call this function
+    assert await f.call(2, 4) == 20
