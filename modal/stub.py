e363b611f0793bb6d678eb95455f5190778f8f83
Jonathon Belotti <jonathon@modal.com>
2023-02-04 22:56:21 +0000

    fix: skip reload and warn when file change during stub.serve in python3.7

--- modal/stub.py
+++ modal/stub.py
@@ -393,7 +393,13 @@ class _Stub:
                 async with self._run(client, output_mgr, None, mode=StubRunMode.SERVE) as app:
                     client.set_pre_stop(app.disconnect)
                     existing_app_id = app.app_id
-                    event = await event_agen.__anext__()  # wait for 1st modification before restarting
+                    event = await event_agen.__anext__()
+                    if sys.version_info.major == 3 and sys.version_info.minor <= 7:
+                        while event != AppChange.TIMEOUT:
+                            output_mgr.print_if_visible(
+                                "Live-reload skipped. This feature is unsupported on Python 3.7. Upgrade to Python 3.8+ to enable live-reloading."
+                            )
+                            event = await event_agen.__anext__()
 
                 while event != AppChange.TIMEOUT:
                     curr_proc = await restart_serve(
