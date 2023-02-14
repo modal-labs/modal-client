6f22a4f23fed5c849e6ed247e3cb9eb6432575df
Erik Bernhardsson <mail@erikbern.com>
2023-02-14 16:52:12 -0500

    type safe _from_id (#302)
    * Stricter type checking in _from_id
    
    * minor metaclass cleanup
    
    * Add test + fix
    
    * type typo
    
    * mypy complaint

--- modal/_object_meta.py
+++ modal/_object_meta.py
@@ -10,6 +10,10 @@ class ObjectMeta(type):
     prefix_to_type: Dict[str, Any] = {}
 
     def __new__(metacls, name, bases, dct, type_prefix=None):
+        # Set the _type_prefix variable
+        dct.update(dict(_type_prefix=type_prefix))
+
+        # Create class
         new_cls = type.__new__(metacls, name, bases, dct)
 
         # If this is a synchronized wrapper, just return early
@@ -18,7 +22,6 @@ class ObjectMeta(type):
 
         # Needed for serialization, also for loading objects dynamically
         if type_prefix is not None:
-            new_cls._type_prefix = type_prefix  # type: ignore
             metacls.prefix_to_type[type_prefix] = new_cls
 
         logger.debug(f"Created Object class {name}")
--- modal/object.py
+++ modal/object.py
@@ -23,6 +23,8 @@ class Handle(metaclass=ObjectMeta):
     well as distributed data structures like Queues or Dicts.
     """
 
+    _type_prefix: str
+
     def __init__(self):
         raise Exception("__init__ disallowed, use proper classmethods")
 
@@ -31,7 +33,7 @@ class Handle(metaclass=ObjectMeta):
         self._object_id = None
 
     @classmethod
-    def _new(cls):
+    def _new(cls: Type[H]) -> H:
         obj = Handle.__new__(cls)
         obj._init()
         obj._initialize_from_proto(None)
@@ -45,22 +47,31 @@ class Handle(metaclass=ObjectMeta):
     def _initialize_from_proto(self, proto: Optional[Message]):
         pass  # default implementation
 
-    @staticmethod
-    def _from_id(object_id: str, client: _Client, proto: Optional[Message]):
-        parts = object_id.split("-")
-        if len(parts) != 2:
-            raise InvalidError(f"Object id {object_id} has no dash in it")
-        prefix = parts[0]
-        if prefix not in ObjectMeta.prefix_to_type:
-            raise InvalidError(f"Object prefix {prefix} does not correspond to a type")
-        object_cls = ObjectMeta.prefix_to_type[prefix]
+    @classmethod
+    def _from_id(cls: Type[H], object_id: str, client: _Client, proto: Optional[Message]) -> H:
+        if cls._type_prefix is not None:
+            # This is called directly on a subclass, e.g. Secret.from_id
+            if not object_id.startswith(cls._type_prefix):
+                raise InvalidError(f"Object {object_id} does not start with {cls._type_prefix}")
+            object_cls = cls
+        else:
+            # This is called on the base class, e.g. Handle.from_id
+            parts = object_id.split("-")
+            if len(parts) != 2:
+                raise InvalidError(f"Object id {object_id} has no dash in it")
+            prefix = parts[0]
+            if prefix not in ObjectMeta.prefix_to_type:
+                raise InvalidError(f"Object prefix {prefix} does not correspond to a type")
+            object_cls = ObjectMeta.prefix_to_type[prefix]
+
+        # Instantiate object and return
         obj = object_cls._new()
         obj._initialize_handle(client, object_id)
         obj._initialize_from_proto(proto)
         return obj
 
     @classmethod
-    async def from_id(cls, object_id: str, client: Optional[_Client] = None):
+    async def from_id(cls: Type[H], object_id: str, client: Optional[_Client] = None) -> H:
         # This is used in a few examples to construct FunctionCall objects
         # TODO(erikbern): doesn't use _initialize_from_proto - let's use AppLookupObjectRequest?
         # TODO(erikbern): this should probably be on the provider?
@@ -69,7 +80,7 @@ class Handle(metaclass=ObjectMeta):
         return cls._from_id(object_id, client, None)
 
     @property
-    def object_id(self):
+    def object_id(self) -> str:
         return self._object_id
 
     @classmethod
@@ -92,7 +103,8 @@ class Handle(metaclass=ObjectMeta):
         if not response.object_id:
             raise NotFoundError(response.error_message)
         proto = response.function  # TODO: handle different object types
-        return Handle._from_id(response.object_id, client, proto)
+        handle: H = cls._from_id(response.object_id, client, proto)
+        return handle
 
 
 async def _lookup(
