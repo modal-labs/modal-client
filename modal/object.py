f41a76565ddb303130a2c1e31ede17913324bb0a
Erik Bernhardsson <mail@erikbern.com>
2023-02-15 17:45:47 -0500

    Step 1 towards deploying standalone objects

--- modal/object.py
+++ modal/object.py
@@ -155,6 +155,24 @@ class Provider(Generic[H]):
     def local_uuid(self):
         return self._local_uuid
 
+    async def _deploy(self, label: str, client: Optional[_Client] = None) -> H:
+        """mdmd:hidden
+
+        Note 1: this uses the single-object app method, which we're planning to get rid of later
+        Note 2: still considering this an "internal" method, but we'll make it "official" later
+        """
+        from .stub import _Stub
+
+        if client is None:
+            client = await _Client.from_env()
+
+        handle_cls = self.get_handle_cls()
+        object_entity = handle_cls._type_prefix
+        _stub = _Stub(label, _object=self)
+        await _stub.deploy(client=client, object_entity=object_entity)
+        handle: H = await handle_cls.from_app(label, client=client)
+        return handle
+
     def persist(self, label: str):
         """Deploy a Modal app containing this object. This object can then be imported from other apps using
         the returned reference, or by calling `modal.SharedVolume.from_name(label)` (or the equivalent method
@@ -178,13 +196,7 @@ class Provider(Generic[H]):
         """
 
         async def _load_persisted(resolver: Resolver) -> H:
-            from .stub import _Stub
-
-            _stub = _Stub(label, _object=self)
-            await _stub.deploy(client=resolver.client)
-            handle_cls = cls.get_handle_cls()
-            handle: H = await handle_cls.from_app(label, client=resolver.client)
-            return handle
+            return await self._deploy(label, resolver.client)
 
         # Create a class of type cls, but use the base constructor
         cls = type(self)
