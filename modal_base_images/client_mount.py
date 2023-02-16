efa292b1f12309934f87a190caaf8e0499ed8994
Erik Bernhardsson <mail@erikbern.com>
2023-02-16 12:56:27 -0500

    Use _deploy for deploying the client mount (#310)

--- modal_base_images/client_mount.py
+++ modal_base_images/client_mount.py
@@ -1,15 +1,13 @@
 # Copyright Modal Labs 2022
 import asyncio
 
-import modal.aio
 from modal.mount import aio_create_client_mount, client_mount_name
 from modal_proto import api_pb2
 
 
 async def main(client=None):
-    stub = modal.aio.AioStub()
-    stub["mount"] = aio_create_client_mount()
-    await stub.deploy(client_mount_name(), api_pb2.DEPLOYMENT_NAMESPACE_GLOBAL, client=client)
+    mount = aio_create_client_mount()
+    await mount._deploy(client_mount_name(), api_pb2.DEPLOYMENT_NAMESPACE_GLOBAL, client=client)
 
 
 if __name__ == "__main__":
