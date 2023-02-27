09d602e165dcb69f430925b75f14f32a003dcd88
Erik Bernhardsson <mail@erikbern.com>
2023-02-26 20:46:51 -0500

    Add a simple test for win and 3.7 too

--- client_test/live_reload_test.py
+++ client_test/live_reload_test.py
@@ -6,6 +6,13 @@ from modal._live_reload import aio_run_serve_loop, run_serve_loop
 from .supports.skip import skip_old_py, skip_windows
 
 
+@pytest.mark.asyncio
+async def test_live_reload(client, monkeypatch, test_dir, server_url_env, servicer):
+    stub_file = str(test_dir / "supports" / "app_run_tests" / "default_stub.py")
+    await aio_run_serve_loop(stub_file, timeout=3.0)
+    assert servicer.app_set_objects_count == 1
+
+
 @skip_old_py("live-reload requires python3.8 or higher", (3, 8))
 @skip_windows("live-reload not supported on windows")
 def test_file_changes_trigger_reloads(client, monkeypatch, test_dir, server_url_env, servicer):
