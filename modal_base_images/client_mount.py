00b73cd5c63f8bb4027f2a658fc60cc1fad86914
Eric Zhang <ekzhang1@gmail.com>
2023-03-13 20:47:26 +0000

    Give feedback when deploying client mount

--- modal_base_images/client_mount.py
+++ modal_base_images/client_mount.py
@@ -7,7 +7,9 @@ from modal_proto import api_pb2
 
 async def main(client=None):
     mount = aio_create_client_mount()
+    name = client_mount_name()
     await mount._deploy(client_mount_name(), api_pb2.DEPLOYMENT_NAMESPACE_GLOBAL, client=client)
+    print(f"✅ Deployed client mount {name} to global namespace.")
 
 
 if __name__ == "__main__":
