3d01f08def5eaf32cff95e23caf81dfa9c3f2755
Erik Bernhardsson <mail@erikbern.com>
2023-03-21 10:25:01 -0400

    Add comment explaining the serialization problem

--- client_test/serialization_test.py
+++ client_test/serialization_test.py
@@ -15,5 +15,12 @@ async def test_persistent_object(servicer, client):
         q = running_app.q
         data = serialize(q)
         assert len(data) < 256  # Currently 93
+        # Note: if this blows up significantly, it's most likely because
+        # cloudpickle can't find a class in the global scope. When this
+        # happens, it tries to serialize the entire class along with the
+        # object. The reason it doesn't find the class in the global scope
+        # is most likely because the name doesn't match. To fix this, make
+        # sure that cls.__name__ (which is something synchronicity sets)
+        # is the same as the symbol defined in the global scope.
         q_roundtrip = deserialize(data, running_app)
         assert q.object_id == q_roundtrip.object_id
