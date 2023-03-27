742ba6014ff7ad930c479479e090a8970d08f2d9
Luis Capelo <953118+luiscape@users.noreply.github.com>
2023-03-27 11:49:27 -0400

    Adds support for multiple A100s in function (#427)
    Creates a new task type for running multi-GPU workloads with A100s. Resource requests like so
    
    ```python
    @stub.function(gpu=modal.gpu.A100(count=N))
    def f(): ...
    ```
    will now be granted `N` GPUs. This is useful for training large models using multiple A100 GPUs on the same node.

--- client_test/gpu_test.py
+++ client_test/gpu_test.py
@@ -99,9 +99,36 @@ def test_memory_selection_gpu_variant(client, servicer, memory, gpu_type):
     with stub.run(client=client):
         pass
 
-    # assert len(servicer.app_functions) == 1
     func_def = next(iter(servicer.app_functions.values()))
 
     assert func_def.resources.gpu_config.count == 1
     assert func_def.resources.gpu_config.type == gpu_type
     assert func_def.resources.gpu_config.memory == memory
+
+
+A100_GPU_COUNT_MAPPING = {1: api_pb2.GPU_TYPE_A100, **{i: api_pb2.GPU_TYPE_A100_40GB_MANY for i in range(2, 5)}}
+
+
+@pytest.mark.parametrize("count,gpu_type", A100_GPU_COUNT_MAPPING.items())
+def test_gpu_type_selection_from_count(client, servicer, count, gpu_type):
+    import modal
+
+    stub = Stub()
+
+    # Functions that use A100 20GB can only request one GPU
+    # at a time.
+    with pytest.raises(ValueError):
+        stub.function(dummy, gpu=modal.gpu.A100(count=2, memory=20))
+        with stub.run(client=client):
+            pass
+
+    # Task type changes whenever user asks more than 1 GPU on
+    # an A100.
+    stub.function(dummy, gpu=modal.gpu.A100(count=count))
+    with stub.run(client=client):
+        pass
+
+    func_def = next(iter(servicer.app_functions.values()))
+
+    assert func_def.resources.gpu_config.count == count
+    assert func_def.resources.gpu_config.type == gpu_type
