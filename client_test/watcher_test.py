173eda3586e138c755378f6631f2fa4a1f845a70
Akshat Bubna <akshatb42@gmail.com>
2023-04-05 23:10:44 -0400

    Fix serve watching logic for directories (#459)

--- client_test/watcher_test.py
+++ client_test/watcher_test.py
@@ -16,13 +16,15 @@ async def test__watch_args_from_mounts(monkeypatch, test_dir):
         mounts=[
             _Mount.from_local_file("/x/foo.py", remote_path="/foo.py"),
             _Mount.from_local_dir("/one/two/bucklemyshoe", remote_path="/"),
+            _Mount.from_local_dir("/x/z", remote_path="/z"),
         ]
     )
 
-    assert paths == {Path("/x"), Path("/one/two/bucklemyshoe")}
+    assert paths == {Path("/x"), Path("/one/two/bucklemyshoe"), Path("/x/z")}
     assert watch_filter(Change.modified, "/x/foo.py")
     assert not watch_filter(Change.modified, "/x/notwatched.py")
     assert not watch_filter(Change.modified, "/x/y/foo.py")
+    assert watch_filter(Change.modified, "/x/z/bar.py")
     random_filename = "".join(random.choices(string.ascii_uppercase + string.digits, k=10))
     assert watch_filter(Change.modified, f"/one/two/bucklemyshoe/{random_filename}")
     assert not watch_filter(Change.modified, "/one/two/bucklemyshoe/.DS_Store")
