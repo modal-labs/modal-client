319bff2935c6e67a4ab3e6562c9522e3870015d7
Erik Bernhardsson <mail@erikbern.com>
2023-04-24 15:02:59 -0400

    Fix the _strptime issue

--- modal/_ipython.py
+++ modal/_ipython.py
@@ -1,13 +1,21 @@
 # Copyright Modal Labs 2022
 import sys
+import warnings
+
+ipy_outstream = None
+try:
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        import ipykernel.iostream
+
+    ipy_outstream = ipykernel.iostream.OutStream
+except ImportError:
+    pass
 
 
 def is_notebook(stdout=None):
+    if ipy_outstream is None:
+        return False
     if stdout is None:
         stdout = sys.stdout
-    try:
-        import ipykernel.iostream
-
-        return isinstance(stdout, ipykernel.iostream.OutStream)
-    except ImportError:
-        return False
+    return isinstance(stdout, ipy_outstream)
