e92e8e428caca037b55743c9a412a9668cfc8e1a
Elias Freider <elias@freider.se>
2023-05-15 16:24:20 +0200

    Some more required changes to get integration tests cleaned from aio references

--- modal_base_images/client_mount.py
+++ modal_base_images/client_mount.py
@@ -1,14 +1,14 @@
 # Copyright Modal Labs 2022
 import asyncio
 
-from modal.mount import aio_create_client_mount, client_mount_name
+from modal.mount import create_client_mount, client_mount_name
 from modal_proto import api_pb2
 
 
 async def main(client=None):
-    mount = aio_create_client_mount()
+    mount = create_client_mount()
     name = client_mount_name()
-    await mount._deploy(client_mount_name(), api_pb2.DEPLOYMENT_NAMESPACE_GLOBAL, client=client)
+    await mount._deploy.aio(client_mount_name(), api_pb2.DEPLOYMENT_NAMESPACE_GLOBAL, client=client)
     print(f"✅ Deployed client mount {name} to global namespace.")
 
 
