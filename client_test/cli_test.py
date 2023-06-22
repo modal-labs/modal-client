bad491e45acadbdd57f191dacfd466a135877335
Elias Freider <elias@modal.com>
2023-06-22 12:11:31 +0200

    More

--- client_test/cli_test.py
+++ client_test/cli_test.py
@@ -380,7 +380,7 @@ def test_environment_flag(test_dir, servicer, command):
         ctx.add_response(
             "AppLookupObject",
             api_pb2.AppLookupObjectResponse(object_id="sv-123"),
-            request_filter=lambda req: req.app_name.startswith("vol"),
+            request_filter=lambda req: req.app_name == "volume_app",
         )
         _run(command + ["--env=staging", str(stub_file)])
 
