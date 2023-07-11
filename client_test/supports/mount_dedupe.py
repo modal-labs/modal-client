b6180b6a087e7d38acd3f29b92ceb4ce63323c53
Elias Freider <elias@modal.com>
2023-07-11 13:38:44 +0200

    Copyright

--- client_test/supports/mount_dedupe.py
+++ client_test/supports/mount_dedupe.py
@@ -1,3 +1,4 @@
+# Copyright Modal Labs 2023
 import os
 
 import modal
@@ -9,7 +10,9 @@ import pkg_a  # noqa
 
 if int(os.environ["USE_EXPLICIT"]):
     explicit_mounts1 = [Mount.from_local_python_packages("pkg_a")]
-    explicit_mounts2 = [Mount.from_local_python_packages("pkg_a")]  # same as above, but different instance
+    explicit_mounts2 = [
+        Mount.from_local_python_packages("pkg_a")
+    ]  # same as above, but different instance
 else:
     explicit_mounts1 = explicit_mounts2 = []
 
