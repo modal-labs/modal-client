e88067304aaa27fc944b33876ff5247f59f35226
Jonathon Belotti <jonathon@modal.com>
2023-07-19 21:59:05 +0000

    add Volume.iterdir, Volume.listdir; finish 'modal vol ls'

--- modal/volume.py
+++ modal/volume.py
@@ -1,10 +1,10 @@
 # Copyright Modal Labs 2023
 import asyncio
-from typing import Optional
+from typing import AsyncIterator, List, Optional
 
 from modal_proto import api_pb2
 from modal_utils.async_utils import asyncnullcontext, synchronize_api
-from modal_utils.grpc_utils import retry_transient_errors
+from modal_utils.grpc_utils import retry_transient_errors, unary_stream
 
 from ._resolver import Resolver
 from .object import _Handle, _Provider
@@ -47,6 +47,27 @@ class _VolumeHandle(_Handle, type_prefix="vo"):
         Reloading will fail if there are open files for the volume.
         """
         await self._do_reload()
+
+    async def iterdir(self, path: str) -> AsyncIterator[api_pb2.VolumeListFilesEntry]:
+        """Iterate over all files in a directory in the volume.
+
+        * Passing a directory path lists all files in the directory (names are relative to the directory)
+        * Passing a file path returns a list containing only that file's listing description
+        * Passing a glob path (including at least one * or ** sequence) returns all files matching that glob path (using absolute paths)
+        """
+        req = api_pb2.VolumeListFilesRequest(volume_id=self._object_id, path=path)
+        async for batch in unary_stream(self._client.stub.VolumeListFiles, req):
+            for entry in batch.entries:
+                yield entry
+
+    async def listdir(self, path: str) -> List[api_pb2.VolumeListFilesEntry]:
+        """List all files under a path prefix in the modal.Volume.
+
+        * Passing a directory path lists all files in the directory
+        * Passing a file path returns a list containing only that file's listing description
+        * Passing a glob path (including at least one * or ** sequence) returns all files matching that glob path (using absolute paths)
+        """
+        return [entry async for entry in self.iterdir(path)]
 
     async def _do_reload(self, lock=True):
         async with self._lock if lock else asyncnullcontext():
