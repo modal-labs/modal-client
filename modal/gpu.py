fdff339febf50556a46e759dd1e169023f73c523
Erik Bernhardsson <mail@erikbern.com>
2023-07-20 16:05:52 -0400

    Use the same GPU type for multi-GPU (#667)

--- modal/gpu.py
+++ modal/gpu.py
@@ -49,10 +49,7 @@ class A100(_GPUConfig):
         if memory not in allowed_memory_values:
             raise ValueError(f"A100s can only have memory values of {allowed_memory_values} => memory={memory}")
 
-        # Multi-GPU workloads require a different GPU type.
         gpu_type = api_pb2.GPU_TYPE_A100
-        if count > 1:
-            gpu_type = api_pb2.GPU_TYPE_A100_40GB_MANY
 
         if memory == 20:
             if count != 1:
