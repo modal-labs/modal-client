46820fa0352a3979ea2be423ebeed6968ffc7e84
Akshat Bubna <akshatb42@gmail.com>
2023-07-20 18:45:12 +0000

    mount and image tests

--- client_test/sandbox_test.py
+++ client_test/sandbox_test.py
@@ -1,19 +1,50 @@
 # Copyright Modal Labs 2022
 
+import hashlib
 import time
+from pathlib import Path
 
-from modal import Image, Stub
+from modal import Image, Mount, Stub
 
 stub = Stub()
 
 
 def test_spawn_sandbox(client, servicer):
     with stub.run(client=client) as app:
-        sb = app.spawn_sandbox("bash", "-c", "sleep 1 && echo hi", image=Image.debian_slim().pip_install("pandas"))
+        sb = app.spawn_sandbox("bash", "-c", "echo bye >&2 && sleep 1 && echo hi")
 
         t0 = time.time()
         sb.wait()
-        # We actually waited for the sandbox to finish.
+        # Test that we actually waited for the sandbox to finish.
         assert time.time() - t0 > 0.3
 
         assert sb.stdout.read() == "hi\n"
+        assert sb.stderr.read() == "bye\n"
+
+
+def test_sandbox_mount(client, servicer, tmpdir):
+    tmpdir.join("a.py").write(b"foo")
+
+    with stub.run(client=client) as app:
+        sb = app.spawn_sandbox(
+            "echo",
+            "hi",
+            mounts=[Mount.from_local_dir(Path(tmpdir), remote_path="/m")],
+        )
+        sb.wait()
+
+    sha = hashlib.sha256(b"foo").hexdigest()
+    assert servicer.files_sha2data[sha]["data"] == b"foo"
+
+
+def test_sandbox_image(client, servicer, tmpdir):
+    tmpdir.join("a.py").write(b"foo")
+
+    with stub.run(client=client) as app:
+        sb = app.spawn_sandbox("echo", "hi", image=Image.debian_slim().pip_install("foo", "bar", "potato"))
+        sb.wait()
+
+    idx = max(servicer.images.keys())
+    last_image = servicer.images[idx]
+
+    assert all(c in last_image.dockerfile_commands[-1] for c in ["foo", "bar", "potato"])
