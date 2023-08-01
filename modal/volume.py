41b33c077ffbb67b53ec4c566614766abb794da9
Jonathon Belotti <jonathon@modal.com>
2023-08-01 23:24:35 +0000

    unhide modal.Volume docstrings

--- modal/volume.py
+++ modal/volume.py
@@ -11,7 +11,7 @@ from .object import _Handle, _Provider
 
 
 class _VolumeHandle(_Handle, type_prefix="vo"):
-    """mdmd:hidden Handle to a `Volume` object."""
+    """Handle to a `Volume` object."""
 
     _lock: asyncio.Lock
 
@@ -23,14 +23,6 @@ class _VolumeHandle(_Handle, type_prefix="vo"):
         self._lock = asyncio.Lock()
 
     async def commit(self):
-        """Commit changes to the volume and fetch any other changes made to the volume by other tasks.
-
-        Committing always triggers a reload after saving changes.
-
-        If successful, the changes made are now persisted in durable storage and available for other functions/tasks.
-
-        Committing will fail if there are open files for the volume.
-        """
         async with self._lock:
             req = api_pb2.VolumeCommitRequest(volume_id=self.object_id)
             _ = await retry_transient_errors(self._client.stub.VolumeCommit, req)
@@ -38,35 +30,15 @@ class _VolumeHandle(_Handle, type_prefix="vo"):
             await self._do_reload(lock=False)
 
     async def reload(self):
-        """Make changes made by other tasks/functions visible in the volume.
-
-        Uncommitted changes to the volume, such as new or modified files, will be preserved during reload. Uncommitted
-        changes will shadow any changes made by other tasks - e.g. if you have an uncommitted modified a file that was
-        also updated by another task/function you will not see the changes made by the other function/task.
-
-        Reloading will fail if there are open files for the volume.
-        """
         await self._do_reload()
 
     async def iterdir(self, path: str) -> AsyncIterator[api_pb2.VolumeListFilesEntry]:
-        """Iterate over all files in a directory in the volume.
-
-        * Passing a directory path lists all files in the directory (names are relative to the directory)
-        * Passing a file path returns a list containing only that file's listing description
-        * Passing a glob path (including at least one * or ** sequence) returns all files matching that glob path (using absolute paths)
-        """
         req = api_pb2.VolumeListFilesRequest(volume_id=self._object_id, path=path)
         async for batch in unary_stream(self._client.stub.VolumeListFiles, req):
             for entry in batch.entries:
                 yield entry
 
     async def listdir(self, path: str) -> List[api_pb2.VolumeListFilesEntry]:
-        """List all files under a path prefix in the modal.Volume.
-
-        * Passing a directory path lists all files in the directory
-        * Passing a file path returns a list containing only that file's listing description
-        * Passing a glob path (including at least one * or ** sequence) returns all files matching that glob path (using absolute paths)
-        """
         return [entry async for entry in self.iterdir(path)]
 
     async def _do_reload(self, lock=True):
@@ -79,7 +51,7 @@ VolumeHandle = synchronize_api(_VolumeHandle)
 
 
 class _Volume(_Provider[_VolumeHandle]):
-    """mdmd:hidden A writeable volume that can be used to share files between one or more Modal functions.
+    """A writeable volume that can be used to share files between one or more Modal functions.
 
     The contents of a volume is exposed as a filesystem. You can use it to share data between different functions, or
     to persist durable state across several instances of the same function.
@@ -170,16 +142,44 @@ class _Volume(_Provider[_VolumeHandle]):
     # Methods on live handles
 
     async def commit(self):
+        """Commit changes to the volume and fetch any other changes made to the volume by other tasks.
+
+        Committing always triggers a reload after saving changes.
+
+        If successful, the changes made are now persisted in durable storage and available for other functions/tasks.
+
+        Committing will fail if there are open files for the volume.
+        """
         return await self._handle.commit()
 
     async def reload(self):
+        """Make changes made by other tasks/functions visible in the volume.
+
+        Uncommitted changes to the volume, such as new or modified files, will be preserved during reload. Uncommitted
+        changes will shadow any changes made by other tasks - e.g. if you have an uncommitted modified a file that was
+        also updated by another task/function you will not see the changes made by the other function/task.
+
+        Reloading will fail if there are open files for the volume.
+        """
         return await self._handle.reload()
 
     async def iterdir(self, path: str) -> AsyncIterator[api_pb2.VolumeListFilesEntry]:
+        """Iterate over all files in a directory in the volume.
+
+        * Passing a directory path lists all files in the directory (names are relative to the directory)
+        * Passing a file path returns a list containing only that file's listing description
+        * Passing a glob path (including at least one * or ** sequence) returns all files matching that glob path (using absolute paths)
+        """
         async for entry in self._handle.iterdir(path):
             yield entry
 
     async def listdir(self, path: str) -> List[api_pb2.VolumeListFilesEntry]:
+        """List all files under a path prefix in the modal.Volume.
+
+        * Passing a directory path lists all files in the directory
+        * Passing a file path returns a list containing only that file's listing description
+        * Passing a glob path (including at least one * or ** sequence) returns all files matching that glob path (using absolute paths)
+        """
         return self._handle.listdir(path)
 
 
