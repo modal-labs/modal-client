0fddadf9b6a503b1f5d5130c4dd305d11d743673
Erik Bernhardsson <mail@erikbern.com>
2023-08-22 09:02:59 -0400

    Move _new_hydrated from Handle to Provider

--- client_test/object_test.py
+++ client_test/object_test.py
@@ -30,18 +30,18 @@ async def test_use_object(client):
 
 
 def test_new_hydrated(client):
-    from modal.dict import _DictHandle
-    from modal.object import _Handle
-    from modal.queue import _QueueHandle
+    from modal.dict import _Dict
+    from modal.object import _Provider
+    from modal.queue import _Queue
 
-    assert isinstance(_DictHandle._new_hydrated("di-123", client, None), _DictHandle)
-    assert isinstance(_QueueHandle._new_hydrated("qu-123", client, None), _QueueHandle)
-
-    with pytest.raises(InvalidError):
-        _QueueHandle._new_hydrated("di-123", client, None)  # Wrong prefix for type
-
-    assert isinstance(_Handle._new_hydrated("qu-123", client, None), _QueueHandle)
-    assert isinstance(_Handle._new_hydrated("di-123", client, None), _DictHandle)
+    assert isinstance(_Dict._new_hydrated("di-123", client, None), _Dict)
+    assert isinstance(_Queue._new_hydrated("qu-123", client, None), _Queue)
 
     with pytest.raises(InvalidError):
-        _Handle._new_hydrated("xy-123", client, None)
+        _Queue._new_hydrated("di-123", client, None)  # Wrong prefix for type
+
+    assert isinstance(_Provider._new_hydrated("qu-123", client, None), _Queue)
+    assert isinstance(_Provider._new_hydrated("di-123", client, None), _Dict)
+
+    with pytest.raises(InvalidError):
+        _Provider._new_hydrated("xy-123", client, None)
