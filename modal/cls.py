4f7a971f4b7b148f0a2decef6d317443a3fdec69
Erik Bernhardsson <mail@erikbern.com>
2023-08-29 19:01:08 -0400

    Make .remote non-async (#804)
    * Make .remote non-async
    
    * Actually we have to keep it async to not break .remote.aio

--- modal/cls.py
+++ modal/cls.py
@@ -4,7 +4,6 @@ from typing import Dict, Type, TypeVar
 
 from modal_utils.async_utils import synchronize_api
 
-from ._resolver import Resolver
 from .functions import PartialFunction, _Function, _PartialFunction
 
 T = TypeVar("T")
@@ -45,11 +44,7 @@ def make_remote_cls_constructors(
         new_functions: Dict[str, _Function] = {}
 
         for k, v in partial_functions.items():
-            base_function: _Function = functions[k]
-            new_function: _Function = base_function.from_parametrized(args, kwargs)
-            resolver = Resolver()
-            await resolver.load(new_function)
-            new_functions[k] = new_function
+            new_functions[k] = functions[k].from_parametrized(args, kwargs)
 
         obj = cls()
         _PartialFunction.initialize_obj(obj, new_functions)
