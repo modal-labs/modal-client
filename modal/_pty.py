813544ba7c0f857d077f677cb127c1195520d44d
Eric Zhang <ekzhang1@gmail.com>
2023-08-29 14:45:45 +0000

    Improve modal shell performance for backlogged I/O

--- modal/_pty.py
+++ modal/_pty.py
@@ -119,10 +119,10 @@ def run_in_pty(fn, queue, pty_info: api_pb2.PTYInfo):
         def _read():
             while True:
                 try:
-                    char = queue.get()
-                    if char is None:
+                    data_segments = queue.get_many(1024)
+                    if data_segments is None:
                         return
-                    writer.write(char)
+                    writer.write(b"".join(data_segments))
                     writer.flush()
                 except asyncio.CancelledError:
                     return
@@ -165,7 +165,7 @@ async def write_stdin_to_pty_stream(queue: _Queue):
     set_nonblocking(sys.stdin.fileno())
 
     @asyncify
-    def _read_char() -> Optional[bytes]:
+    def _read_stdin() -> Optional[bytes]:
         nonlocal quit_pipe_read
         # TODO: Windows support.
         (readable, _, _) = select.select([sys.stdin.buffer, quit_pipe_read], [], [])
@@ -175,10 +175,10 @@ async def write_stdin_to_pty_stream(queue: _Queue):
 
     async def _write():
         while True:
-            char = await _read_char()
-            if char is None:
+            data = await _read_stdin()
+            if data is None:
                 return
-            await queue.put(char)
+            await queue.put(data)
 
     async with TaskContext(grace=0.1) as tc:
         write_task = tc.create_task(_write())
