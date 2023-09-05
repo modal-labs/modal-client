bc3a018228d859df52cc6c66cdbcea93d880cf5a
Eric Zhang <ekzhang1@gmail.com>
2023-09-05 15:11:56 -0400

    Only setup Rich tracebacks when running CLI (#834)
    This means that `import modal`, as well as Modal in Jupyter notebooks
    and `python` directly with `app.run()`, will not register global
    traceback handlers.

--- modal/__main__.py
+++ modal/__main__.py
@@ -1,10 +1,13 @@
 # Copyright Modal Labs 2022
+from ._traceback import setup_rich_traceback
 from .cli.entry_point import entrypoint_cli
 
 
 def main():
+    # Setup rich tracebacks, but only on user's end, when using the Modal CLI.
+    setup_rich_traceback()
     entrypoint_cli()
 
 
 if __name__ == "__main__":
-    entrypoint_cli()
+    main()
