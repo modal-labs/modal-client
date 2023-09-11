178fac9d975e0a6ad7e83db0668ede8c853e25a9
Erik Bernhardsson <mail@erikbern.com>
2023-09-11 09:46:50 -0400

    use match=

--- client_test/decorator_test.py
+++ client_test/decorator_test.py
@@ -8,51 +8,43 @@ from modal.exception import InvalidError
 def test_local_entrypoint_forgot_parentheses():
     stub = Stub()
 
-    with pytest.raises(InvalidError) as excinfo:
+    with pytest.raises(InvalidError, match="local_entrypoint()") as excinfo:
 
         @stub.local_entrypoint  # type: ignore
         def f():
             pass
 
-    assert "local_entrypoint()" in str(excinfo.value)
-
 
 def test_function_forgot_parentheses():
     stub = Stub()
 
-    with pytest.raises(InvalidError) as excinfo:
+    with pytest.raises(InvalidError, match="function()") as excinfo:
 
         @stub.function  # type: ignore
         def f():
             pass
 
-    assert "function()" in str(excinfo.value)
-
 
 def test_cls_forgot_parentheses():
     stub = Stub()
 
-    with pytest.raises(InvalidError) as excinfo:
+    with pytest.raises(InvalidError, match="cls()") as excinfo:
 
         @stub.cls  # type: ignore
         class XYZ:
             pass
 
-    assert "cls()" in str(excinfo.value)
-
 
 def test_method_forgot_parentheses():
     stub = Stub()
 
-    with pytest.raises(InvalidError) as excinfo:
+    with pytest.raises(InvalidError, match="method()") as excinfo:
 
         @stub.cls()
         class XYZ:
             @method  # type: ignore
             def f(self):
                 pass
-
-    assert "method()" in str(excinfo.value)
 
 
 def test_invalid_web_decorator_usage():
