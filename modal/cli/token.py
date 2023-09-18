1716ab365b53a7a65e99da4e332fbd0a73e6decd
Erik Bernhardsson <mail@erikbern.com>
2023-09-18 11:03:09 -0400

    Minor update to token flow text

--- modal/cli/token.py
+++ modal/cli/token.py
@@ -46,14 +46,14 @@ def set(
 def new(profile: Optional[str] = profile_option, no_verify: bool = False, source: Optional[str] = None):
     server_url = config.get("server_url", profile=profile)
 
+    console = Console()
+
     with Client.unauthenticated_client(server_url) as client:
         token_flow_id, web_url = client.start_token_flow(source)
-        console = Console()
-        with console.status("Waiting for authentication in the web browser...", spinner="dots"):
+        with console.status("Waiting for authentication in the web browser", spinner="dots"):
             # Open the web url in the browser
-            console.print("Launching login page in your browser window...")
             if webbrowser.open_new_tab(web_url):
-                console.print("If this is not showing up, please copy this URL into your web browser manually:")
+                console.print("If the web browser didn't open, please copy this URL into your web browser manually:")
             else:
                 console.print(
                     "[red]Was not able to launch web browser[/red]"
@@ -61,13 +61,16 @@ def new(profile: Optional[str] = profile_option, no_verify: bool = False, source
                 )
             console.print(f"\n[link={web_url}]{web_url}[/link]\n")
 
-        token_id, token_secret = client.finish_token_flow(token_flow_id)
-        console.print("[green]Success![/green]")
+        with console.status("Waiting for token flow to complete...", spinner="dots"):
+            token_id, token_secret = client.finish_token_flow(token_flow_id)
+
+        console.print("[green]Web authentication finished successfully![/green]")
 
     if not no_verify:
-        rich.print(f"Verifying token against [blue]{server_url}[/blue]")
-        Client.verify(server_url, (token_id, token_secret))
-        rich.print("[green]Token verified successfully[/green]")
+        with console.status(f"Verifying token against [blue]{server_url}[/blue]", spinner="dots"):
+            Client.verify(server_url, (token_id, token_secret))
+            console.print("[green]Token verified successfully![/green]")
 
-    _store_user_config({"token_id": token_id, "token_secret": token_secret}, profile=profile)
-    rich.print(f"Token written to {user_config_path}")
+    with console.status("Storing token", spinner="dots"):
+        _store_user_config({"token_id": token_id, "token_secret": token_secret}, profile=profile)
+        console.print(f"[green]Token written to [white]{user_config_path}[/white] successfully![/green]")
