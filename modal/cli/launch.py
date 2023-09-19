a4c1bea541d5e5e240d0ddc65d6498a00f262cfa
Eric Zhang <ekzhang1@gmail.com>
2023-09-19 17:21:10 -0400

    Rename to "modal launch" and add vscode (#891)

--- modal/cli/launch.py
+++ modal/cli/launch.py
@@ -5,20 +5,19 @@ from pathlib import Path
 
 from typer import Typer
 
-jupyter_cli = Typer(
-    name="jupyter",
+launch_cli = Typer(
+    name="launch",
     no_args_is_help=True,
     help="""
-    [Preview] Open a serverless Jupyter instance on Modal.
+    [Preview] Open a serverless app instance on Modal.
 
     This command is in preview and may change in the future.
     """,
 )
 
 
-@jupyter_cli.command(name="lab", help="Start Jupyter Lab on Modal.")
-def lab():
-    contents = (Path(__file__).parent / "programs" / "jupyterlab.py").read_text()
+def _launch_program(name: str) -> None:
+    contents = (Path(__file__).parent / "programs" / name).read_text()
 
     # TODO: This is a big hack and can break for unexpected $PATH reasons. Make an actual code path
     # for correctly setting up and running a program in the CLI.
@@ -26,3 +25,13 @@ def lab():
         f = Path(tmpdir) / "_main.py"
         f.write_text(contents)
         subprocess.run(["modal", "run", f])
+
+
+@launch_cli.command(name="jupyter", help="Start Jupyter Lab on Modal.")
+def jupyter():
+    _launch_program("jupyterlab.py")
+
+
+@launch_cli.command(name="vscode", help="Start VS Code on Modal.")
+def vscode():
+    _launch_program("vscode.py")
