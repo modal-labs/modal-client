d27382631ad0f65142515f41cc03928e015fb737
Akshat Bubna <akshatb42@gmail.com>
2023-09-25 15:59:36 +0000

    image_test

--- client_test/image_test.py
+++ client_test/image_test.py
@@ -468,29 +468,41 @@ def test_workdir(servicer, client):
 
 cls_stub = Stub()
 
+VARIABLE_5 = 1
+VARIABLE_6 = 1
+
 
 @cls_stub.cls(image=Image.debian_slim().pip_install("pandas"), secrets=[Secret.from_dict({"xyz": "123"})])
 class Foo:
     def __enter__(self):
-        print("foo!")
+        global VARIABLE_5
+
+        print("foo!", VARIABLE_5)
 
     @method()
     def f(self):
-        print("bar!")
+        global VARIABLE_6
+
+        print("bar!", VARIABLE_6)
 
 
 def test_image_auto_snapshot(client, servicer):
     with cls_stub.run(client=client):
         idx = max(*servicer.images.keys())
         layers = get_image_layers(f"im-{idx}", servicer)
-        print(layers)
-        assert "foo!" in layers[0].build_function_def
-        assert "bar!" not in layers[0].build_function_def
-        assert "Secret.from_dict([xyz])" in layers[0].build_function_def
-    #     # globals is none when no globals are referenced
-    #     assert layers[0].build_function_globals == b""
 
-    # function_id = servicer.image_build_function_ids[2]
-    # assert function_id
-    # assert servicer.app_functions[function_id].function_name == "run_f"
-    # assert len(servicer.app_functions[function_id].secret_ids) == 1
+        assert "foo!" in layers[0].build_function_def
+        assert "Secret.from_dict([xyz])" in layers[0].build_function_def
+        assert any("pip install pandas" in cmd for cmd in layers[1].dockerfile_commands)
+
+        globals = layers[0].build_function_globals
+        assert b"VARIABLE_5" in globals
+
+        # Globals and def for the main function should not affect __enter__.
+        assert "bar!" not in layers[0].build_function_def
+        assert b"VARIABLE_6" not in globals
+
+    function_id = servicer.image_build_function_ids[idx]
+    assert function_id
+    assert servicer.app_functions[function_id].function_name == "Foo.__enter__"
+    assert len(servicer.app_functions[function_id].secret_ids) == 1
