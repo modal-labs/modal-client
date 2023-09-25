238e6b71c600b3c0e04e27bd1f951ed4b0618d8f
gongy <richard@modal.com>
2023-09-25 23:16:55 +0000

    add experimental boost flag to function
    proto update

--- modal/functions.py
+++ modal/functions.py
@@ -584,6 +584,7 @@ class _Function(_Object, type_prefix="fu"):
         keep_warm: Optional[int] = None,
         interactive: bool = False,
         cloud: Optional[str] = None,
+        _experimental_boost: bool = False,
         is_builder_function: bool = False,
         cls: Optional[type] = None,
         is_auto_snapshot: bool = False,
@@ -849,6 +850,7 @@ class _Function(_Object, type_prefix="fu"):
                 block_network=block_network,
                 max_inputs=max_inputs,
                 s3_mounts=s3_mounts_to_proto(s3_mounts),
+                _experimental_boost=_experimental_boost,
             )
             request = api_pb2.FunctionCreateRequest(
                 app_id=resolver.app_id,
