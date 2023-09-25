84f0f44317371163a81b8fd83850ac7590d9b414
Staffan Gimåker <gimaker@users.noreply.github.com>
2023-09-25 09:04:15 +0200

    [MOD-1518] Add CLI for uploading files to volumes (#907)

--- client_test/volume_test.py
+++ client_test/volume_test.py
@@ -1,6 +1,6 @@
 # Copyright Modal Labs 2023
-
 import pytest
+from unittest import mock
 
 import modal
 from modal.exception import InvalidError
@@ -71,22 +71,20 @@ def test_volume_commit(client, servicer):
 
 
 @pytest.mark.asyncio
-async def test_volume_get(servicer, client):
+async def test_volume_get(servicer, client, tmp_path):
     stub = modal.Stub()
     vol = modal.Volume.persisted("my-vol")
     stub.vol = vol
     await vol._deploy.aio("my-vol", client=client)
     assert await modal.Volume._exists.aio("my-vol", client=client)  # type: ignore
-
-    file_path = b"foo.txt"
-    file_contents = b"hello world"
-
-    with stub.run(client=client):
-        object_id = stub.vol.object_id
-    # TODO: A PUT implementation for volumes will make this test simpler.
-    servicer.volume_files[object_id][file_path] = file_contents
-
     vol = await modal.Volume.lookup.aio("my-vol", client=client)  # type: ignore
+
+    file_contents = b"hello world"
+    file_path = b"foo.txt"
+    local_file_path = tmp_path / file_path.decode("utf-8")
+    local_file_path.write_bytes(file_contents)
+    await vol._add_local_file.aio(local_file_path, file_path.decode("utf-8"))
+
     data = b""
     for chunk in vol.read_file(file_path):
         data += chunk
@@ -132,3 +130,67 @@ def test_redeploy(servicer, client):
     # Should be unique and different
     assert len(set(app3_ids)) == 3
     assert set(app1_ids) & set(app3_ids) == set()
+
+
+@pytest.mark.asyncio
+async def test_volume_add_local_file(servicer, client, tmp_path):
+    stub = modal.Stub()
+    stub.vol = modal.Volume.new()
+    local_file_path = tmp_path / "some_file"
+    local_file_path.write_text("hello world")
+
+    with stub.run(client=client):
+        stub.vol._add_local_file(local_file_path)
+        stub.vol._add_local_file(local_file_path.as_posix(), remote_path="/foo/other_destination")
+        object_id = stub.vol.object_id
+
+    assert servicer.volume_files[object_id].keys() == {
+        "/some_file",
+        "/foo/other_destination",
+    }
+    assert servicer.volume_files[object_id]["/some_file"].data == b"hello world"
+    assert servicer.volume_files[object_id]["/foo/other_destination"].data == b"hello world"
+
+
+@pytest.mark.asyncio
+async def test_volume_add_local_dir(client, tmp_path, servicer):
+    stub = modal.Stub()
+    stub.vol = modal.Volume.new()
+    local_dir = tmp_path / "some_dir"
+    local_dir.mkdir()
+    (local_dir / "smol").write_text("###")
+
+    subdir = local_dir / "subdir"
+    subdir.mkdir()
+    (subdir / "other").write_text("####")
+
+    with stub.run(client=client):
+        stub.vol._add_local_dir(local_dir)
+        object_id = stub.vol.object_id
+
+    assert servicer.volume_files[object_id].keys() == {
+        "/some_dir/smol",
+        "/some_dir/subdir/other",
+    }
+    assert servicer.volume_files[object_id]["/some_dir/smol"].data == b"###"
+    assert servicer.volume_files[object_id]["/some_dir/subdir/other"].data == b"####"
+
+
+@pytest.mark.asyncio
+async def test_volume_put_large_file(client, tmp_path, servicer, blob_server, *args):
+    with mock.patch("modal._blob_utils.LARGE_FILE_LIMIT", 10):
+        stub = modal.Stub()
+        stub.vol = modal.Volume.new()
+        local_file_path = tmp_path / "bigfile"
+        local_file_path.write_text("hello world, this is a lot of text")
+
+        async with stub.run(client=client):
+            await stub.vol._add_local_file.aio(local_file_path)
+            object_id = stub.vol.object_id
+
+        assert servicer.volume_files[object_id].keys() == {"/bigfile"}
+        assert servicer.volume_files[object_id]["/bigfile"].data == b""
+        assert servicer.volume_files[object_id]["/bigfile"].data_blob_id == "bl-1"
+
+        _, blobs = blob_server
+        assert blobs["bl-1"] == b"hello world, this is a lot of text"
