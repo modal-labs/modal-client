160b84bad26480e900e2eb3517cd440cb3c280fc
Erik Bernhardsson <mail@erikbern.com>
2023-09-26 10:11:57 -0400

    Remove `_App._tag_to_object` (#922)
    * remove tag_to_object for deprecated methods
    
    * remove _tag_to_object from _create_all_objects
    
    * remove _tag_to_object from _get_object
    
    * move _uncreate_all_objects to the stub
    
    * remove _tag_to_object

--- modal/stub.py
+++ modal/stub.py
@@ -205,9 +205,8 @@ class _Stub:
             # If this is inside a container, and some module is loaded lazily, then a function may be
             # defined later than the container initialization. If this happens then lets hydrate the
             # function at this point
-            other_obj = self._app._get_object(tag)
-            if other_obj is not None:
-                obj._hydrate_from_other(other_obj)
+            if self._app._has_object(tag):
+                self._app._hydrate_object(obj, tag)
 
         self._blueprint[tag] = obj
 
@@ -240,6 +239,11 @@ class _Stub:
     def get_objects(self) -> List[Tuple[str, _Object]]:
         """Used by the container app to initialize objects."""
         return list(self._blueprint.items())
+
+    def _uncreate_all_objects(self):
+        # TODO(erikbern): this doesn't unhydrate objects that aren't tagged
+        for obj in self._blueprint.values():
+            obj._unhydrate()
 
     @typechecked
     def is_inside(self, image: Optional[_Image] = None) -> bool:
