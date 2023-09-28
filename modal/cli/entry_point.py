fedeb777acc2332ec4139a2f32755e3827790c29
Erik Bernhardsson <mail@erikbern.com>
2023-09-28 12:30:24 -0400

    Handle permission errors too

--- modal/cli/entry_point.py
+++ modal/cli/entry_point.py
@@ -45,20 +45,32 @@ def modal(
     pass
 
 
-def setup():
-    # Sanity check the path
+def check_path():
+    """Checks whether the `modal` executable is on the path and usable."""
+    console = Console()
+    url = "https://modal.com/docs/guide/troubleshooting#command-not-found-errors"
     try:
         subprocess.run(["modal", "--help"], capture_output=True)
         # TODO(erikbern): check returncode?
+        return
     except FileNotFoundError:
-        console = Console()
-        url = "https://modal.com/docs/guide/troubleshooting#command-not-found-errors"
         console.print(
             "[red]The `[white]modal[/white]` command was not found on your path!\n"
-            "You may need to add it to your path or use `[white]python -m modal[/white]` as a workaround.\n"
-            "See more information here:[/red]\n\n"
-            f"[link={url}]{url}[/link]\n"
+            "You may need to add it to your path or use `[white]python -m modal[/white]` as a workaround.[/red]\n"
         )
+    except PermissionError:
+        console.print(
+            "[red]The `[white]modal[/white]` command is not executable!\n"
+            "You may need to give it permissions or use `[white]python -m modal[/white]` as a workaround.[/red]\n"
+        )
+    console.print(
+        "See more information here:\n\n"
+        f"[link={url}]{url}[/link]\n"
+    )
+
+
+def setup():
+    check_path()
 
     # Fetch a new token (same as `modal token new` but redirect to /home once finishes)
     _new_token(next_url="/home")
