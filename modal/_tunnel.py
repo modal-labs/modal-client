836d6e308dcf0e8c0715d0ea54a9046747778426
Eric Zhang <ekzhang1@gmail.com>
2023-10-20 16:28:48 -0400

    Add client test for tunnels and mock the RPCs (#993)
    * Add client test for tunnels and mock the RPCs
    
    * Add client_type annotation
    
    * Skip tunnel tests on Windows, non-Unix

--- modal/_tunnel.py
+++ modal/_tunnel.py
@@ -1,9 +1,9 @@
 # Copyright Modal Labs 2023
 """Client for Modal relay servers, allowing users to expose TLS."""
 
-import contextlib
+from contextlib import asynccontextmanager
 from dataclasses import dataclass
-from typing import AsyncIterator
+from typing import AsyncIterator, Optional
 
 from grpclib import GRPCError, Status
 
@@ -11,7 +11,6 @@ from modal_proto import api_pb2
 from modal_utils.async_utils import synchronize_api
 
 from .client import _Client
-from .config import config
 from .exception import InvalidError, RemoteError
 
 
@@ -30,8 +29,8 @@ class Tunnel:
         return f"https://{self.host}"
 
 
-@contextlib.asynccontextmanager
-async def _forward(port: int) -> AsyncIterator[Tunnel]:
+@asynccontextmanager
+async def _forward(port: int, client: Optional[_Client] = None) -> AsyncIterator[Tunnel]:
     """Expose a port publicly from inside a running Modal container, with TLS.
 
     This is an EXPERIMENTAL API and may change in the future.
@@ -61,10 +60,11 @@ async def _forward(port: int) -> AsyncIterator[Tunnel]:
     if not isinstance(port, int) or port < 1 or port > 65535:
         raise InvalidError(f"Invalid port number {port}")
 
-    if not config.get("task_id"):
-        raise InvalidError("Forwarding ports only works inside a Modal container")
+    if not client:
+        client = await _Client.from_env()
 
-    client = await _Client.from_env()
+    if client.client_type != api_pb2.CLIENT_TYPE_CONTAINER:
+        raise InvalidError("Forwarding ports only works inside a Modal container")
 
     try:
         response = await client.stub.TunnelStart(api_pb2.TunnelStartRequest(port=port))
