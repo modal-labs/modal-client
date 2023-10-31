b768f4eb003bfd59fad2a531da50a4acb614fabc
Akshat Bubna <akshatb42@gmail.com>
2023-10-31 19:59:13 +0000

    add debug logs for RPCs

--- modal_utils/logger.py
+++ modal_utils/logger.py
@@ -1,5 +1,14 @@
 # Copyright Modal Labs 2022
 import logging
+import os
 
 # TODO: set this from env.
 logger = logging.getLogger("modal-utils")
+
+ch = logging.StreamHandler()
+
+log_level_numeric = logging.getLevelName(os.environ.get("MODAL_LOGLEVEL", "WARNING").upper())
+logger.setLevel(log_level_numeric)
+ch.setLevel(log_level_numeric)
+ch.setFormatter(logging.Formatter("%(asctime)s %(message)s", datefmt="%Y-%m-%dT%H:%M:%S%z"))
+logger.addHandler(ch)
