e600c8883b40343cc0f57fa8b4ba8174da438611
Akshat Bubna <akshatb42@gmail.com>
2023-11-04 14:15:44 +0000

    Revert "bump micro Python versions (#1036)"
    This reverts commit 77f36578da3be41d750eb61b7862f9777631d044.

--- test/image_test.py
+++ test/image_test.py
@@ -14,8 +14,8 @@ from modal_proto import api_pb2
 
 
 def test_python_version():
-    assert _dockerhub_python_version("3.9.1") == "3.9.18"
-    assert _dockerhub_python_version("3.9") == "3.9.18"
+    assert _dockerhub_python_version("3.9.1") == "3.9.15"
+    assert _dockerhub_python_version("3.9") == "3.9.15"
     v = _dockerhub_python_version().split(".")
     assert len(v) == 3
     assert (int(v[0]), int(v[1])) == sys.version_info[:2]
