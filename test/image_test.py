27dc83bc24626c1e01aef3fda94eeed0095626d4
Akshat Bubna <akshatb42@gmail.com>
2023-11-04 14:37:18 +0000

    fix test

--- test/image_test.py
+++ test/image_test.py
@@ -14,7 +14,7 @@ from modal_proto import api_pb2
 
 
 def test_python_version():
-    assert _dockerhub_python_version("3.9.1") == "3.9.15"
+    assert _dockerhub_python_version("3.9.1") == "3.9.1"
     assert _dockerhub_python_version("3.9") == "3.9.15"
     v = _dockerhub_python_version().split(".")
     assert len(v) == 3
