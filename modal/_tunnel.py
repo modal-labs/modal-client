f1e59e5e085ff4b60a747f83def8f13d85bd4f36
Eric Zhang <ekzhang1@gmail.com>
2023-11-24 14:26:35 -0500

    Add client support for unencrypted tunnels (#1064)
    * Add client support for unencrypted tunnels
    
    * Fix typo
    
    * Fix another typo
    
    * It really is Thanksgiving isn't it
    
    * More client API changes to Tunnel

--- modal/_tunnel.py
+++ modal/_tunnel.py
@@ -3,7 +3,7 @@
 
 from contextlib import asynccontextmanager
 from dataclasses import dataclass
-from typing import AsyncIterator, Optional
+from typing import AsyncIterator, Optional, Tuple
 
 from grpclib import GRPCError, Status
 
@@ -22,16 +22,40 @@ class Tunnel:
     """
 
     host: str
+    port: int
+    unencrypted_host: str
+    unencrypted_port: int
 
     @property
     def url(self) -> str:
         """Get the public HTTPS URL of the forwarded port."""
-        return f"https://{self.host}"
+        value = f"https://{self.host}"
+        if self.port != 443:
+            value += f":{self.port}"
+        return value
+
+    @property
+    def tls_socket(self) -> Tuple[str, int]:
+        """Get the public TLS socket as a (host, port) tuple."""
+        return (self.host, self.port)
+
+    @property
+    def tcp_socket(self) -> Tuple[str, int]:
+        """Get the public TCP socket as a (host, port) tuple."""
+        if not self.unencrypted_host:
+            raise InvalidError(
+                "This tunnel is not configured for unencrypted TCP. Please use `forward(..., unencrypted=True)`."
+            )
+        return (self.unencrypted_host, self.unencrypted_port)
 
 
 @asynccontextmanager
-async def _forward(port: int, client: Optional[_Client] = None) -> AsyncIterator[Tunnel]:
+async def _forward(port: int, *, unencrypted: bool = False, client: Optional[_Client] = None) -> AsyncIterator[Tunnel]:
     """Expose a port publicly from inside a running Modal container, with TLS.
+
+    If `unencrypted` is set, this also exposes the TCP socket without encryption on a random port
+    number. This can be used to SSH into a container. Note that it is on the public Internet, so
+    make sure you are using a secure protocol over TCP.
 
     This is an EXPERIMENTAL API and may change in the future.
 
@@ -39,9 +63,11 @@ async def _forward(port: int, client: Optional[_Client] = None) -> AsyncIterator
 
     ```python
     from flask import Flask
+    from modal import Image, Stub, forward
 
-
+    stub = Stub(image=Image.debian_slim().pip_install("Flask"))
     app = Flask(__name__)
+
 
     @app.route("/")
     def hello_world():
@@ -52,11 +78,56 @@ async def _forward(port: int, client: Optional[_Client] = None) -> AsyncIterator
     def run_app():
         # Start a web server inside the container at port 8000. `modal.forward(8000)` lets us
         # expose that port to the world at a random HTTPS URL.
-        with modal.forward(8000) as tunnel:
+        with forward(8000) as tunnel:
             print("Server listening at", tunnel.url)
             app.run("0.0.0.0", 8000)
+
+        # When the context manager exits, the port is no longer exposed.
     ```
+
+    **Raw TCP usage:**
+
+    ```python
+    import socket
+    import threading
+    from modal import Stub, forward
+
+
+    def run_echo_server(port: int):
+        \"""Run a TCP echo server listening on the given port.\"""
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.bind(("0.0.0.0", port))
+        sock.listen(1)
+
+        while True:
+            conn, addr = sock.accept()
+            print("Connection from:", addr)
+
+            # Start a new thread to handle the connection
+            def handle(conn):
+                with conn:
+                    while True:
+                        data = conn.recv(1024)
+                        if not data:
+                            break
+                        conn.sendall(data)
+
+            threading.Thread(target=handle, args=(conn,)).start()
+
+
+    stub = Stub()
+
+
+    @stub.function()
+    def tcp_tunnel():
+        # This exposes port 8000 to public Internet traffic over TCP.
+        with forward(8000, unencrypted=True) as tunnel:
+            # You can connect to this TCP socket from outside the container, for example, using `nc`:
+            #  nc <HOST> <PORT>
+            print("TCP tunnel listening at:", tunnel.tcp_socket)
+            run_echo_server(8000)
     """
+
     if not isinstance(port, int) or port < 1 or port > 65535:
         raise InvalidError(f"Invalid port number {port}")
 
@@ -67,7 +138,7 @@ async def _forward(port: int, client: Optional[_Client] = None) -> AsyncIterator
         raise InvalidError("Forwarding ports only works inside a Modal container")
 
     try:
-        response = await client.stub.TunnelStart(api_pb2.TunnelStartRequest(port=port))
+        response = await client.stub.TunnelStart(api_pb2.TunnelStartRequest(port=port, unencrypted=unencrypted))
     except GRPCError as exc:
         if exc.status == Status.ALREADY_EXISTS:
             raise InvalidError(f"Port {port} is already forwarded")
@@ -77,7 +148,8 @@ async def _forward(port: int, client: Optional[_Client] = None) -> AsyncIterator
             raise
 
     try:
-        yield Tunnel(response.host)
+        port = response.port or 443  # reverse compatibility
+        yield Tunnel(response.host, response.port, response.unencrypted_host, response.unencrypted_port)
     finally:
         await client.stub.TunnelStop(api_pb2.TunnelStopRequest(port=port))
 
