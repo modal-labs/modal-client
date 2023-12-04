8e72f41e5e4a50d4e6af9dd9a4dd9d119860b8cc
Erik Bernhardsson <mail@erikbern.com>
2023-12-04 12:36:28 -0500

    Tell people to email support@modal.com if the installation is bad

--- modal/__init__.py
+++ modal/__init__.py
@@ -1,24 +1,41 @@
 # Copyright Modal Labs 2022
 from modal_version import __version__
 
-from ._tunnel import Tunnel, forward
-from .app import container_app, is_local
-from .client import Client
-from .cls import Cls
-from .dict import Dict
-from .exception import Error
-from .functions import Function, asgi_app, current_function_call_id, current_input_id, method, web_endpoint, wsgi_app
-from .image import Image
-from .mount import Mount, create_package_mounts
-from .network_file_system import NetworkFileSystem
-from .proxy import Proxy
-from .queue import Queue
-from .retries import Retries
-from .schedule import Cron, Period
-from .secret import Secret
-from .shared_volume import SharedVolume
-from .stub import Stub
-from .volume import Volume
+try:
+    from ._tunnel import Tunnel, forward
+    from .app import container_app, is_local
+    from .client import Client
+    from .cls import Cls
+    from .dict import Dict
+    from .exception import Error
+    from .functions import (
+        Function,
+        asgi_app,
+        current_function_call_id,
+        current_input_id,
+        method,
+        web_endpoint,
+        wsgi_app,
+    )
+    from .image import Image
+    from .mount import Mount, create_package_mounts
+    from .network_file_system import NetworkFileSystem
+    from .proxy import Proxy
+    from .queue import Queue
+    from .retries import Retries
+    from .schedule import Cron, Period
+    from .secret import Secret
+    from .shared_volume import SharedVolume
+    from .stub import Stub
+    from .volume import Volume
+except Exception:
+    print()
+    print("#" * 80)
+    print("#" + "Something with the Modal installation seems broken.".center(78) + "#")
+    print("#" + "Please email support@modal.com and we will try to help!".center(78) + "#")
+    print("#" * 80)
+    print()
+    raise
 
 __all__ = [
     "__version__",
