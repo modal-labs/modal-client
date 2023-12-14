596d23eab00b4031a5f3dd486c66f5b786133699
Jonathon Belotti <jonathon@modal.com>
2023-12-14 17:20:09 +0000

    Revert "unskip in windows"
    This reverts commit 50d197614effeb097de09e5e1723af07c3cce3ec.

--- test/queue_test.py
+++ test/queue_test.py
@@ -5,6 +5,8 @@ import time
 
 from modal import Queue, Stub
 
+from .supports.skip import skip_windows
+
 
 def test_queue(servicer, client):
     stub = Stub()
@@ -20,6 +22,7 @@ def test_queue(servicer, client):
         assert stub.q.len() == 0
 
 
+@skip_windows("TODO(Jonathon): figure out why timeouts don't occur on Windows.")
 @pytest.mark.parametrize(
     ["put_timeout_secs", "min_queue_full_exc_count", "max_queue_full_exc_count"],
     [
