10e101aefb53600eb086336482d0faf0d0cdcf5c
Nathan Wang <nathan.r.wang@gmail.com>
2024-01-11 22:26:17 +0000

    proof-of-concept stdout/stderr streaming

--- modal/cli/container.py
+++ modal/cli/container.py
@@ -3,12 +3,14 @@
 from typing import List, Union
 
 import typer
+from grpclib.exceptions import GRPCError, StreamTerminatedError
 from rich.text import Text
 
 from modal.cli.utils import display_table, timestamp_to_local
 from modal.client import _Client
 from modal_proto import api_pb2
 from modal_utils.async_utils import synchronizer
+from modal_utils.grpc_utils import RETRYABLE_GRPC_STATUS_CODES, unary_stream
 
 container_cli = typer.Typer(name="container", help="Manage running containers.", no_args_is_help=True)
 
@@ -43,4 +45,40 @@ async def exec(task_id: str, command: str):
     res: api_pb2.ContainerExecResponse = await client.stub.ContainerExec(
         api_pb2.ContainerExecRequest(task_id=task_id, command=command)
     )
-    print(res)
+    if res.exec_id is None:
+        # todo(nathan): proper error message?
+        print("failed to execute command, unclear why")
+        return
+
+    last_entry_id = ""
+
+    async def _get_output():
+        nonlocal last_entry_id
+
+        req = api_pb2.ContainerExecGetOutputRequest(
+            exec_id=res.exec_id,
+            timeout=55,
+            last_entry_id=last_entry_id,
+        )
+        async for message in unary_stream(client.stub.ContainerExecGetOutput, req):
+            print(f"[{message.file_descriptor}] {message.message}")
+            # todo(nathan): ugh need to actually implement entry id's as well...
+            # if log_batch.entry_id:
+            #     # log_batch entry_id is empty for fd="server" messages from AppGetLogs
+            #     last_entry_id = log_batch.entry_id
+
+            # if log_batch.eof:
+            #     completed = True
+            #     break
+
+    while True:
+        try:
+            await _get_output()
+        except (GRPCError, StreamTerminatedError) as exc:
+            # todo(nathan): consider debounce?
+            if isinstance(exc, GRPCError):
+                if exc.status in RETRYABLE_GRPC_STATUS_CODES:
+                    continue
+            elif isinstance(exc, StreamTerminatedError):
+                continue
+            raise
