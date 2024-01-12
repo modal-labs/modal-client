77b88a4b72e5602d7e873c4b207f94c45c98d7f5
Nathan Wang <nathan.r.wang@gmail.com>
2024-01-12 00:11:58 +0000

    (wip) modal container connect

--- modal/cli/container.py
+++ modal/cli/container.py
@@ -82,3 +82,51 @@ async def exec(task_id: str, command: str):
             elif isinstance(exc, StreamTerminatedError):
                 continue
             raise
+
+
+@container_cli.command("connect")
+@synchronizer.create_blocking
+async def connect(task_id: str):
+    """Connects to a container and spawns /bin/bash."""
+    # todo(nathan): get rid of repeated code from above
+    client = await _Client.from_env()
+    res: api_pb2.ContainerExecResponse = await client.stub.ContainerExec(
+        api_pb2.ContainerExecRequest(task_id=task_id, command="/bin/bash")
+    )
+    if res.exec_id is None:
+        # todo(nathan): proper error message?
+        print("failed to execute command, unclear why")
+        return
+
+    last_entry_id = ""
+
+    async def _get_output():
+        nonlocal last_entry_id
+
+        req = api_pb2.ContainerExecGetOutputRequest(
+            exec_id=res.exec_id,
+            timeout=55,
+            last_entry_id=last_entry_id,
+        )
+        async for message in unary_stream(client.stub.ContainerExecGetOutput, req):
+            print(f"[{message.file_descriptor}] {message.message}")
+            # todo(nathan): ugh need to actually implement entry id's as well...
+            # if log_batch.entry_id:
+            #     # log_batch entry_id is empty for fd="server" messages from AppGetLogs
+            #     last_entry_id = log_batch.entry_id
+
+            # if log_batch.eof:
+            #     completed = True
+            #     break
+
+    while True:
+        try:
+            await _get_output()
+        except (GRPCError, StreamTerminatedError) as exc:
+            # todo(nathan): consider debounce?
+            if isinstance(exc, GRPCError):
+                if exc.status in RETRYABLE_GRPC_STATUS_CODES:
+                    continue
+            elif isinstance(exc, StreamTerminatedError):
+                continue
+            raise
