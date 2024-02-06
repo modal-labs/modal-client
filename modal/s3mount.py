2e0109d7ae5bbc23f8bb904fc7c7879d21131f33
Nathan Wang <nathan.r.wang@gmail.com>
2024-02-06 19:36:51 +0000

    make s3 mount credentials optional

--- modal/s3mount.py
+++ modal/s3mount.py
@@ -1,16 +1,23 @@
 # Copyright Modal Labs 2022
-from dataclasses import dataclass
-from typing import List, Dict, Union
 import os
+from dataclasses import dataclass
+from typing import Dict, List, Optional, Union
 
 from modal_proto import api_pb2
-from .secret import _Secret
 from modal_utils.async_utils import synchronize_api
+
+from .secret import _Secret
+
 
 @dataclass
 class _S3Mount:
     bucket_name: str
-    credentials: _Secret
+
+    # Credentials used to access the S3 bucket.
+    # The given secret should contain AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY.
+    # This does not need to be given if the S3 bucket is publicly accessible.
+    credentials: Optional[_Secret] = None
+
 
 def s3mounts_to_proto(mounts: Dict[Union[str, os.PathLike], _S3Mount]) -> List[api_pb2.S3Mount]:
     """
@@ -22,8 +29,10 @@ def s3mounts_to_proto(mounts: Dict[Union[str, os.PathLike], _S3Mount]) -> List[a
         api_pb2.S3Mount(
             bucket_name=mount.bucket_name,
             mount_path=path,
-            credentials_secret_id=mount.credentials.object_id
-        ) for path, mount in mounts.items()
+            credentials_secret_id=mount.credentials.object_id if mount.credentials else "",
+        )
+        for path, mount in mounts.items()
     ]
+
 
 S3Mount = synchronize_api(_S3Mount)
