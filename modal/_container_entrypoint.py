299f440379d23ff4cbd26845816059ad13e03cd6
Luis Capelo <luis@modal.com>
2024-02-06 20:12:59 +0000

    Formatting

--- modal/_container_entrypoint.py
+++ modal/_container_entrypoint.py
@@ -466,9 +466,9 @@ class _FunctionIOManager:
 
     async def checkpoint(self) -> None:
         """Message server indicating that function is ready to be checkpointed."""
-        await self._client.stub.ContainerCheckpoint(api_pb2.ContainerCheckpointRequest(
-            checkpoint_id=os.getenv("MODAL_CHECKPOINT_ID", "")
-        ))
+        await self._client.stub.ContainerCheckpoint(
+            api_pb2.ContainerCheckpointRequest(checkpoint_id=os.getenv("MODAL_CHECKPOINT_ID", ""))
+        )
 
         self._waiting_for_checkpoint = True
         await self._client._close()
