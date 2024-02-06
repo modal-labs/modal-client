9c1a973e0656083fefe666071b7524d0f8003922
Nathan Wang <nathan.r.wang@gmail.com>
2024-02-06 21:09:12 +0000

    AWS_REGION is mandatory

--- modal/s3mount.py
+++ modal/s3mount.py
@@ -1,6 +1,6 @@
 # Copyright Modal Labs 2022
 from dataclasses import dataclass
-from typing import List, Optional, Tuple
+from typing import List, Tuple
 
 from modal_proto import api_pb2
 from modal_utils.async_utils import synchronize_api
@@ -14,8 +14,8 @@ class _S3Mount:
 
     # Credentials used to access the S3 bucket.
     # The given secret should contain AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, and AWS_REGION.
-    # This does not need to be given if the S3 bucket is publicly accessible.
-    credentials: Optional[_Secret] = None
+    # AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY can be omitted if the bucket is publicly accessible.
+    credentials: _Secret
 
 
 def s3_mounts_to_proto(mounts: List[Tuple[str, _S3Mount]]) -> List[api_pb2.S3Mount]:
@@ -26,7 +26,7 @@ def s3_mounts_to_proto(mounts: List[Tuple[str, _S3Mount]]) -> List[api_pb2.S3Mou
         api_pb2.S3Mount(
             bucket_name=mount.bucket_name,
             mount_path=path,
-            credentials_secret_id=mount.credentials.object_id if mount.credentials else "",
+            credentials_secret_id=mount.credentials.object_id,
         )
         for path, mount in mounts
     ]
