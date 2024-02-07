3514d1917618090f968fa7ec0800bfff37faa2b5
Nathan Wang <nathan.r.wang@gmail.com>
2024-02-07 17:02:10 +0000

    make credentials optional

--- modal/s3mount.py
+++ modal/s3mount.py
@@ -1,6 +1,6 @@
 # Copyright Modal Labs 2022
 from dataclasses import dataclass
-from typing import List, Tuple
+from typing import List, Optional, Tuple
 
 from modal_proto import api_pb2
 from modal_utils.async_utils import synchronize_api
@@ -13,9 +13,10 @@ class _S3Mount:
     bucket_name: str
 
     # Credentials used to access the S3 bucket.
-    # The given secret should contain AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, and AWS_REGION.
+    # The given secret can contain AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY.
     # AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY can be omitted if the bucket is publicly accessible.
-    credentials: _Secret
+    # AWS_REGION can also be specified to manually configure the AWS region of the S3 bucket.
+    credentials: Optional[_Secret]
 
 
 def s3_mounts_to_proto(mounts: List[Tuple[str, _S3Mount]]) -> List[api_pb2.S3Mount]:
@@ -26,7 +27,7 @@ def s3_mounts_to_proto(mounts: List[Tuple[str, _S3Mount]]) -> List[api_pb2.S3Mou
         api_pb2.S3Mount(
             bucket_name=mount.bucket_name,
             mount_path=path,
-            credentials_secret_id=mount.credentials.object_id,
+            credentials_secret_id=mount.credentials.object_id if mount.credentials else "",
         )
         for path, mount in mounts
     ]
