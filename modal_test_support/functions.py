838cce9411ba505c5bea912f12b2b112c3b41569
Luis Capelo <953118+luiscape@users.noreply.github.com>
2024-02-14 12:57:38 -0500

    Checks for open connections before checkpointing (#1347)
    Adds a check for remote connections. This informs user that checkpointing isn't possible if connections are open. It also shows which targets currently have connections open.

--- modal_test_support/functions.py
+++ modal_test_support/functions.py
@@ -2,6 +2,7 @@
 from __future__ import annotations
 
 import asyncio
+import socket
 import time
 from datetime import date
 
@@ -296,3 +297,14 @@ class CheckpointingCls:
     @method()
     def f(self, x):
         return "".join(self._vals) + x
+
+
+@stub.cls(checkpointing_enabled=True)
+class CheckpointingClsNetworkConnectionOpen:
+    def __init__(self):
+        self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    @enter(checkpoint=True)
+    def open_connection(self):
+        remote_ip = socket.gethostbyname("modal.com")
+        self._socket.connect((remote_ip, 80))
