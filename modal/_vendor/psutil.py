a5dc7727556db10d29ddc945303d2e3540108b14
Luis Capelo <luis@modal.com>
2024-02-26 02:42:52 +0000

    Adds entire license

--- modal/_vendor/psutil.py
+++ modal/_vendor/psutil.py
@@ -1,14 +1,47 @@
+# This is a vendored copy of parts of the 'psutil' Python package [1] redistributed
+# according to the requirements of its original license. [2]
+#
+# It only contains what's needed to run the `net_connections()` function, which
+# lists active network connections from all processes on the system.
+#
+# [1] https://pypi.org/project/psutil/
+# [2] https://github.com/giampaolo/psutil?tab=BSD-3-Clause-1-ov-file
+#
+# ----
+#
+# BSD 3-Clause License
+#
 # Copyright (c) 2009, Jay Loden, Dave Daeschler, Giampaolo Rodola
 # All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without modification,
+# are permitted provided that the following conditions are met:
+#
+#  * Redistributions of source code must retain the above copyright notice, this
+#    list of conditions and the following disclaimer.
+#
+#  * Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+
+#  * Neither the name of the psutil authors nor the names of its contributors
+#    may be used to endorse or promote products derived from this software without
+#    specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+# ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+# ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+# ----
 
 # mypy: ignore-errors
-
-"""This is a vendored copy of parts of the 'psutil' Python package.
-
-It only contains what's needed to run the `net_connections()` function, which
-lists active network connections from all processes on the system.
-"""
-
 
 import base64
 import errno
