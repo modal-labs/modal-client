32083523b7451f38efac148b2a47b49421af1f71
Michael Waskom <mwaskom@users.noreply.github.com>
2024-02-29 13:30:16 -0500

    Show traceback from user code when local entrypoint function raises errors (#1443)
    * Show user code traceback for exceptions when running local entrypoint
    
    * Add retrospective changelog note about new traceback behavior
    
    * Update test
    
    * Fix typo
    
    * Show full traceback chain for user exceptions when requested
    
    * Add support script for test
    
    * Exit with nonzero status after printing user exception
    
    * Futz with whitespace

--- modal/__main__.py
+++ modal/__main__.py
@@ -3,7 +3,7 @@ import sys
 
 from ._traceback import highlight_modal_deprecation_warnings, setup_rich_traceback
 from .cli.entry_point import entrypoint_cli
-from .cli.import_refs import CliUserExecutionError
+from .cli.import_refs import _CliUserExecutionError
 from .config import config
 
 
@@ -14,7 +14,11 @@ def main():
 
     try:
         entrypoint_cli()
-    except CliUserExecutionError as exc:
+
+    except _CliUserExecutionError as exc:
+        if config.get("traceback"):
+            raise
+
         # Try to step forward in the traceback until we get to the user code that failed to import
         tb = orig_tb = exc.__cause__.__traceback__
         if exc.user_source.endswith(".py"):
@@ -27,17 +31,19 @@ def main():
             # In case we didn't find a frame that matched the user source, revert to the original traceback
             tb = orig_tb
         sys.excepthook(type(exc.__cause__), exc.__cause__, tb)
+        sys.exit(1)
+
     except Exception as exc:
         if config.get("traceback"):
             raise
-        else:
-            from rich.console import Console
-            from rich.panel import Panel
 
-            console = Console(stderr=True)
-            panel = Panel(str(exc), border_style="red", title="Error", title_align="left")
-            console.print(panel, highlight=False)
-            sys.exit(1)
+        from rich.console import Console
+        from rich.panel import Panel
+
+        console = Console(stderr=True)
+        panel = Panel(str(exc), border_style="red", title="Error", title_align="left")
+        console.print(panel, highlight=False)
+        sys.exit(1)
 
 
 if __name__ == "__main__":
