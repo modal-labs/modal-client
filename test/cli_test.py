b343caa6c04540161cc8bdb2d7083e9aef39c9ec
Michael Waskom <michael@modal.com>
2024-02-29 17:26:52 +0000

    Update test

--- test/cli_test.py
+++ test/cli_test.py
@@ -230,7 +230,7 @@ def test_run_local_entrypoint_invalid_with_stub_run(servicer, set_env_client, te
     stub_file = test_dir / "supports" / "app_run_tests" / "local_entrypoint_invalid.py"
 
     res = _run(["run", stub_file.as_posix()], expected_exit_code=1)
-    assert "app is already running" in str(res.exception).lower()
+    assert "app is already running" in str(res.exception.__cause__).lower()
     assert "unreachable" not in res.stdout
     assert len(servicer.client_calls) == 0
 
