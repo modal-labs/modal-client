d841e42b5ffeab8d55c4fa5389f4150f218858fd
Akshat Bubna <akshatb42@gmail.com>
2024-03-13 19:13:34 +0000

    copyright

--- modal/experimental.py
+++ modal/experimental.py
@@ -1,3 +1,6 @@
+# Copyright Modal Labs 2022
+
+
 def stop_fetching_inputs():
     """Don't fetch any more inputs from the server, after the current one.
     The container will exit gracefully after the current input is processed."""
