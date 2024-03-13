c66167a55707ad09a0ae03acd718b501a97cb6fc
Akshat Bubna <akshatb42@gmail.com>
2024-03-13 15:40:47 -0400

    `modal.experimental.stop_fetching_inputs` (#1388)

---
+++ modal/experimental.py
@@ -0,0 +1,10 @@
+# Copyright Modal Labs 2022
+
+
+def stop_fetching_inputs():
+    """Don't fetch any more inputs from the server, after the current one.
+    The container will exit gracefully after the current input is processed."""
+
+    from .app import _container_app
+
+    _container_app.stop_fetching_inputs()
