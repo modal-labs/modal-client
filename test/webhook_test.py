739627d84c7b1202304c4bfcaef8c6ce1acf1da1
Erik Bernhardsson <mail@erikbern.com>
2024-03-25 19:45:45 -0400

    Remove stub assignments: fix misc tests (#1601)

--- test/webhook_test.py
+++ test/webhook_test.py
@@ -39,9 +39,8 @@ async def test_webhook(servicer, client):
         container_app = ContainerApp()
         await ContainerApp.init.aio(client, stub.app_id)
         container_app._associate_stub_container(stub)
-        f_c = stub.f
-        assert isinstance(f_c, Function)
-        assert f_c.web_url
+        assert isinstance(f, Function)
+        assert f.web_url
 
 
 def test_webhook_cors():
