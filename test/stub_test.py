95eb72ab149e168f3dc9f3ad5addce718edc664c
Erik Bernhardsson <mail@erikbern.com>
2024-03-26 16:17:42 -0400

    Deprecate stub assignments (#1602)
    * Deprecate stub assignments
    
    * Change missing attribute exception to AttributeError

--- test/stub_test.py
+++ test/stub_test.py
@@ -42,10 +42,11 @@ async def test_attrs(servicer, client):
         stub.d = Dict.new()
         stub.q = Queue.new()
     async with stub.run(client=client):
-        await stub.d.put.aio("foo", "bar")  # type: ignore
-        await stub.q.put.aio("baz")  # type: ignore
-        assert await stub.d.get.aio("foo") == "bar"  # type: ignore
-        assert await stub.q.get.aio() == "baz"  # type: ignore
+        with pytest.warns(DeprecationError):
+            await stub.d.put.aio("foo", "bar")  # type: ignore
+            await stub.q.put.aio("baz")  # type: ignore
+            assert await stub.d.get.aio("foo") == "bar"  # type: ignore
+            assert await stub.q.get.aio() == "baz"  # type: ignore
 
         with pytest.raises(DeprecationError):
             stub.app.d
@@ -152,7 +153,7 @@ def test_missing_attr():
     an understandable error message."""
 
     stub = Stub()
-    with pytest.raises(KeyError):
+    with pytest.raises(AttributeError):
         stub.fun()  # type: ignore
 
 
@@ -329,7 +330,8 @@ def test_redeploy_from_name_change(servicer, client):
 
     # Use it from stub
     stub = Stub()
-    stub.q = modal.Queue.from_name("foo-queue")
+    with pytest.warns(DeprecationError):
+        stub.q = modal.Queue.from_name("foo-queue")
     deploy_stub(stub, "my-app", client=client)
 
     # Change the object id of foo-queue
@@ -357,3 +359,8 @@ def test_hydrated_other_app_object_gets_referenced(servicer, client):
             deploy_stub(stub, client=client)
             app_set_objects_req = ctx.pop_request("AppSetObjects")
             assert vol.object_id in app_set_objects_req.unindexed_object_ids
+
+
+def test_hasattr():
+    stub = Stub()
+    assert not hasattr(stub, "xyz")
