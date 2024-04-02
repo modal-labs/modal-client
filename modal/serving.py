e6af6430321ca9f61dbd5181616ba289d091a1ff
Erik Bernhardsson <mail@erikbern.com>
2024-04-02 08:12:51 -0400

    Move _LocalApp.disconnect into runner.py (#1646)

--- modal/serving.py
+++ modal/serving.py
@@ -14,10 +14,11 @@ from ._output import OutputManager
 from ._utils.async_utils import TaskContext, asyncify, synchronize_api, synchronizer
 from ._utils.logger import logger
 from ._watcher import watch
+from .app import _LocalApp
 from .cli.import_refs import import_stub
 from .client import _Client
 from .config import config
-from .runner import _run_stub, serve_update
+from .runner import _disconnect, _run_stub, serve_update
 
 if TYPE_CHECKING:
     from .stub import _Stub
@@ -120,7 +121,8 @@ async def _serve_stub(
         watcher = watch(mounts_to_watch, output_mgr)
 
     async with _run_stub(stub, client=client, output_mgr=output_mgr, environment_name=environment_name):
-        client.set_pre_stop(stub._local_app.disconnect)
+        app: _LocalApp = stub._local_app
+        client.set_pre_stop(lambda: _disconnect(app))
         async with TaskContext(grace=0.1) as tc:
             tc.create_task(_run_watch_loop(stub_ref, stub.app_id, output_mgr, watcher, environment_name))
             yield stub
