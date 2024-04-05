0ccc4b23a3107eedc7dd347abfa7ff280b4a5281
Jonathon Belotti <jonathon@modal.com>
2024-04-05 13:17:31 +0000

    rename 'Function env' to 'Function spec'

--- modal/functions.py
+++ modal/functions.py
@@ -536,10 +536,11 @@ def _parse_retries(
 
 
 @dataclass
-class FunctionEnv:
+class _FunctionSpec:
     """
-    Stores information about the function environment. This is used for `modal shell` to support
-    running shells in the same environment as a user-defined function.
+    Stores information about a Function specification.
+    This is used for `modal shell` to support running shells with
+    the same configuration as a user-defined Function.
     """
 
     image: Optional[_Image]
@@ -569,7 +570,7 @@ class _Function(_Object, type_prefix="fu"):
     _is_remote_cls_method: bool = False  # TODO(erikbern): deprecated
     _function_name: Optional[str]
     _is_method: bool
-    _env: FunctionEnv
+    _spec: _FunctionSpec
     _tag: str
     _raw_f: Callable[..., Any]
     _build_args: dict
@@ -665,7 +666,7 @@ class _Function(_Object, type_prefix="fu"):
             if image:
                 image = image.apt_install("autossh")
 
-        function_env = FunctionEnv(
+        function_spec = _FunctionSpec(
             mounts=all_mounts,
             secrets=secrets,
             gpu=gpu,
@@ -960,7 +961,7 @@ class _Function(_Object, type_prefix="fu"):
         obj._obj = None
         obj._is_generator = is_generator
         obj._is_method = bool(info.cls)
-        obj._env = function_env  # needed for modal shell
+        obj._spec = function_spec  # needed for modal shell
 
         # Used to check whether we should rebuild an image using run_function
         # Plaintext source and arg definition for the function, so it's part of the image
@@ -1115,9 +1116,9 @@ class _Function(_Object, type_prefix="fu"):
         return self._info
 
     @property
-    def env(self) -> FunctionEnv:
+    def spec(self) -> _FunctionSpec:
         """mdmd:hidden"""
-        return self._env
+        return self._spec
 
     def get_build_def(self) -> str:
         """mdmd:hidden"""
