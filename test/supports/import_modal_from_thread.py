caa9fce3b7279ce7218a4de59dad34fc67838de4
Elias Freider <elias@modal.com>
2024-04-24 12:03:11 +0200

    Adds test that modal can be imported from non-main thread

---
+++ test/supports/import_modal_from_thread.py
@@ -0,0 +1,19 @@
+import threading
+import traceback
+
+success = threading.Event()
+
+
+def main():
+    try:
+        import modal  # noqa
+    except BaseException:
+        traceback.print_exc()
+    success.set()
+
+
+if __name__ == "__main__":
+    t = threading.Thread(target=main, daemon=True)
+    t.start()
+    was_success = success.wait(timeout=1)
+    assert was_success
