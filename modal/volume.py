0de9e736219643084e414b97b73d125dcc468ddc
Michael Waskom <michael@modal.com>
2024-04-24 18:48:34 +0000

    Fix backwards compatibility for wrapped and unwrapped methods

--- modal/volume.py
+++ modal/volume.py
@@ -528,6 +528,7 @@ class _Volume(_Object, type_prefix="vo"):
 
     # @staticmethod  # TODO uncomment when enforcing deprecation of instance method invocation
     async def delete(*args, label: str = "", client: Optional[_Client] = None, environment_name: Optional[str] = None):
+        # -- Backwards-compatibility section
         # TODO(michael) Upon enforcement of this deprecation, remove *args and the default argument for label=.
         if isinstance(self := args[0], _Volume):
             msg = (
@@ -538,10 +539,15 @@ class _Volume(_Object, type_prefix="vo"):
             deprecation_warning((2024, 4, 23), msg)
             await self._instance_delete()
             return
-        if args[1:] and isinstance(args[1], str):
+        elif isinstance(args[0], type):
+            args = args[1:]
+
+        if args and isinstance(args[0], str):
             if label:
                 raise InvalidError("`label` specified as both positional and keyword argument")
-            label = args[1]
+            label = args[0]
+        # -- Backwards-compatibility code ends here
+
         obj = await _Volume.lookup(label, client=client, environment_name=environment_name)
         req = api_pb2.VolumeDeleteRequest(volume_id=obj.object_id)
         await retry_transient_errors(obj._client.stub.VolumeDelete, req)
