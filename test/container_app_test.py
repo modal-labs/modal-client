54d95e6782db24a4ee63d729d3b11aaaaf4846e5
Jonathon Belotti <jonathon@modal.com>
2024-05-02 03:48:01 +0000

    fix task secret expiry on snapshot containers

--- test/container_app_test.py
+++ test/container_app_test.py
@@ -1,4 +1,6 @@
 # Copyright Modal Labs 2022
+import json
+import os
 import pytest
 from typing import Dict
 
@@ -41,10 +43,31 @@ async def test_container_function_lazily_imported(container_client):
 
 
 @skip_windows_unix_socket
+@pytest.mark.asyncio
+async def test_container_snapshot_restore(container_client, tmpdir, servicer):
+    old_client = container_client
+    io_manager = ContainerIOManager(api_pb2.ContainerArguments(), container_client)
+    restore_path = tmpdir.join("fake-restore-state.json")
+    restore_path.write_text(
+        json.dumps(
+            {
+                "task_id": "ta-i-am-restored",
+                "task_secret": "ts-i-am-restored",
+                "function_id": "fu-i-am-restored",
+            }
+        ),
+        encoding="utf-8",
+    )
+    os.environ["MODAL_RESTORE_STATE_PATH"] = str(restore_path)
+    os.environ["MODAL_SERVER_URL"] = servicer.remote_addr
+    io_manager.checkpoint()
+    assert old_client.credentials == ("ta-i-am-restored", "ts-i-am-restored")
+
+
+@skip_windows_unix_socket
 def test_interact(container_client, unix_servicer):
     # Initialize container singleton
     ContainerIOManager(api_pb2.ContainerArguments(), container_client)
-
     with unix_servicer.intercept() as ctx:
         ctx.add_response("FunctionStartPtyShell", Empty())
         interact()
