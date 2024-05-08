da7aaf58a5ed3cb83b1fa3e6bb367576fcb0c27f
Deven Navani <deven.navani@gmail.com>
2024-05-08 13:17:13 +0000

    Incorporate feedback

--- modal/app.py
+++ modal/app.py
@@ -551,7 +551,7 @@ class _App:
 
             # Check if the decorated object is a class
             if inspect.isclass(f):
-                raise TypeError("The @app.function decorator cannot be used on a class.")
+                raise TypeError("The @app.function decorator cannot be used on a class. Please use @app.cls instead.")
 
             if isinstance(f, _PartialFunction):
                 f.wrapped = True
