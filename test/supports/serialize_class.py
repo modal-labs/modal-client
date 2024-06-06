e3843793ee38b113af208e11bab48832b096900f
Elias Freider <freider@users.noreply.github.com>
2024-06-06 11:33:09 +0200

    Make sure serialized partial functions are actually usable (#1876)
    * Changes how PartialMethods implement the descriptor protocol
    
    * ModalWrappedCls().method is unchanged (returns the _Obj-bound Function object)
    * ModalWrappedCls.method and UserCls.method now return the PartialFunction object - this is used by serialize(UserCls)
    * UserCls().method now returns the original method bound to the UserCls instance - acts like vanilla python since that's what UserCls is
    
    * Make sure serialized partial functions are actually usable
    
    Synchronicity wrapped objects that are serialized are not generally useful when deserialized in
    a different process. If we however add a custom serialization format that pickles the underlying
    object and then translates it on the receiving end, we get something that can be used.
    
    This is necessary to get usable methods on classes, since those are PartialFunctions which
    are synchronicity wrapped

---
+++ test/supports/serialize_class.py
@@ -0,0 +1,26 @@
+# Copyright Modal Labs 2024
+import sys
+
+import modal
+from modal import enter, method, web_endpoint
+from modal._serialization import serialize
+
+
+class UserCls:
+    @enter()
+    def enter(self):
+        pass
+
+    @method()
+    def method(self):
+        return "a"
+
+    @web_endpoint()
+    def web_endpoint(self):
+        pass
+
+
+app = modal.App()
+app.cls()(UserCls)  # avoid warnings about not turning methods into functions
+
+sys.stdout.buffer.write(serialize(UserCls))
