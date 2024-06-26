14050a217fb30911cbd2cc29f88a594b7d37c501
Daniel Norberg <dano@modal.com>
2024-06-26 15:42:22 -0400

    cleanup

--- modal_telemetry/_instrument.py
+++ modal_telemetry/_instrument.py
@@ -2,27 +2,25 @@
 
 import logging
 import os
+import socket
 import sys
 
 from modal_telemetry._telemetry import ImportInterceptor
 
 
-def instrument_imports():
+def instrument_imports(socket_filename: str):
     if hasattr(sys, "frozen"):
         raise Exception("unable to patch meta_path: sys is frozen")
-    socket_filename = os.environ.get("MODAL_TELEMETRY_SOCKET")
-    if socket_filename:
-        import socket
-
-        tracing_socket = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-        tracing_socket.connect(socket_filename)
-        sys.meta_path = [ImportInterceptor(tracing_socket)] + sys.meta_path  # type: ignore
+    tracing_socket = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+    tracing_socket.connect(socket_filename)
+    sys.meta_path = [ImportInterceptor(tracing_socket)] + sys.meta_path  # type: ignore
 
 
 def auto_instrument_imports():
-    if os.environ.get("MODAL_TELEMETRY_SOCKET"):
+    socket_filename = os.environ.get("MODAL_TELEMETRY_SOCKET")
+    if socket_filename:
         try:
-            instrument_imports()
+            instrument_imports(socket_filename)
         except BaseException as e:
             logging.warning(f"failed to instrument imports: {e}")
 
