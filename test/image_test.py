decebd9c974d090133a170eb2b7fd7790c5bc51e
Kamyar Salahi <kamyar@modal.com>
2024-07-03 02:24:38 +0000

    Separate app for image rebuild

--- test/image_test.py
+++ test/image_test.py
@@ -762,13 +762,14 @@ class FooInstance:
 
 
 def test_image_cls_var_rebuild(client, servicer):
+    rebuild_app = App()
     image_id = -1
-    cls_app.cls(image=Image.debian_slim())(FooInstance)
-    with cls_app.run(client=client):
+    rebuild_app.cls(image=Image.debian_slim())(FooInstance)
+    with rebuild_app.run(client=client):
         image_id = list(servicer.images.keys())[-1]
     FooInstance.bar = "22"
-    cls_app.cls(image=Image.debian_slim())(FooInstance)
-    with cls_app.run(client=client):
+    rebuild_app.cls(image=Image.debian_slim())(FooInstance)
+    with rebuild_app.run(client=client):
         image_id2 = list(servicer.images.keys())[-1]
     assert image_id != image_id2
 
