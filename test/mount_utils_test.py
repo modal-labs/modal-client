89e3169fce9020a4e8bd8a5a4004b519b7e5e3c1
kramstrom <kasper@modal.com>
2024-07-10 15:20:31 -0400

    typing

--- test/mount_utils_test.py
+++ test/mount_utils_test.py
@@ -1,4 +1,5 @@
 import pytest
+from collections.abc import Mapping
 
 from modal._utils.mount_utils import validate_mount_points, validate_network_file_systems, validate_volumes
 from modal.exception import InvalidError
@@ -8,45 +9,46 @@ from modal.volume import _Volume
 
 def test_validate_mount_points():
     # valid mount points
-    dict_input = {"/foo/bar": _NetworkFileSystem.from_name("NetworkFileSystem", create_if_missing=False)}
+    dict_input: Mapping = {"/foo/bar": _NetworkFileSystem.from_name("NetworkFileSystem", create_if_missing=False)}
     validate_mount_points("NetworkFileSystem", dict_input)
 
     # invalid list input, should be dicts
     list_input = [_NetworkFileSystem.from_name("NetworkFileSystem", create_if_missing=False)]
 
     with pytest.raises(InvalidError, match="NetworkFileSystem"):
-        validate_mount_points("NetworkFileSystem", list_input)
+        validate_mount_points("NetworkFileSystem", list_input)  # type: ignore
 
 
 @pytest.mark.parametrize("path", ["/", "/root", "/tmp", "foo/bar"])
 def test_validate_mount_points_invalid_paths(path):
-    validated_mount_points = {path: _NetworkFileSystem.from_name("NetworkFileSystem", create_if_missing=False)}
+    validated_mount_points: Mapping = {path: _NetworkFileSystem.from_name("NetworkFileSystem", create_if_missing=False)}
     with pytest.raises(InvalidError, match="NetworkFileSystem"):
         validate_mount_points("NetworkFileSystem", validated_mount_points)
 
 
 def test_validate_network_file_systems(client, servicer):
     # valid network_file_systems input
-    network_file_systems = {"/my/path": _NetworkFileSystem.from_name("foo", create_if_missing=False)}
+    network_file_systems: Mapping = {"/my/path": _NetworkFileSystem.from_name("foo", create_if_missing=False)}
     validate_network_file_systems(network_file_systems)
 
     # invalid non network_file_systems input
     not_network_file_systems = {"/my/path": _Volume.from_name("foo", create_if_missing=False)}
     with pytest.raises(InvalidError, match="_Volume"):
-        validate_network_file_systems(not_network_file_systems)
+        validate_network_file_systems(not_network_file_systems)  # type: ignore
 
 
 def test_validate_volumes(client, servicer):
     # valid volume input
-    volumes = {"/my/path": _Volume.from_name("foo", create_if_missing=False)}
+    volumes: Mapping = {"/my/path": _Volume.from_name("foo", create_if_missing=False)}
     validate_volumes(volumes)
 
     # invalid non volume input
     not_volumes = {"/my/path": _NetworkFileSystem.from_name("foo", create_if_missing=False)}
     with pytest.raises(InvalidError, match="_NetworkFileSystem"):
-        validate_volumes(not_volumes)
+        validate_volumes(not_volumes)  # type: ignore
 
     # invalid attempt mount volume twice
     vol = _Volume.from_name("foo", create_if_missing=False)
+    volumes: Mapping = {"/my/path": vol, "/my/other/path": vol}
     with pytest.raises(InvalidError, match="Volume"):
-        validate_volumes({"/my/path": vol, "/my/other/path": vol})
+        validate_volumes(volumes)
