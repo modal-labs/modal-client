31033ee334a8096f1c17db6e28f46b3cc145acf7
cathyzbn <cathyzhou0317@gmail.com>
2024-08-08 19:38:41 +0000

    fix test

--- modal/functions.py
+++ modal/functions.py
@@ -658,14 +658,13 @@ class _Function(_Object, type_prefix="fu"):
             else:
                 raise InvalidError("Webhooks cannot be generators")
 
-        func_name = info.raw_f.__name__
         if is_generator and batch_max_size:
-            raise InvalidError(f"Modal batched function {func_name} cannot return generators")
+            raise InvalidError(f"Modal batched function {info.raw_f} cannot return generators")
 
         if batch_max_size:
             for arg in inspect.signature(info.raw_f).parameters.values():
                 if arg.default is not inspect.Parameter.empty:
-                    raise InvalidError(f"Modal batched function {func_name} does not accept default arguments.")
+                    raise InvalidError(f"Modal batched function {info.raw_f} does not accept default arguments.")
 
         if container_idle_timeout is not None and container_idle_timeout <= 0:
             raise InvalidError("`container_idle_timeout` must be > 0")
