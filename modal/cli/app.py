aa6053ba73ee39488d55279c317df8e37a3290b3
Kasper Ramström <kramstrom@users.noreply.github.com>
2024-08-13 14:13:59 +0200

    MOD-3467: App deployment history client cli (#2084)

--- modal/cli/app.py
+++ modal/cli/app.py
@@ -5,7 +5,7 @@ import typer
 from click import UsageError
 from rich.table import Column
 from rich.text import Text
-from typer import Argument, Option
+from typer import Argument
 
 from modal._utils.async_utils import synchronizer
 from modal.client import _Client
@@ -13,7 +13,7 @@ from modal.environments import ensure_env
 from modal.object import _get_environment_name
 from modal_proto import api_pb2
 
-from .utils import ENV_OPTION, display_table, get_app_id_from_name, stream_app_logs, timestamp_to_local
+from .utils import ENV_OPTION, NAME_OPTION, display_table, get_app_id_from_name, stream_app_logs, timestamp_to_local
 
 app_cli = typer.Typer(name="app", help="Manage deployed and running apps.", no_args_is_help=True)
 
@@ -69,7 +69,7 @@ async def list(env: Optional[str] = ENV_OPTION, json: bool = False):
 def logs(
     app_id: str = Argument("", help="Look up any App by its ID"),
     *,
-    name: str = Option("", "-n", "--name", help="Look up a deployed App by its name"),
+    name: str = NAME_OPTION,
     env: Optional[str] = ENV_OPTION,
 ):
     """Show App logs, streaming while active.
@@ -102,7 +102,7 @@ def logs(
 async def stop(
     app_id: str = Argument(""),
     *,
-    name: str = Option("", "-n", "--name", help="Look up a deployed App by its name"),
+    name: str = NAME_OPTION,
     env: Optional[str] = ENV_OPTION,
 ):
     """Stop an app."""
@@ -111,3 +111,71 @@ async def stop(
         app_id = await get_app_id_from_name.aio(name, env, client)
     req = api_pb2.AppStopRequest(app_id=app_id, source=api_pb2.APP_STOP_SOURCE_CLI)
     await client.stub.AppStop(req)
+
+
+@app_cli.command("history", no_args_is_help=True)
+@synchronizer.create_blocking
+async def history(
+    app_id: str = Argument("", help="Look up an App's deployment history by its ID"),
+    *,
+    env: Optional[str] = ENV_OPTION,
+    name: str = NAME_OPTION,
+    json: bool = False,
+):
+    """Show App deployment history, for a currently deployed app
+
+    **Examples:**
+
+    Get the history based on an app ID:
+
+    ```bash
+    modal app history ap-123456
+    ```
+
+    Get the history for a currently deployed App based on its name:
+
+    ```bash
+    modal app history --name my-app
+    ```
+
+    """
+    if not bool(app_id) ^ bool(name):
+        raise UsageError("Must pass either an ID or a name.")
+
+    env = ensure_env(env)
+    client = await _Client.from_env()
+
+    if not app_id:
+        app_id = await get_app_id_from_name.aio(name, env, client)
+
+    resp = await client.stub.AppDeploymentHistory(api_pb2.AppDeploymentHistoryRequest(app_id=app_id))
+
+    columns = [
+        "Version",
+        "Time deployed",
+        "Client",
+        "Deployed by",
+    ]
+    rows = []
+    deployments_with_tags = False
+    for idx, app_stats in enumerate(resp.app_deployment_histories):
+        style = "bold green" if idx == 0 else ""
+
+        row = [
+            Text(str(app_stats.version), style=style),
+            Text(timestamp_to_local(app_stats.deployed_at, json), style=style),
+            Text(app_stats.client_version, style=style),
+            Text(app_stats.deployed_by, style=style),
+        ]
+
+        if app_stats.tag:
+            deployments_with_tags = True
+            row.append(Text(app_stats.tag, style=style))
+
+        rows.append(row)
+
+    if deployments_with_tags:
+        columns.append("Tag")
+
+    rows = sorted(rows, key=lambda x: str(x[0]), reverse=True)
+    display_table(columns, rows, json)
