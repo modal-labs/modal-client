4ea8ddd2593ef2bc403c09efaae6f1e93ce75061
cathyzbn <cathyzhou0317@gmail.com>
2024-08-30 13:44:51 +0000

    fix type

--- test/container_test.py
+++ test/container_test.py
@@ -1965,7 +1965,7 @@ def test_no_warn_on_remote_local_volume_mount(client, servicer, recwarn, set_env
 @pytest.mark.parametrize("concurrency_limit", [1, 2])
 def test_container_io_manager_concurrency_tracking(client, servicer, concurrency_limit):
     dummy_container_args = api_pb2.ContainerArguments(
-        function_id="fu-123", function_def=api_pb2.Function(allow_concurrent_inputs=concurrency_limit)
+        function_id="fu-123", function_def=api_pb2.Function(target_concurrent_inputs=concurrency_limit)
     )
     from modal._utils.async_utils import synchronizer
 
