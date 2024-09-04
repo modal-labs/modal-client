a4c496279ad43a2ec64feb57a70b704299d59fac
Elias Freider <elias@modal.com>
2024-09-04 13:54:29 +0200

    Remove prints

--- tasks.py
+++ tasks.py
@@ -45,7 +45,6 @@ def protoc(ctx):
             f.write(src)
         grpc_plugin_path = Path(f.name)
 
-    print("plugin: {grpc_plugin_path}")
     ctx.run(
         f"{protoc_cmd} --plugin=protoc-gen-modal-grpclib-python={grpc_plugin_path}"
         + f" --modal-grpclib-python_out=. -I . {input_files}"
