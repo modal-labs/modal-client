1272c901c9f133c06de239372170b687872a0556
Richard Gong <richard@modal.com>
2024-09-09 23:01:10 +0000

    tmp

--- modal/experimental.py
+++ modal/experimental.py
@@ -14,17 +14,18 @@ def stop_fetching_inputs():
     _ContainerIOManager.stop_fetching_inputs()
 
 
+
+
 def get_local_input_concurrency():
     """Get the container's local input concurrency. Return 0 if the container is not running."""
 
     return _ContainerIOManager.get_input_concurrency()
 
 async def _logger(message: str) -> None:
-    """Log a message to the Modal logs."""
+    """Send a structured log with tags."""
     request = api_pb2.ContainerLogRequest(message=message, input_id=current_input_id())
     io_manager = _ContainerIOManager._singleton
     assert io_manager is not None
     await retry_transient_errors(io_manager._client.stub.ContainerLog, request)
-
 
 logger = synchronize_api(_logger)
