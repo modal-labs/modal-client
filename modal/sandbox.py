8466d259a8a03d5c044d88124ea1221f011b02fe
Akshat Bubna <akshatb42@gmail.com>
2024-09-15 01:01:36 +0000

    update docstring

--- modal/sandbox.py
+++ modal/sandbox.py
@@ -336,7 +336,9 @@ class _Sandbox(_Object, type_prefix="sb"):
         **Usage**
 
         ```python
-        sandbox = modal.Sandbox.create("sleep", "infinity")
+        app = modal.App.lookup("my-app", create_if_missing=True)
+
+        sandbox = modal.Sandbox.create("sleep", "infinity", app=app)
 
         process = sandbox.exec("bash", "-c", "for i in $(seq 1 10); do echo foo $i; sleep 0.5; done")
 
