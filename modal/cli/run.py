4adbb93a19e32972897a651cd313b85bfa9789af
irfan sharif <irfanmahmoudsharif@gmail.com>
2024-09-17 23:07:47 -0400

    More proto changes for GPU fallbacks (#2243)
    Get rid of the _experimental_task_templates fields in Function - we'll
    using the FunctionData form introduced earlier instead. Remove the
    FunctionMeta proto (it was detritus). Rip out the _experimental_gpus
    flags too.

--- modal/cli/run.py
+++ modal/cli/run.py
@@ -426,7 +426,6 @@ def shell(
             memory=function_spec.memory,
             volumes=function_spec.volumes,
             region=function_spec.scheduler_placement.proto.regions if function_spec.scheduler_placement else None,
-            _experimental_gpus=function_spec._experimental_gpus,
         )
     else:
         modal_image = Image.from_registry(image, add_python=add_python) if image else None
