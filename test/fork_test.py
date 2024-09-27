dbee4ee867b52db041e8873ea74f50f5d16fc6ea
Elias Freider <elias@modal.com>
2024-09-27 09:59:16 +0200

    Disable fork test on windows

--- test/fork_test.py
+++ test/fork_test.py
@@ -1,8 +1,10 @@
 # Copyright Modal Labs 2024
 import subprocess
 import sys
+from test.supports.skip import skip_windows
 
 
+@skip_windows("fork not supported on windows")
 def test_process_fork(supports_dir, server_url_env):
     output = subprocess.check_output([sys.executable, supports_dir / "forking.py"], timeout=2, encoding="utf8")
 
