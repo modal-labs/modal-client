dc0ca0f4a551d519bb0b0cba73e26aaf770ec114
David Flemström <david@modal.com>
2024-10-09 17:28:47 +0200

    rust client: Simplify example

--- crates/modal/examples/hello.rs
+++ crates/modal/examples/hello.rs
@@ -3,10 +3,10 @@ use modal::args;
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let config = modal::Config::load()?;
-    let mut client = modal::Client::connect(config, Some("modal-labs")).await?;
+    let mut client = modal::Client::connect(config, None).await?;
 
     let mut func = client.lookup_function("main", "payload-value", "f").await?;
-    let result = func.call(args![foo = 3, bar = "hi"]).await?;
+    let result = func.call(args!(3, bar = "hi")).await?;
     eprintln!("result = {:?}", result);
     Ok(())
 }
