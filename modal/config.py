c2c4808522ba6d856b550307010477ea637f0c69
Jonathon Belotti <jonathon@modal.com>
2024-10-21 17:51:09 +0000

    snapshots: add snapshot_debug flag for debugging

--- modal/config.py
+++ modal/config.py
@@ -221,6 +221,7 @@ _SETTINGS = {
     "image_builder_version": _Setting(),
     "strict_parameters": _Setting(False, transform=_to_boolean),  # For internal/experimental use
     "spawn_extended": _Setting(False, transform=_to_boolean),
+    "snapshot_debug": _Setting(False, transform=_to_boolean),
 }
 
 
