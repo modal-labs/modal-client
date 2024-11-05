45f0301f8fa2877b9f0b299311deb9854e37a5e5
Nathan Wang <nathan.r.wang@gmail.com>
2024-11-05 19:39:58 +0000

    Remove grouped functions; add clustered function

--- modal/functions.py
+++ modal/functions.py
@@ -315,6 +315,7 @@ class _Function(typing.Generic[P, ReturnType, OriginalReturnType], _Object, type
     _build_args: dict
     _can_use_base_function: bool = False  # whether we need to call FunctionBindParams
     _is_generator: Optional[bool] = None
+    _cluster_size: Optional[int] = None
 
     # when this is the method of a class/object function, invocation of this function
     # should be using another function id and supply the method name in the FunctionInput:
@@ -416,6 +417,7 @@ class _Function(typing.Generic[P, ReturnType, OriginalReturnType], _Object, type
         fun._use_method_name = method_name
         fun._app = class_service_function._app
         fun._is_generator = partial_function.is_generator
+        fun._cluster_size = partial_function.cluster_size
         fun._all_mounts = class_service_function._all_mounts
         fun._spec = class_service_function._spec
         fun._is_method = True
@@ -446,6 +448,7 @@ class _Function(typing.Generic[P, ReturnType, OriginalReturnType], _Object, type
             )  # TODO: this shouldn't be set when actual parameters are used
             method_placeholder_fun._function_name = full_function_name
             method_placeholder_fun._is_generator = class_bound_method._is_generator
+            method_placeholder_fun._cluster_size = class_bound_method._cluster_size
             method_placeholder_fun._use_method_name = method_name
             method_placeholder_fun._use_function_id = instance_service_function.object_id
             method_placeholder_fun._is_method = True
@@ -518,7 +521,7 @@ class _Function(typing.Generic[P, ReturnType, OriginalReturnType], _Object, type
         enable_memory_snapshot: bool = False,
         block_network: bool = False,
         i6pn_enabled: bool = False,
-        group_size: Optional[int] = None,  # Experimental: Grouped functions
+        cluster_size: Optional[int] = None,  # Experimental: Clustered functions
         max_inputs: Optional[int] = None,
         ephemeral_disk: Optional[int] = None,
         _experimental_buffer_containers: Optional[int] = None,
@@ -838,7 +841,7 @@ class _Function(typing.Generic[P, ReturnType, OriginalReturnType], _Object, type
                     i6pn_enabled=i6pn_enabled,
                     schedule=schedule.proto_message if schedule is not None else None,
                     snapshot_debug=config.get("snapshot_debug"),
-                    _experimental_group_size=group_size or 0,  # Experimental: Grouped functions
+                    _experimental_group_size=cluster_size or 0,  # Experimental: Clustered functions
                     _experimental_concurrent_cancellations=True,
                     _experimental_buffer_containers=_experimental_buffer_containers or 0,
                     _experimental_proxy_ip=_experimental_proxy_ip,
@@ -929,6 +932,7 @@ class _Function(typing.Generic[P, ReturnType, OriginalReturnType], _Object, type
         obj._app = app  # needed for CLI right now
         obj._obj = None
         obj._is_generator = is_generator
+        obj._cluster_size = cluster_size
         obj._is_method = False
         obj._spec = function_spec  # needed for modal shell
 
@@ -1155,6 +1159,7 @@ class _Function(typing.Generic[P, ReturnType, OriginalReturnType], _Object, type
         # Overridden concrete implementation of base class method
         self._progress = None
         self._is_generator = None
+        self._cluster_size = None
         self._web_url = None
         self._function_name = None
         self._info = None
@@ -1217,6 +1222,11 @@ class _Function(typing.Generic[P, ReturnType, OriginalReturnType], _Object, type
         """mdmd:hidden"""
         assert self._is_generator is not None
         return self._is_generator
+
+    @property
+    def cluster_size(self) -> int:
+        """mdmd:hidden"""
+        return self._cluster_size or 1
 
     @live_method_gen
     async def _map(
