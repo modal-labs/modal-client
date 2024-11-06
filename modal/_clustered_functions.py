0847f59e42984b369ab65cc4008c31e4f5d58939
Nathan Wang <nathan.r.wang@gmail.com>
2024-11-06 21:18:05 +0000

    update implementation to not require container knowledge of cluster_rank/cluster_size

--- modal/_clustered_functions.py
+++ modal/_clustered_functions.py
@@ -8,7 +8,7 @@ from modal.client import _Client
 from modal_proto import api_pb2
 
 
-async def _initialize_clustered_function(client: _Client, cluster_id: str, cluster_rank: int, cluster_size: int):
+async def _initialize_clustered_function(client: _Client, task_id: str):
     def get_i6pn():
         """Returns the ipv6 address assigned to this container."""
         return socket.getaddrinfo("i6pn.modal.local", None, socket.AF_INET6)[0][4][0]
@@ -23,17 +23,23 @@ async def _initialize_clustered_function(client: _Client, cluster_id: str, clust
     # See MOD-4067.
     os.environ["NCCL_HOSTID"] = f"{hostname}{container_ip}"
 
-    print(
-        f"Initializing clustered function with cluster ID {cluster_id}, rank {cluster_rank}, "
-        f"size {cluster_size}, and IP {container_ip}"
-    )
+    print(f"Initializing clustered function with task ID {task_id} and IP {container_ip}")
     resp: api_pb2.ClusterHelloResponse = await retry_transient_errors(
         client.stub.ClusterHello,
-        api_pb2.ClusterHelloRequest(cluster_id=cluster_id, cluster_rank=cluster_rank, container_ip=container_ip),
+        api_pb2.ClusterHelloRequest(
+            task_id=task_id,
+            container_ip=container_ip,
+        ),
     )
     container_ips = resp.container_ips
+    cluster_id = resp.cluster_id
+    cluster_rank = resp.cluster_rank
+    cluster_size = resp.cluster_size
 
-    print(f"Got container IPs: {container_ips}")
+    print(
+        f"Cluster initialized with ID {cluster_id}, rank {cluster_rank}, size {cluster_size}, "
+        f"and IPs {container_ips}"
+    )
 
 
 initialize_clustered_function = synchronize_api(_initialize_clustered_function)
