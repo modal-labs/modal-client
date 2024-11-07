228610151eeaef165103d43dcf247ded4c1ce265
Luis Capelo <953118+luiscape@users.noreply.github.com>
2024-11-07 14:40:27 -0500

    Replaces `ProxyGetOrCreate` with `ProxyGet` (#2470)
    Companion client PR to https://github.com/modal-labs/modal/pull/17120. This replaces the use of `ProxyGetOrCreate` with `ProxyGet` to hydrate `Proxy` objects.

--- modal/proxy.py
+++ modal/proxy.py
@@ -22,13 +22,12 @@ class _Proxy(_Object, type_prefix="pr"):
         environment_name: Optional[str] = None,
     ) -> "_Proxy":
         async def _load(self: _Proxy, resolver: Resolver, existing_object_id: Optional[str]):
-            req = api_pb2.ProxyGetOrCreateRequest(
-                deployment_name=label,
-                namespace=namespace,
+            req = api_pb2.ProxyGetRequest(
+                name=label,
                 environment_name=_get_environment_name(environment_name, resolver),
             )
-            response = await resolver.client.stub.ProxyGetOrCreate(req)
-            self._hydrate(response.proxy_id, resolver.client, None)
+            response: api_pb2.ProxyGetResponse = await resolver.client.stub.ProxyGet(req)
+            self._hydrate(response.proxy.proxy_id, resolver.client, None)
 
         return _Proxy._from_loader(_load, "Proxy()", is_another_app=True)
 
