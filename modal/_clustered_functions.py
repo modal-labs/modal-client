23e93d2a5067105c2efae9b7960e8b06620cc374
Nathan Wang <nathan.r.wang@gmail.com>
2024-11-13 13:39:06 -0500

    [Multi Node] Add Clustered Functions (#2465)
    * Remove grouped functions; add clustered function
    
    * Move grouped functions to clustered functions
    
    * clean up
    
    * add cluster hello request
    
    * copyright
    
    * update implementation to not require container knowledge of cluster_rank/cluster_size
    
    * sketchy get_cluster_info()
    
    * extend ClusterInfo a bit
    
    * address code review
    
    * remove clutser id
    
    * remove resp.cluster_size
    
    * oops
    
    * remove outdated rpcs
    
    * oops
    
    * oops
    
    * use List
    
    * use Optional

---
+++ modal/_clustered_functions.py
@@ -0,0 +1,61 @@
+# Copyright Modal Labs 2024
+import os
+import socket
+from dataclasses import dataclass
+from typing import List, Optional
+
+from modal._utils.async_utils import synchronize_api
+from modal._utils.grpc_utils import retry_transient_errors
+from modal.client import _Client
+from modal_proto import api_pb2
+
+
+@dataclass
+class ClusterInfo:
+    rank: int
+    world_size: int
+    container_ips: List[str]
+
+
+cluster_info: Optional[ClusterInfo] = None
+
+
+def get_cluster_info() -> ClusterInfo:
+    if cluster_info is None:
+        raise Exception("Cluster info not initialized; please ensure that the function is a clustered function")
+    return cluster_info
+
+
+async def _initialize_clustered_function(client: _Client, task_id: str):
+    global cluster_info
+
+    def get_i6pn():
+        """Returns the ipv6 address assigned to this container."""
+        return socket.getaddrinfo("i6pn.modal.local", None, socket.AF_INET6)[0][4][0]
+
+    hostname = socket.gethostname()
+    container_ip = get_i6pn()
+
+    # nccl's default host ID is $(hostname)$(cat /proc/sys/kernel/random/boot_id).
+    # on runc, if two i6pn-linked containers get scheduled on the same worker,
+    # their boot ID and hostname will both be identical, causing nccl to break.
+    # As a workaround, we can explicitly specify a unique host ID here.
+    # See MOD-4067.
+    os.environ["NCCL_HOSTID"] = f"{hostname}{container_ip}"
+
+    resp: api_pb2.TaskClusterHelloResponse = await retry_transient_errors(
+        client.stub.TaskClusterHello,
+        api_pb2.TaskClusterHelloRequest(
+            task_id=task_id,
+            container_ip=container_ip,
+        ),
+    )
+
+    cluster_info = ClusterInfo(
+        rank=resp.cluster_rank,
+        world_size=len(resp.container_ips),
+        container_ips=resp.container_ips,
+    )
+
+
+initialize_clustered_function = synchronize_api(_initialize_clustered_function)
