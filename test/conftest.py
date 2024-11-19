db5ff17439d955d08a79fbe2fe7e1315babc0fe0
Erik Bernhardsson <mail@erikbern.com>
2024-11-19 13:53:04 +0000

    Fix test

--- test/conftest.py
+++ test/conftest.py
@@ -152,7 +152,6 @@ class MockClientServicer(api_grpc.ModalClientBase):
             }
         ]
         self.app_objects = {}
-        self.app_single_objects = {}
         self.app_unindexed_objects = {
             "ap-1": ["im-1", "vo-1"],
         }
@@ -485,8 +484,6 @@ class MockClientServicer(api_grpc.ModalClientBase):
         request: api_pb2.AppSetObjectsRequest = await stream.recv_message()
         self.app_objects[request.app_id] = dict(request.indexed_object_ids)
         self.app_unindexed_objects[request.app_id] = list(request.unindexed_object_ids)
-        if request.single_object_id:
-            self.app_single_objects[request.app_id] = request.single_object_id
         self.app_set_objects_count += 1
         if request.new_app_state:
             self.app_state_history[request.app_id].append(request.new_app_state)
