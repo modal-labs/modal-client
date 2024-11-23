720aa559cfff44298a7549ecc2e50375b8006368
Deven Navani <deven.navani@gmail.com>
2024-11-23 01:48:52 +0000

    edit build_number

--- modal_version/_version_generated.py
+++ modal_version/_version_generated.py
@@ -1,4 +1,4 @@
 # Copyright Modal Labs 2024
 
 # Note: Reset this value to -1 whenever you make a minor `0.X` release of the client.
-build_number = 39  # git: 892d93f
+build_number = -1  # git: 892d93f
