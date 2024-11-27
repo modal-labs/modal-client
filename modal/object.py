a1760e20cd660e793aec2c5cbd5ad8463424370a
Elias Freider <freider@users.noreply.github.com>
2024-11-27 13:19:01 +0100

    Fixes issue where Volume.from_name eagerly assigns event loop (#2581)
    * Fixes issue where Volume.from_name eagerly assigns the main thread event loop to internal lock

--- modal/object.py
+++ modal/object.py
@@ -17,7 +17,7 @@ O = TypeVar("O", bound="_Object")
 
 _BLOCKING_O = synchronize_api(O)
 
-EPHEMERAL_OBJECT_HEARTBEAT_SLEEP = 300
+EPHEMERAL_OBJECT_HEARTBEAT_SLEEP: int = 300
 
 
 def _get_environment_name(environment_name: Optional[str] = None, resolver: Optional[Resolver] = None) -> Optional[str]:
