41aa4c4a945495994a84468eac3dc56ab0fb9e2f
Elias Freider <freider@users.noreply.github.com>
2024-12-04 15:29:26 +0100

    Small comment and type correctness for Obj constructor (#2607)

--- modal/cls.py
+++ modal/cls.py
@@ -90,7 +90,7 @@ class _Obj:
 
     def __init__(
         self,
-        user_cls: type,
+        user_cls: Optional[type],  # this would be None in case of lookups
         class_service_function: Optional[_Function],  # only None for <v0.63 classes
         classbound_methods: dict[str, _Function],
         from_other_workspace: bool,
