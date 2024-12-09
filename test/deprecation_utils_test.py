139ccf02022287adae0d8330a139b95f056b199e
Michael Waskom <michael@modal.com>
2024-12-09 22:57:33 +0000

    Add utility for warning about renamed parameters

---
+++ test/deprecation_utils_test.py
@@ -0,0 +1,23 @@
+import inspect
+import pytest
+
+from modal._utils.deprecation_utils import renamed_parameter
+from modal.exception import DeprecationError
+
+
+@renamed_parameter("bar", "foo", (2024, 12, 1))
+def my_func(bar: int) -> int:
+    return bar**2
+
+
+def test_renamed_parameter():
+    message = "The 'foo' parameter .+ has been renamed to 'bar'"
+    with pytest.warns(DeprecationError, match=message):
+        res = my_func(foo=2)  # type: ignore
+        assert res == 4
+    assert my_func(bar=3) == 9
+    assert my_func(4) == 16
+
+    sig = inspect.signature(my_func)
+    assert "bar" in sig.parameters
+    assert "foo" not in sig.parameters
