a8403b748170fb21df9ff6179a38989f4c74b675
Michael Waskom <michael@modal.com>
2024-12-09 22:57:33 +0000

    Add utility for warning about renamed parameters

---
+++ modal/_utils/deprecation_utils.py
@@ -0,0 +1,30 @@
+import functools
+from typing import Callable, ParamSpec, TypeVar
+
+from ..exception import deprecation_warning  # TODO move that helper into this module?
+
+P = ParamSpec("P")
+R = TypeVar("R")
+
+
+def renamed_parameter(
+    new_name: str,
+    old_name: str,
+    date: tuple[int, int, int],
+) -> Callable[[Callable[P, R]], Callable[P, R]]:
+    def decorator(func: Callable[P, R]) -> Callable[P, R]:
+        @functools.wraps(func)
+        def wrapper(*args: P.args, **kwargs: P.kwargs) -> R:
+            if old_name in kwargs:
+                kwargs[new_name] = kwargs.pop(old_name)
+                func_name = func.__qualname__.removeprefix("_")  # Avoid confusion when synchronicity-wrapped
+                message = (
+                    f"The '{old_name}' parameter of `{func_name}` has been renamed to '{new_name}'."
+                    "\nUsing the old name will become an error in a future release. Please update your code."
+                )
+                deprecation_warning(date, message, show_source=False)
+            return func(*args, **kwargs)
+
+        return wrapper
+
+    return decorator
