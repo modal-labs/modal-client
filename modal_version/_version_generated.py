1e08ea2522326431410c44845a8f6c2877d1f532
Erik Bernhardsson <erik@modal.com>
2024-12-11 15:32:59 -0500

    Remove ClientHello (attempt 2) (#2641)
    * Remove ClientHello on client construction
    
    * Fix test_grpc_protocol
    
    * No need since #2454 to call hello to catch connection errors
    
    * Bump minor number

--- modal_version/_version_generated.py
+++ modal_version/_version_generated.py
@@ -1,4 +1,4 @@
 # Copyright Modal Labs 2024
 
 # Note: Reset this value to -1 whenever you make a minor `0.X` release of the client.
-build_number = 47  # git: a3d399f
+build_number = -1  # git: a3d399f
