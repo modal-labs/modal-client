7af52a99d9892158f457886de6fd7c451cd65383
Andrew Liu <andrewliu278@gmail.com>
2024-12-11 15:41:05 +0000

    add readline type assertion

--- test/supports/type_assertions.py
+++ test/supports/type_assertions.py
@@ -93,8 +93,10 @@ for line_bytes in cmd2.stdout:
 # check file_io
 file_io = sandbox.open("foo", "w")
 assert_type(file_io.read(), str)
+assert_type(file_io.readline(), str)
 assert_type(file_io.readlines(), typing.Sequence[str])
 
 file_io2 = sandbox.open("foo", "rb")
 assert_type(file_io2.read(), bytes)
+assert_type(file_io2.readline(), bytes)
 assert_type(file_io2.readlines(), typing.Sequence[bytes])
