312299fce9b6b2cee99a61614007ebdba10a1f52
Kasper Vogel <kasper@modal.com>
2024-12-13 09:14:56 +0100

    remove public api

--- modal/__init__.py
+++ modal/__init__.py
@@ -11,7 +11,6 @@ from modal_version import __version__
 try:
     from ._runtime.execution_context import current_function_call_id, current_input_id, interact, is_local
     from ._tunnel import Tunnel, forward
-    from ._utils.local_file_filter import LocalFileFilter
     from .app import App, Stub
     from .client import Client
     from .cloud_bucket_mount import CloudBucketMount
@@ -80,5 +79,4 @@ __all__ = [
     "web_endpoint",
     "web_server",
     "wsgi_app",
-    "LocalFileFilter",
 ]
--- modal/_utils/local_file_filter.py
+++ modal/_utils/local_file_filter.py
@@ -8,19 +8,51 @@ class LocalFileFilter:
     """Allows checking paths against one or more patterns."""
 
     def __init__(self, *pattern: str, invert: bool = False) -> None:
-        """Initialize a new LocalFileFilter instance.
+        """Initialize a new LocalFileFilter instance. Calling it will return True if the path matches any of
+        the patterns. Uses the underlying PatternMatcher to match the patterns which is dockerignore compatible.
 
         Args:
             pattern (str): One or more pattern strings.
-
+            invert (bool): If True, the filter will return True for paths that do not match any of the patterns.
         """
         self.patterns: list[str] = list(pattern)
         self.invert: bool = invert
 
     def __call__(self, file_path: Path) -> bool:
+        """Check if the path matches any of the patterns.
+
+        Args:
+            file_path (Path): The path to check.
+
+        Returns:
+            True if the path matches any of the patterns.
+            If self.invert is set to True, returns True if the path does not match any of the patterns instead.
+
+        Usage:
+        ```python
+
+        filter = LocalFileFilter("*.py")
+
+        assert filter(Path("foo.py"))
+        assert not ~filter(Path("foo.py"))
+        ```
+        """
         if self.invert:
             return not PatternMatcher(self.patterns).matches(str(file_path))
         return PatternMatcher(self.patterns).matches(str(file_path))
 
     def __invert__(self) -> "LocalFileFilter":
+        """Invert the filter. If self.invert is set to True, returns True if the path does not match any of the patterns
+        instead.
+
+        Usage:
+        ```python
+
+        filter = LocalFileFilter("*.py")
+
+        assert filter(Path("foo.py"))
+
+        assert not ~filter(Path("foo.py"))
+        ```
+        """
         return LocalFileFilter(*self.patterns, invert=True)
