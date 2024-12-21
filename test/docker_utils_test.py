e73fea208a4f59070cc4e9576220d4c699cdbbd8
Elias Freider <elias@modal.com>
2024-12-21 10:21:51 +0100

    Remove dockerignore stuff for now

--- test/docker_utils_test.py
+++ test/docker_utils_test.py
@@ -5,7 +5,7 @@ import sys
 from pathlib import Path
 from tempfile import NamedTemporaryFile, TemporaryDirectory
 
-from modal._utils.docker_utils import extract_copy_command_patterns, find_dockerignore_file
+from modal._utils.docker_utils import extract_copy_command_patterns
 
 
 def test_extract_copy_command_patterns():
@@ -42,96 +42,6 @@ def test_extract_copy_command_patterns():
         copy_command_sources = sorted(extract_copy_command_patterns(dockerfile_lines))
         expected = sorted(expected)
         assert copy_command_sources == expected
-
-
-@pytest.mark.usefixture("tmp_cwd")
-def test_find_dockerignore_file():
-    # case 1:
-    # generic dockerignore file in cwd --> find it
-    test_cwd = Path.cwd()
-    with TemporaryDirectory(dir=test_cwd) as tmp_dir:
-        tmp_path = Path(tmp_dir)
-        dir1 = tmp_path / "dir1"
-        dir1.mkdir()
-
-        dockerfile_path = dir1 / "Dockerfile"
-        dockerignore_path = tmp_path / ".dockerignore"
-        dockerignore_path.write_text("**/*")
-        assert find_dockerignore_file(test_cwd / tmp_dir, dockerfile_path) == dockerignore_path
-
-    # case 2:
-    # should not match specific dockerignore file in cwd
-    # when dockerfile is nested
-    with TemporaryDirectory(dir=test_cwd) as tmp_dir:
-        tmp_path = Path(tmp_dir)
-        dir1 = tmp_path / "dir1"
-        dir1.mkdir()
-
-        dockerfile_path = dir1 / "foo"
-        dockerignore_path = tmp_path / "foo.dockerignore"
-        dockerignore_path.write_text("**/*")
-        assert find_dockerignore_file(test_cwd / tmp_dir, dockerfile_path) is None
-
-    # case 3:
-    # generic dockerignore file and nested dockerignore file in cwd
-    # should match specific
-    with TemporaryDirectory(dir=test_cwd) as tmp_dir:
-        tmp_path = Path(tmp_dir)
-        dir1 = tmp_path / "dir1"
-        dir1.mkdir()
-
-        dockerfile_path = tmp_path / "Dockerfile"
-        generic_dockerignore_path = tmp_path / ".dockerignore"
-        generic_dockerignore_path.write_text("**/*.py")
-        specific_dockerignore_path = tmp_path / "Dockerfile.dockerignore"
-        specific_dockerignore_path.write_text("**/*")
-        assert find_dockerignore_file(test_cwd / tmp_dir, dockerfile_path) == specific_dockerignore_path
-        assert find_dockerignore_file(test_cwd / tmp_dir, dockerfile_path) != generic_dockerignore_path
-
-    # case 4:
-    # should not match nested dockerignore files
-    # or parent dockerignore files
-    # when dockerfile is in cwd
-    with TemporaryDirectory(dir=test_cwd) as tmp_dir:
-        tmp_path = Path(tmp_dir)
-        dir1 = tmp_path / "dir1"
-        dir1.mkdir()
-        dir2 = dir1 / "dir2"
-        dir2.mkdir()
-
-        dockerfile_path = dir1 / "Dockerfile"
-        dockerfile_path.write_text("COPY . /dummy")
-
-        # should ignore parent ones
-        generic_dockerignore_path = tmp_path / ".dockerignore"
-        generic_dockerignore_path.write_text("**/*")
-        specific_dockerignore_path = tmp_path / "Dockerfile.dockerignore"
-        specific_dockerignore_path.write_text("**/*")
-
-        # should ignore nested ones
-        nested_generic_dockerignore_path = dir2 / ".dockerignore"
-        nested_generic_dockerignore_path.write_text("**/*")
-        nested_specific_dockerignore_path = dir2 / "Dockerfile.dockerignore"
-        nested_specific_dockerignore_path.write_text("**/*")
-
-        assert find_dockerignore_file(dir1, dockerfile_path) is None
-
-    # case 5:
-    # should match dockerignore file next to dockerfile
-    # and not next to cwd if both exist
-    # even if more specific
-    with TemporaryDirectory(dir=test_cwd) as tmp_dir:
-        tmp_path = Path(tmp_dir)
-        dir1 = tmp_path / "dir1"
-        dir1.mkdir()
-
-        # os.chdir(context_directory)
-
-        dockerfile_path = dir1 / "Dockerfile"
-        dockerignore_path = tmp_path / ".dockerignore"
-        dockerignore_path.write_text("**/*")
-
-        assert find_dockerignore_file(test_cwd / tmp_dir, dockerfile_path) == dockerignore_path
 
 
 def create_tmp_files(tmp_path):
