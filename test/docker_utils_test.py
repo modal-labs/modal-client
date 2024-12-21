01a83c72bbb2a600052a60617abb72f12477d4af
Elias Freider <elias@modal.com>
2024-12-21 15:25:48 +0100

    Clean up more tests

--- test/docker_utils_test.py
+++ test/docker_utils_test.py
@@ -1,30 +1,28 @@
 # Copyright Modal Labs 2024
-import os
 import pytest
-import sys
-from pathlib import Path
 from tempfile import NamedTemporaryFile, TemporaryDirectory
 
 from modal._utils.docker_utils import extract_copy_command_patterns
 
 
-def test_extract_copy_command_patterns():
-    x = [
+@pytest.mark.parametrize(
+    ("copy_commands", "expected_patterns"),
+    [
         (
             ["CoPY files/dir1 ./smth_copy"],
-            ["files/dir1"],
+            {"files/dir1"},
         ),
         (
             ["COPY files/*.txt /dest/", "COPY files/**/*.py /dest/"],
-            ["files/*.txt", "files/**/*.py"],
+            {"files/*.txt", "files/**/*.py"},
         ),
         (
             ["COPY files/special/file[[]1].txt /dest/"],
-            ["files/special/file[[]1].txt"],
+            {"files/special/file[[]1].txt"},
         ),
         (
             ["COPY files/*.txt files/**/*.py /dest/"],
-            ["files/*.txt", "files/**/*.py"],
+            {"files/*.txt", "files/**/*.py"},
         ),
         (
             [
@@ -34,58 +32,18 @@ def test_extract_copy_command_patterns():
                 "./bar.py \\",
                 "/x",
             ],
-            ["./smth", "./foo.py", "./bar.py"],
+            {"./smth", "./foo.py", "./bar.py"},
         ),
-    ]
-
-    for dockerfile_lines, expected in x:
-        copy_command_sources = sorted(extract_copy_command_patterns(dockerfile_lines))
-        expected = sorted(expected)
-        assert copy_command_sources == expected
-
-
-def create_tmp_files(tmp_path):
-    (tmp_path / "dir1").mkdir()
-    (tmp_path / "dir1" / "a.txt").write_text("a")
-    (tmp_path / "dir1" / "b.txt").write_text("b")
-
-    (tmp_path / "dir2").mkdir()
-    (tmp_path / "dir2" / "test1.py").write_text("test1")
-    (tmp_path / "dir2" / "test2.py").write_text("test2")
-
-    (tmp_path / "file1.txt").write_text("content1")
-    (tmp_path / "file10.txt").write_text("content1")
-    (tmp_path / "file2.txt").write_text("content2")
-    (tmp_path / "test.py").write_text("python")
-
-    (tmp_path / "special").mkdir()
-    (tmp_path / "special" / "file[1].txt").write_text("special1")
-    (tmp_path / "special" / "file{2}.txt").write_text("special2")
-
-    if sys.platform != "win32":
-        (tmp_path / "special" / "test?file.py").write_text("special3")
-
-    (tmp_path / "this").mkdir()
-    (tmp_path / "this" / "is").mkdir()
-    (tmp_path / "this" / "is" / "super").mkdir()
-    (tmp_path / "this" / "is" / "super" / "nested").mkdir()
-    (tmp_path / "this" / "is" / "super" / "nested" / "file.py").write_text("python")
-
-    all_fps = []
-    for root, _, files in os.walk(tmp_path):
-        for file in files:
-            all_fps.append(f"{os.path.join(root, file)}".lstrip("./"))
-
-    return all_fps
+    ],
+)
+def test_extract_copy_command_patterns(copy_commands, expected_patterns):
+    copy_command_sources = set(extract_copy_command_patterns(copy_commands))
+    assert copy_command_sources == expected_patterns
 
 
 @pytest.mark.usefixture("tmp_cwd")
 def test_image_dockerfile_copy_messy():
     with TemporaryDirectory(dir="./") as tmp_dir:
-        tmp_path = Path(tmp_dir)
-
-        create_tmp_files(tmp_path)
-
         dockerfile = NamedTemporaryFile("w", delete=False)
         dockerfile.write(
             f"""
