767d5ed53812c1d6fc91c5d328d7194a73dc8f73
Kasper Vogel <kasper@modal.com>
2025-01-14 16:07:11 +0100

    type

--- test/file_pattern_matcher_test.py
+++ test/file_pattern_matcher_test.py
@@ -337,18 +337,13 @@ def test_invert_patterns(tmp_path_with_content):
 
 
 @pytest.mark.usefixtures("tmp_cwd")
-@pytest.mark.parametrize("use_path", [True, False])
-def test_from_file(use_path):
+@pytest.mark.parametrize("as_type", [Path, str])
+def test_from_file(as_type):
     rel_top_dir = Path("top")
     rel_top_dir.mkdir()
     ignore_file = rel_top_dir / "pattern_file"
     ignore_file.write_text("**/*.txt")
 
-    if use_path:
-        ignore_file = Path(ignore_file)
-    else:
-        ignore_file = str(ignore_file)
-
-    lff = FilePatternMatcher.from_file(ignore_file)
+    lff = FilePatternMatcher.from_file(as_type(ignore_file))
     assert lff(Path("top/data.txt"))
     assert not lff(Path("top/data.py"))
