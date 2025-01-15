2d672ab6697913cdffbff3c44ba7a0d41757922c
Elias Freider <elias@modal.com>
2025-01-15 14:44:10 +0100

    starting to remove Mount constructors from tests

--- test/supports/package_mount.py
+++ test/supports/package_mount.py
@@ -1,10 +1,14 @@
 # Copyright Modal Labs 2022
-from modal import App, Mount
+from modal import App, Image
 
 app = App()
 
+image = Image.debian_slim().add_local_python_source("module_1")
 
-@app.function()
+
+@app.function(image=image, serialized=True)
 def num_mounts(_x):
-    mount = Mount.from_local_python_packages("module_1")
+    print(image._mount_layers)
+    assert len(image._mount_layers) == 1
+    mount = image._mount_layers[0]
     return len(mount.entries)
