b230080c7c1e3f822d0f7dddfabbb003dbd098cc
Elias Freider <elias@modal.com>
2025-01-28 15:38:45 +0100

    Split functions into separate modules for implementation and synchronicity wrapper

---
+++ modal/functions.py
@@ -0,0 +1,6 @@
+from ._functions import _Function, _FunctionCall, _gather
+from ._utils.async_utils import synchronize_api
+
+Function = synchronize_api(_Function, target_module=__name__)
+FunctionCall = synchronize_api(_FunctionCall, target_module=__name__)
+gather = synchronize_api(_gather, target_module=__name__)
