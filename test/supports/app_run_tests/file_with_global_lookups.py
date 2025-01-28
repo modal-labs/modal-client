8e99c23604ed61af16f7018b73c4cc0afa6ef179
Elias Freider <elias@modal.com>
2025-01-28 08:43:16 +0100

    Fix and test for Function.from_name in module scope preventing modal run

---
+++ test/supports/app_run_tests/file_with_global_lookups.py
@@ -0,0 +1,11 @@
+import modal
+
+remote_func = modal.Function.from_name("app", "some_func")
+remote_cls = modal.Cls.from_name("app", "some_class")
+
+app = modal.App()
+
+
+@app.function()
+def local_f():
+    pass
