9e5aecd0e4239c4ef23b30accd2f193e6cdbad30
Elias Freider <freider@users.noreply.github.com>
2025-02-06 11:27:59 +0100

    Use parameter info to validate parameters on instance creation (#2826)
    * Use parameter info to validate parameters on instance creation
    
    This also fixes the incorrect assumption that the base/parent service for a class
    can be used whenever the calling arguments are empty, by instead checking if
    the arguments can be serialized and if they match the defaults

--- tasks.py
+++ tasks.py
@@ -128,6 +128,7 @@ def type_check(ctx):
         "venv39",
         "venv38",
         "test/cls_test.py",  # blocked by mypy bug: https://github.com/python/mypy/issues/16527
+        "test/supports/sibling_hydration_app.py",  # blocked by mypy bug: https://github.com/python/mypy/issues/16527
         "test/supports/type_assertions_negative.py",
     ]
     excludes = " ".join(f"--exclude {path}" for path in mypy_exclude_list)
