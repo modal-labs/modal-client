812b920553aa0221b0631a849016ae4053e79f63
Michael Waskom <michael@modal.com>
2025-02-20 20:48:26 +0000

    Update parameterization in vscode app

--- modal/_functions.py
+++ modal/_functions.py
@@ -1087,7 +1087,15 @@ class _Function(typing.Generic[P, ReturnType, OriginalReturnType], _Object, type
             """
                 )
             )
-        # TODO deprecation warn
+
+        deprecation_warning(
+            (2025, 2, 24),
+            "The .keep_warm() method has been deprecated in favor of the more general "
+            ".update_autoscaler(min_containers=...) method.",
+            pending=True,
+            show_source=True,
+        )
+
         await self.update_autoscaler(min_containers=warm_pool_size)
 
     @classmethod
