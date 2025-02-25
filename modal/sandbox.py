f8b59454a74c77dc51348385e9734ec1ef6ee648
Michael Waskom <michael@modal.com>
2025-02-25 23:24:51 +0000

    Fix docs for objects defined in submodules

--- modal/sandbox.py
+++ modal/sandbox.py
@@ -520,8 +520,10 @@ class _Sandbox(_Object, type_prefix="sb"):
         # Internal option to set terminal size and metadata
         _pty_info: Optional[api_pb2.PTYInfo] = None,
     ):
-        """Execute a command in the Sandbox and return
-        a [`ContainerProcess`](/docs/reference/modal.ContainerProcess#modalcontainer_process) handle.
+        """Execute a command in the Sandbox and return a ContainerProcess handle.
+
+        See the [`ContainerProcess`](/docs/reference/modal.container_process#modalcontainer_processcontainerprocess)
+        docs for more information.
 
         **Usage**
 
@@ -623,8 +625,9 @@ class _Sandbox(_Object, type_prefix="sb"):
         path: str,
         mode: Union["_typeshed.OpenTextMode", "_typeshed.OpenBinaryMode"] = "r",
     ):
-        """Open a file in the Sandbox and return
-        a [`FileIO`](/docs/reference/modal.FileIO#modalfile_io) handle.
+        """Open a file in the Sandbox and return a FileIO handle.
+
+        See the [`FileIO`](/docs/reference/modal.file_io#modalfile_iofileio) docs for more information.
 
         **Usage**
 
