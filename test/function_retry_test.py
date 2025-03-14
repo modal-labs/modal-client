7e0ff9998b2075e1f7d2ad5eb6fd865bda08601f
Ryan Culbertson <rculbertson@gmail.com>
2025-03-14 13:18:37 -0400

    Add flag to enable/disable client retries (#2944)

--- test/function_retry_test.py
+++ test/function_retry_test.py
@@ -51,7 +51,8 @@ def setup_app_and_function(servicer):
     return app, f
 
 
-def test_all_retries_fail_raises_error(client, setup_app_and_function, monkeypatch):
+def test_all_retries_fail_raises_error(client, setup_app_and_function, monkeypatch, servicer):
+    servicer.sync_client_retries_enabled = True
     monkeypatch.setenv("MODAL_CLIENT_RETRIES", "true")
     app, f = setup_app_and_function
     with app.run(client=client):
@@ -62,7 +63,8 @@ def test_all_retries_fail_raises_error(client, setup_app_and_function, monkeypat
         assert exc_info.value.function_call_count == 4
 
 
-def test_failures_followed_by_success(client, setup_app_and_function, monkeypatch):
+def test_failures_followed_by_success(client, setup_app_and_function, monkeypatch, servicer):
+    servicer.sync_client_retries_enabled = True
     monkeypatch.setenv("MODAL_CLIENT_RETRIES", "true")
     app, f = setup_app_and_function
     with app.run(client=client):
@@ -70,13 +72,24 @@ def test_failures_followed_by_success(client, setup_app_and_function, monkeypatc
         assert function_call_count == 3
 
 
-def test_no_retries_when_first_call_succeeds(client, setup_app_and_function, monkeypatch):
+def test_no_retries_when_first_call_succeeds(client, setup_app_and_function, monkeypatch, servicer):
+    servicer.sync_client_retries_enabled = True
     monkeypatch.setenv("MODAL_CLIENT_RETRIES", "true")
     app, f = setup_app_and_function
     with app.run(client=client):
         function_call_count = f.remote(1)
         assert function_call_count == 1
 
+def test_no_retries_when_client_retries_disabled(client, setup_app_and_function, monkeypatch, servicer):
+    servicer.sync_client_retries_enabled = False
+    # Set MODAL_CLIENT_RETRIES to true to use SYNC, rather than SYNC_LEGACY. This tests the sync_client_retries_enabled
+    # flag is honored even when using SYNC.
+    monkeypatch.setenv("MODAL_CLIENT_RETRIES", "true")
+    app, f = setup_app_and_function
+    with app.run(client=client):
+        with pytest.raises(FunctionCallCountException) as exc_info:
+            f.remote(2)
+        assert exc_info.value.function_call_count == 1
 
 def test_retry_dealy_ms():
     with pytest.raises(ValueError):
@@ -90,6 +103,7 @@ def test_retry_dealy_ms():
 
 
 def test_lost_inputs_retried(client, setup_app_and_function, monkeypatch, servicer):
+    servicer.sync_client_retries_enabled = True
     monkeypatch.setenv("MODAL_CLIENT_RETRIES", "true")
     app, f = setup_app_and_function
     # The client should retry if it receives a internal failure status.
