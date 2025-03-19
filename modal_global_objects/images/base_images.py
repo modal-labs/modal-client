f8b8d77092a51275a5a0e07be68a2c13e3cf04e2
Eric Zhang <ekzhang1@gmail.com>
2025-03-19 22:02:07 +0000

    Add image IDs / output to table

--- modal_global_objects/images/base_images.py
+++ modal_global_objects/images/base_images.py
@@ -3,6 +3,9 @@ import os
 import sys
 from typing import cast
 
+import rich
+from rich.table import Table
+
 import modal
 from modal.image import SUPPORTED_PYTHON_SERIES, ImageBuilderVersion
 
@@ -20,9 +23,22 @@ if __name__ == "__main__":
     python_versions = SUPPORTED_PYTHON_SERIES[cast(ImageBuilderVersion, builder_version)]
 
     app = modal.App(f"build-{name.replace('_', '-')}-image")
+    images_map: dict[str, modal.Image] = {}
     for v in python_versions:
-        app.function(image=constructor(python_version=v), name=f"{v}")(dummy)
+        image = constructor(python_version=v)
+        images_map[v] = image
+        app.function(image=image, name=f"{v}")(dummy)
 
     with modal.enable_output():
         with app.run():
             pass
+
+    table = Table(title=f"Images for {name} ({builder_version})")
+    table.add_column("Python version")
+    table.add_column("Image ID")
+
+    for v, image in images_map.items():
+        table.add_row(v, image.object_id)
+
+    rich.print()
+    rich.print(table)
