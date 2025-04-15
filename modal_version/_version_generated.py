9deeea0e1a813692a0b145ff7e02b9f673a3ec68
Elias Freider <freider@users.noreply.github.com>
2025-04-15 16:06:02 +0200

    Adds deprecation warning when using constructors on Modal class (#2936)
    * Adds deprecation warning when using constructors on Modal class
    
    * Fixes
    
    * Fix cls tests that were previously using explicit constructors
    
    * Bump date
    
    * Bump minor version
    
    * Remove dedent
    
    * Reset patch version

--- modal_version/_version_generated.py
+++ modal_version/_version_generated.py
@@ -1,4 +1,5 @@
 # Copyright Modal Labs 2025
 
 # Note: Reset this value to -1 whenever you make a minor `0.X` release of the client.
-build_number = 173  # git: 69da997
+build_number = -1  # git: b0e5f86
+
