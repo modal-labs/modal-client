4670400651932d7b3224a98b006ff7815915b7e5
Elias Freider <freider@users.noreply.github.com>
2025-04-15 16:06:02 +0200

    Adds deprecation warning when using constructors on Modal class (#2936)
    * Adds deprecation warning when using constructors on Modal class
    
    * Fixes
    
    * Fix cls tests that were previously using explicit constructors
    
    * Bump date
    
    * Bump minor version
    
    * Remove dedent
    
    * Reset patch version

--- modal/cls.py
+++ modal/cls.py
@@ -455,7 +455,22 @@ class _Cls(_Object, type_prefix="cs"):
                 "A class can't have both a custom __init__ constructor "
                 "and dataclass-style modal.parameter() annotations"
             )
+        elif has_custom_constructor:
+            deprecation_warning(
+                (2025, 4, 15),
+                f"""
+{user_cls} uses a non-default constructor (__init__) method.
+Custom constructors will not be supported in a a future version of Modal.
 
+To parameterize classes, use dataclass-style modal.parameter() declarations instead,
+e.g.:\n
+
+class {user_cls.__name__}:
+    model_name: str = modal.parameter()
+
+More information on class parameterization can be found here: https://modal.com/docs/guide/parametrized-functions
+""",
+            )
         annotations = user_cls.__dict__.get("__annotations__", {})  # compatible with older pythons
         missing_annotations = params.keys() - annotations.keys()
         if missing_annotations:
