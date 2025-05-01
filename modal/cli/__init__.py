a8bd541ae685712b1e1ea1d79e6c9e8efbdc8dbe
Michael Waskom <mwaskom@users.noreply.github.com>
2025-05-01 14:59:31 -0400

    Add guidance about not using the CLI implementation as API (#3075)

--- modal/cli/__init__.py
+++ modal/cli/__init__.py
@@ -1,1 +1,16 @@
 # Copyright Modal Labs 2022
+"""
+The `modal.cli` package contains the Python implementations for Modal's CLI.
+
+The functions defined in this package are intended to be called via the CLI,
+not from Python code. No backwards compatibility guarantees are made with
+respect to Python calling patterns (e.g., the order of parameters corresponding
+to CLI options may change in the Python signature without warning).
+
+Any utility or helper functions defined in this package are intended for use by
+the CLI commands and are not for external consumption; they should be
+considered private even if they do not use a private naming convention.
+
+Over time, we aspire to support feature parity between the CLI and the public
+Python API, although this remains a work in progress.
+"""
