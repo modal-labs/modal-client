4806fea9357bfd00a0b38ccc308bab6a8ac5766d
Michael Waskom <michael@modal.com>
2025-05-08 14:11:24 +0000

    Add test for Dockerfile methods with relative ignore pattern

--- test/image_test.py
+++ test/image_test.py
@@ -906,6 +906,47 @@ def test_image_dockerfile_copy_ignore_from_file(builder_version, servicer, clien
         }
 
 
+@pytest.mark.parametrize("use_dockerfile", (True, False))
+@pytest.mark.usefixtures("tmp_cwd")
+def test_image_dockerfile_ignore_context_dir(builder_version, servicer, client, use_dockerfile):
+    rel_top_dir = Path("top")
+    rel_top_dir.mkdir()
+    (rel_top_dir / "data.txt").write_text("world")
+    (rel_top_dir / "file.py").write_text("world")
+
+    sub_dir = rel_top_dir / "sub"
+    sub_dir.mkdir()
+    (sub_dir / "notes.txt").write_text("whatever")
+
+    docker_cmd = "COPY . /dummy"
+    dockerfile = rel_top_dir / "Dockerfile"
+    dockerfile.write_text(docker_cmd + "\n")
+
+    dockerignore = rel_top_dir / ".dockerignore"
+    dockerignore.write_text("*.txt")
+
+    app = App()
+    if use_dockerfile:
+        image = Image.debian_slim().from_dockerfile(dockerfile, context_dir=rel_top_dir)
+        layer = 1
+    else:
+        image = Image.debian_slim().dockerfile_commands([docker_cmd], context_dir=rel_top_dir)
+        layer = 0
+    app.function(image=image)(dummy)
+
+    with app.run(client=client):
+        layers = get_image_layers(image.object_id, servicer)
+        assert docker_cmd in layers[layer].dockerfile_commands
+        mount_id = layers[layer].context_mount_id
+        files = set(Path(fn) for fn in servicer.mount_contents[mount_id].keys())
+        assert files == {
+            Path("/") / "Dockerfile",
+            Path("/") / ".dockerignore",
+            Path("/") / "file.py",
+            Path("/") / "sub" / "notes.txt",
+        }
+
+
 @pytest.mark.parametrize("use_callable", (True, False))
 @pytest.mark.parametrize("use_dockerfile", (True, False))
 @pytest.mark.usefixtures("tmp_cwd")
