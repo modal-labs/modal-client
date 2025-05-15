45a7d9edb1201740c5a31b64c088c4d0c720c18e
Elias Freider <freider@users.noreply.github.com>
2025-05-15 15:24:12 +0200

    Removes automount helper logic that isn't used anymore (#3147)

--- modal/mount.py
+++ modal/mount.py
@@ -5,9 +5,6 @@ import concurrent.futures
 import dataclasses
 import os
 import re
-import site
-import sys
-import sysconfig
 import time
 import typing
 import warnings
@@ -32,7 +29,7 @@ from ._utils.name_utils import check_object_name
 from ._utils.package_utils import get_module_mount_info
 from .client import _Client
 from .config import config, logger
-from .exception import ExecutionError, InvalidError, ModuleNotMountable
+from .exception import ExecutionError, InvalidError
 from .file_pattern_matcher import FilePatternMatcher
 
 ROOT_DIR: PurePosixPath = PurePosixPath("/root")
@@ -800,22 +797,6 @@ def _get_client_mount():
         return _Mount.from_name(client_mount_name(), namespace=api_pb2.DEPLOYMENT_NAMESPACE_GLOBAL)
 
 
-SYS_PREFIXES = {
-    Path(p)
-    for p in (
-        sys.prefix,
-        sys.base_prefix,
-        sys.exec_prefix,
-        sys.base_exec_prefix,
-        *sysconfig.get_paths().values(),
-        *site.getsitepackages(),
-        site.getusersitepackages(),
-    )
-}
-
-
-SYS_PREFIXES |= {p.resolve() for p in SYS_PREFIXES}
-
 MODAL_PACKAGES = ["modal", "modal_proto", "modal_version"]
 
 
@@ -829,45 +810,3 @@ def _is_modal_path(remote_path: PurePosixPath):
         if is_modal_path:
             return True
     return False
-
-
-def get_sys_modules_mounts() -> dict[str, _Mount]:
-    """mdmd:hidden
-
-    Auto-mount local modules that have been imported in global scope.
-    This may or may not include the "entrypoint" of the function as well, depending on how modal is invoked
-    Note: sys.modules may change during the iteration
-    """
-    auto_mounts = {}
-    top_level_modules = []
-    skip_prefixes = set()
-    for name, module in sorted(sys.modules.items(), key=lambda kv: len(kv[0])):
-        parent = name.rsplit(".")[0]
-        if parent and parent in skip_prefixes:
-            skip_prefixes.add(name)
-            continue
-        skip_prefixes.add(name)
-        top_level_modules.append((name, module))
-
-    for module_name, module in top_level_modules:
-        if module_name.startswith("__"):
-            # skip "built in" modules like __main__ and __mp_main__
-            # the running function's main file should be included anyway
-            continue
-
-        try:
-            # at this point we don't know if the sys.modules module should be mounted or not
-            potential_mount = _Mount._from_local_python_packages(module_name)
-            mount_paths = potential_mount._top_level_paths()
-        except ModuleNotMountable:
-            # this typically happens if the module is a built-in, has binary components or doesn't exist
-            continue
-
-        for local_path, remote_path in mount_paths:
-            if any(local_path.is_relative_to(p) for p in SYS_PREFIXES) or _is_modal_path(remote_path):
-                # skip any module that has paths in SYS_PREFIXES, or would overwrite the modal Package in the container
-                break
-        else:
-            auto_mounts[module_name] = potential_mount
-
-    return auto_mounts
