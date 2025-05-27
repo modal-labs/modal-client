b857fc040430ca7d4399bba7e911656060a3ea7d
Mats Linander <mats@modal.com>
2025-05-27 15:41:52 +0000

    cope w version mock overwriting version list

--- test/conftest.py
+++ test/conftest.py
@@ -915,7 +915,13 @@ class MockClientServicer(api_grpc.ModalClientBase):
         else:
             environment_id = f"en-{len(self.environments) + 1}"
             self.environments[name] = environment_id
-        image_builder_version = max([v for v in get_args(ImageBuilderVersion) if v != "PREVIEW"])
+
+        versions = get_args(ImageBuilderVersion)
+        if len(versions) == 1:
+            image_builder_version = versions[0]
+        else:
+            image_builder_version = max([v for v in versions if v != "PREVIEW"])
+
         settings = api_pb2.EnvironmentSettings(image_builder_version=image_builder_version)
         metadata = api_pb2.EnvironmentMetadata(name=name, settings=settings)
         await stream.send_message(
