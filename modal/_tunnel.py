e83202f999f5c1755b1c1050fc6996472f964254
Ryan Nguyen <ryan.nguyen@modal.com>
2025-05-29 21:01:22 +0000

    fix

--- modal/_tunnel.py
+++ modal/_tunnel.py
@@ -52,7 +52,7 @@ class Tunnel:
 
 @asynccontextmanager
 async def _forward(
-    port: int, *, unencrypted: bool = False, tunnel_type: str = None, client: Optional[_Client] = None
+    port: int, *, unencrypted: bool = False, tunnel_type: Optional[api_pb2.TunnelType] = None, client: Optional[_Client] = None
 ) -> AsyncIterator[Tunnel]:
     """Expose a port publicly from inside a running Modal container, with TLS.
 
@@ -179,7 +179,7 @@ async def _forward(
 
     try:
         response = await client.stub.TunnelStart(
-            api_pb2.TunnelStartRequest(port=port, unencrypted=unencrypted, tunnel_type=api_pb2.TUNNEL_TYPE_H2)
+            api_pb2.TunnelStartRequest(port=port, unencrypted=unencrypted, tunnel_type=tunnel_type)
         )
     except GRPCError as exc:
         if exc.status == Status.ALREADY_EXISTS:
