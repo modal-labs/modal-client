9a80f68bffcd11a682d88da470c385ecc1cc012f
Mats Linander <mats@modal.com>
2025-05-30 19:34:45 +0000

    unused imports

--- modal_global_objects/mounts/modal_client_dependencies.py
+++ modal_global_objects/mounts/modal_client_dependencies.py
@@ -1,14 +1,7 @@
 # Copyright Modal Labs 2025
 import asyncio
-import subprocess
-import tempfile
 
-from modal.client import Client
-from modal.config import config
-
-from modal.image import ImageBuilderVersion, _get_modal_requirements_path
 from modal.mount import _create_client_dependency_mounts
-from modal_proto import api_pb2
 
 
 def main():
