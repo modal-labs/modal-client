4dda8daef349d9a64c2d6bf62f878b382343b3bb
Cyril Sharma <44011901+CyrilSharma@users.noreply.github.com>
2025-06-25 11:40:50 -0400

    Move blob uploads on async function calls to the client (#3221)
    Move async uploads from server to client

---
+++ test/should_upload_test.py
@@ -0,0 +1,39 @@
+# Copyright Modal Labs 2025
+from dataclasses import dataclass
+
+from modal._utils.blob_utils import MAX_ASYNC_OBJECT_SIZE_BYTES, MAX_OBJECT_SIZE_BYTES
+from modal._utils.function_utils import should_upload
+from modal_proto import api_pb2
+
+
+@dataclass
+class Input:
+    size: int
+    is_async: bool
+    should_use_blob: bool
+
+
+def test_should_upload():
+    SMALL = MAX_ASYNC_OBJECT_SIZE_BYTES // 2
+    LARGE = MAX_ASYNC_OBJECT_SIZE_BYTES * 2
+    HUGE = MAX_OBJECT_SIZE_BYTES * 2
+    test_cases = [
+        Input(size=SMALL, is_async=True, should_use_blob=False),
+        Input(size=SMALL, is_async=False, should_use_blob=False),
+        Input(size=LARGE, is_async=True, should_use_blob=True),
+        Input(size=LARGE, is_async=False, should_use_blob=False),
+        Input(size=HUGE, is_async=True, should_use_blob=True),
+        Input(size=HUGE, is_async=False, should_use_blob=True),
+    ]
+
+    for case in test_cases:
+        used_blob = should_upload(
+            case.size,
+            function_call_invocation_type=(
+                api_pb2.FUNCTION_CALL_INVOCATION_TYPE_ASYNC
+                if case.is_async
+                else api_pb2.FUNCTION_CALL_INVOCATION_TYPE_SYNC
+            ),
+        )
+
+        assert used_blob == case.should_use_blob
