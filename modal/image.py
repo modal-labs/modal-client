24c4e4b72c1f93c8bab05f16d88f63e164dc715b
Eric Zhang <ekzhang1@gmail.com>
2025-07-31 13:14:42 -0400

    Better error message in uv_sync (#3401)
    * Better error message in uv_sync
    
    * Move KeyError up
    
    * Use normal repr
    
    ---------
    
    Co-authored-by: Thomas J. Fan <thomas@modal.com>

--- modal/image.py
+++ modal/image.py
@@ -1393,7 +1393,13 @@ class _Image(_Object, type_prefix="im"):
                 # a requirement in `uv.lock`
                 return
 
-            dependencies = pyproject_toml_content["project"]["dependencies"]
+            try:
+                dependencies = pyproject_toml_content["project"]["dependencies"]
+            except KeyError as e:
+                raise InvalidError(
+                    f"Invalid pyproject.toml file: missing key {e} in {pyproject_toml}. "
+                    "See https://packaging.python.org/en/latest/guides/writing-pyproject-toml for guidelines."
+                )
 
             for group in groups:
                 if (
@@ -1459,7 +1465,7 @@ class _Image(_Object, type_prefix="im"):
                 commands.append(f"COPY /.uv.lock {UV_ROOT}/uv.lock")
 
                 if frozen:
-                    # Do not update `uv.lock` when we have one when `frozen=True`. This it ehd efault because this
+                    # Do not update `uv.lock` when we have one when `frozen=True`. This is the default because this
                     # ensures that the runtime environment matches the local `uv.lock`.
                     #
                     # If `frozen=False`, then `uv sync` will update the the dependencies in the `uv.lock` file
