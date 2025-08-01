1adb775ca66f5640e98cd3b5d38968d2f3367465
Eric Zhang <ekzhang1@gmail.com>
2025-08-01 13:02:15 -0400

    Show message about Notebooks in launch jupyter (#3416)

--- modal/cli/launch.py
+++ modal/cli/launch.py
@@ -6,8 +6,10 @@ import os
 from pathlib import Path
 from typing import Any, Optional
 
+import rich.panel
 from typer import Typer
 
+from .._output import make_console
 from ..exception import _CliUserExecutionError
 from ..output import enable_output
 from ..runner import run_app
@@ -61,6 +63,17 @@ def jupyter(
     volume: Optional[str] = None,  # Attach a persisted `modal.Volume` by name (creating if missing).
     detach: bool = False,  # Run the app in "detached" mode to persist after local client disconnects
 ):
+    console = make_console()
+    console.print(
+        rich.panel.Panel(
+            (
+                "[link=https://modal.com/notebooks]Try Modal Notebooks! "
+                "modal.com/notebooks[/link]\n"
+                "Notebooks have a new UI, saved content, real-time collaboration and more."
+            ),
+        ),
+        style="bold cyan",
+    )
     args = {
         "cpu": cpu,
         "memory": memory,
