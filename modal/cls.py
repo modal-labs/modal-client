bfc0c4092f07fddc083de60a473de9012241c435
Emily Han <emily@modal.com>
2025-08-21 23:10:33 +0000

    fix type stubs error

--- modal/cls.py
+++ modal/cls.py
@@ -12,7 +12,7 @@ from grpclib import GRPCError, Status
 from modal_proto import api_pb2
 
 from ._functions import _Function, _parse_retries
-from ._object import _Object
+from ._object import _Object, live_method
 from ._partial_function import (
     _find_callables_for_obj,
     _find_partial_methods_for_user_cls,
@@ -902,7 +902,7 @@ More information on class parameterization can be found here: https://modal.com/
     def _is_local(self) -> bool:
         return self._user_cls is not None
 
-    @synchronizer.create_blocking
+    @live_method
     async def get_flash_url(self) -> Optional[str]:
         """
         Get the stable Flash service URL for this class.
@@ -912,8 +912,6 @@ More information on class parameterization can be found here: https://modal.com/
         Model = modal.Cls.from_name("my-app", "Model")
         flash_url = Model.get_flash_url()
         """
-        await self.hydrate()
-
         service_function = self._get_class_service_function()
         function_id = service_function.object_id
         if not function_id:
