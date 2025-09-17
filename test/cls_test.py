e51a2fa64e5a26f25458798d8d3593249a5b4e74
Elias Freider <elias@modal.com>
2025-09-17 13:07:23 +0200

    Fix more

--- test/cls_test.py
+++ test/cls_test.py
@@ -107,8 +107,8 @@ def test_run_class(client, servicer):
                     base_type=api_pb2.ParameterType.PARAM_TYPE_UNKNOWN,
                 ),
             ),
-            supported_output_formats=[api_pb2.DATA_FORMAT_PICKLE],
-            supported_input_formats=[api_pb2.DATA_FORMAT_PICKLE],
+            supported_output_formats=[api_pb2.DATA_FORMAT_PICKLE, api_pb2.DATA_FORMAT_CBOR],
+            supported_input_formats=[api_pb2.DATA_FORMAT_PICKLE, api_pb2.DATA_FORMAT_CBOR],
         ),
         "baz": api_pb2.MethodDefinition(
             function_name="Foo.baz",
@@ -127,8 +127,8 @@ def test_run_class(client, servicer):
                     base_type=api_pb2.ParameterType.PARAM_TYPE_UNKNOWN,
                 ),
             ),
-            supported_output_formats=[api_pb2.DATA_FORMAT_PICKLE],
-            supported_input_formats=[api_pb2.DATA_FORMAT_PICKLE],
+            supported_output_formats=[api_pb2.DATA_FORMAT_PICKLE, api_pb2.DATA_FORMAT_CBOR],
+            supported_input_formats=[api_pb2.DATA_FORMAT_PICKLE, api_pb2.DATA_FORMAT_CBOR],
         ),
     }
 
