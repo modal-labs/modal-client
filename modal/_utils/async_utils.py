b683c70ace636ba524a6d9097380b98bf3d61e36
Thomas J. Fan <thomas@modal.com>
2025-10-30 14:35:44 +0000

    Use nonlocal instead

--- modal/_utils/async_utils.py
+++ modal/_utils/async_utils.py
@@ -79,13 +79,12 @@ def retry(direct_fn=None, *, n_attempts=3, base_delay=0, delay_factor=2, timeout
     def decorator(fn):
         @functools.wraps(fn)
         async def f_wrapped(*args, **kwargs):
+            nonlocal n_attempts
             if RETRY_N_ATTEMPTS_OVERRIDE is not None:
-                local_n_attempts = RETRY_N_ATTEMPTS_OVERRIDE
-            else:
-                local_n_attempts = n_attempts
+                n_attempts = RETRY_N_ATTEMPTS_OVERRIDE
 
             delay = base_delay
-            for i in range(local_n_attempts):
+            for i in range(n_attempts):
                 t0 = time.time()
                 try:
                     return await asyncio.wait_for(fn(*args, **kwargs), timeout=timeout)
@@ -93,12 +92,12 @@ def retry(direct_fn=None, *, n_attempts=3, base_delay=0, delay_factor=2, timeout
                     logger.debug(f"Function {fn} was cancelled")
                     raise
                 except Exception as e:
-                    if i >= local_n_attempts - 1:
+                    if i >= n_attempts - 1:
                         raise
                     logger.debug(
                         f"Failed invoking function {fn}: {e}"
                         f" (took {time.time() - t0}s, sleeping {delay}s"
-                        f" and trying {local_n_attempts - i - 1} more times)"
+                        f" and trying {n_attempts - i - 1} more times)"
                     )
                 await asyncio.sleep(delay)
                 delay *= delay_factor
