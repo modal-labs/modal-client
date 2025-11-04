349ec17ae06c40abfc454ae9cfb736fed53ef89e
Matthew Saltz <matthew@modal.com>
2025-11-04 16:39:09 +0000

    Include servicer in test

--- test/sandbox_test.py
+++ test/sandbox_test.py
@@ -548,7 +548,7 @@ def test_sandbox_exec_with_streamtype_stdout_read_from_stdout_raises_error(app, 
 
 @pytest.mark.asyncio
 @pytest.mark.parametrize("exec_backend", ["router"], indirect=True)
-async def test_sandbox_exec_stdout_requires_text_true_router_backend(app, exec_backend):
+async def test_sandbox_exec_stdout_requires_text_true_router_backend(app, servicer, exec_backend):
     sb = await Sandbox.create.aio(app=app)
 
     with pytest.raises(ValueError, match="only supported when text=True"):
