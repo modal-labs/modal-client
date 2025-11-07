90d9a38213b03dcd2a2b5ae35bdac7abbadc181c
Elias Freider <freider@users.noreply.github.com>
2025-11-07 18:02:48 +0100

    Fix Secret.from_dotenv lazy hydration when no app_id exists (#3683)

--- modal/secret.py
+++ modal/secret.py
@@ -206,6 +206,33 @@ class _SecretManager:
 SecretManager = synchronize_api(_SecretManager)
 
 
+async def _load_from_env_dict(instance: "_Secret", load_context: LoadContext, env_dict: dict[str, str]):
+    """helper method for loaders .from_dict and .from_dotenv etc."""
+    if load_context.app_id is not None:
+        req = api_pb2.SecretGetOrCreateRequest(
+            object_creation_type=api_pb2.OBJECT_CREATION_TYPE_ANONYMOUS_OWNED_BY_APP,
+            env_dict=env_dict,
+            app_id=load_context.app_id,
+            environment_name=load_context.environment_name,
+        )
+    else:
+        req = api_pb2.SecretGetOrCreateRequest(
+            object_creation_type=api_pb2.OBJECT_CREATION_TYPE_EPHEMERAL,
+            env_dict=env_dict,
+            environment_name=load_context.environment_name,
+        )
+
+    try:
+        resp = await load_context.client.stub.SecretGetOrCreate(req)
+    except GRPCError as exc:
+        if exc.status == Status.INVALID_ARGUMENT:
+            raise InvalidError(exc.message)
+        if exc.status == Status.FAILED_PRECONDITION:
+            raise InvalidError(exc.message)
+        raise
+    instance._hydrate(resp.secret_id, load_context.client, resp.metadata)
+
+
 class _Secret(_Object, type_prefix="st"):
     """Secrets provide a dictionary of environment variables for images.
 
@@ -263,26 +290,7 @@ class _Secret(_Object, type_prefix="st"):
         async def _load(
             self: _Secret, resolver: Resolver, load_context: LoadContext, existing_object_id: Optional[str]
         ):
-            if load_context.app_id is not None:
-                object_creation_type = api_pb2.OBJECT_CREATION_TYPE_ANONYMOUS_OWNED_BY_APP
-            else:
-                object_creation_type = api_pb2.OBJECT_CREATION_TYPE_EPHEMERAL
-
-            req = api_pb2.SecretGetOrCreateRequest(
-                object_creation_type=object_creation_type,
-                env_dict=env_dict_filtered,
-                app_id=load_context.app_id,
-                environment_name=load_context.environment_name,
-            )
-            try:
-                resp = await load_context.client.stub.SecretGetOrCreate(req)
-            except GRPCError as exc:
-                if exc.status == Status.INVALID_ARGUMENT:
-                    raise InvalidError(exc.message)
-                if exc.status == Status.FAILED_PRECONDITION:
-                    raise InvalidError(exc.message)
-                raise
-            self._hydrate(resp.secret_id, load_context.client, resp.metadata)
+            await _load_from_env_dict(self, load_context, env_dict_filtered)
 
         rep = f"Secret.from_dict([{', '.join(env_dict.keys())}])"
         # TODO: scoping - these should probably not be lazily hydrated without having an app and/or sandbox association
@@ -306,7 +314,7 @@ class _Secret(_Object, type_prefix="st"):
         return _Secret.from_dict({})
 
     @staticmethod
-    def from_dotenv(path=None, *, filename=".env") -> "_Secret":
+    def from_dotenv(path=None, *, filename=".env", client: Optional[_Client] = None) -> "_Secret":
         """Create secrets from a .env file automatically.
 
         If no argument is provided, it will use the current working directory as the starting
@@ -360,19 +368,12 @@ class _Secret(_Object, type_prefix="st"):
                 # To simplify this, we just support the cwd and don't do any automatic path inference.
                 dotenv_path = find_dotenv(filename, usecwd=True)
 
-            env_dict = dotenv_values(dotenv_path)
+            env_dict = {k: v or "" for k, v in dotenv_values(dotenv_path).items()}
 
-            req = api_pb2.SecretGetOrCreateRequest(
-                object_creation_type=api_pb2.OBJECT_CREATION_TYPE_ANONYMOUS_OWNED_BY_APP,
-                env_dict=env_dict,
-                app_id=load_context.app_id,  # TODO: what if app_id isn't set here (e.g. .hydrate())
-            )
-            resp = await load_context.client.stub.SecretGetOrCreate(req)
-
-            self._hydrate(resp.secret_id, load_context.client, resp.metadata)
+            await _load_from_env_dict(self, load_context, env_dict)
 
         return _Secret._from_loader(
-            _load, "Secret.from_dotenv()", hydrate_lazily=True, load_context_overrides=LoadContext.empty()
+            _load, "Secret.from_dotenv()", hydrate_lazily=True, load_context_overrides=LoadContext(client=client)
         )
 
     @staticmethod
