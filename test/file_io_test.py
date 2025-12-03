4a098677e5be115f36bd7953f2b09a943df659ac
Amit Prasad <amit@modal.com>
2025-12-03 18:04:30 +0000

    remove file i/o tests for write/delete bytes

--- test/file_io_test.py
+++ test/file_io_test.py
@@ -11,8 +11,6 @@ from modal.file_io import (  # type: ignore
     FileIO,
     FileWatchEvent,
     FileWatchEventType,
-    delete_bytes,
-    replace_bytes,
 )
 from modal_proto import api_pb2
 
@@ -308,56 +306,6 @@ def test_file_seek(servicer, client):
         for i in range(4):
             f.seek(3)
             assert f.read() == expected_outputs[i]
-        f.close()
-
-
-def test_file_write_replace_bytes(servicer, client):
-    """Test file write replace bytes."""
-    index = 0
-    expected_outputs = ["foo\nbar\nbaz\n", "foo\nbarbar\nbaz\n"]
-
-    async def container_filesystem_exec_get_output(servicer, stream):
-        nonlocal index
-        req = await stream.recv_message()
-        if req.exec_id == READ_EXEC_ID:
-            await stream.send_message(api_pb2.FilesystemRuntimeOutputBatch(output=[expected_outputs[index].encode()]))
-            index += 1
-        await stream.send_message(api_pb2.FilesystemRuntimeOutputBatch(eof=True))
-
-    with servicer.intercept() as ctx:
-        ctx.set_responder("ContainerFilesystemExec", container_filesystem_exec)
-        ctx.set_responder("ContainerFilesystemExecGetOutput", container_filesystem_exec_get_output)
-
-        f = FileIO.create("/test.txt", "a+", client, "task-123")
-        f.write("foo\nbar\nbaz\n")
-        assert f.read() == expected_outputs[0]
-        replace_bytes(f, data=b"barbar", start=4, end=7)
-        assert f.read() == expected_outputs[1]
-        f.close()
-
-
-def test_file_delete_bytes(servicer, client):
-    """Test file delete bytes."""
-    index = 0
-    expected_outputs = ["foo\nbar\nbaz\n", "foo\nbaz\n"]
-
-    async def container_filesystem_exec_get_output(servicer, stream):
-        nonlocal index
-        req = await stream.recv_message()
-        if req.exec_id == READ_EXEC_ID:
-            await stream.send_message(api_pb2.FilesystemRuntimeOutputBatch(output=[expected_outputs[index].encode()]))
-            index += 1
-        await stream.send_message(api_pb2.FilesystemRuntimeOutputBatch(eof=True))
-
-    with servicer.intercept() as ctx:
-        ctx.set_responder("ContainerFilesystemExec", container_filesystem_exec)
-        ctx.set_responder("ContainerFilesystemExecGetOutput", container_filesystem_exec_get_output)
-
-        f = FileIO.create("/test.txt", "a+", client, "task-123")
-        f.write("foo\nbar\nbaz\n")
-        assert f.read() == expected_outputs[0]
-        delete_bytes(f, start=4, end=7)
-        assert f.read() == expected_outputs[1]
         f.close()
 
 
