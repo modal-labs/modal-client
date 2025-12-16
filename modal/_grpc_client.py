39fe0ff7587d60e28521a0f9326fede2b95a84c1
Michael Waskom <michael@modal.com>
2025-12-16 16:57:34 +0000

    Fix nit in setting gRPC data after transforming type

--- modal/_grpc_client.py
+++ modal/_grpc_client.py
@@ -53,7 +53,7 @@ class grpc_error_converter:
         with suppress_tb_frames(1):
             if isinstance(exc, GRPCError):
                 modal_exc = _STATUS_TO_EXCEPTION[exc.status](exc.message)
-                modal_exc.message = exc.message
+                modal_exc._grpc_message = exc.message
                 modal_exc._grpc_status = exc.status
                 modal_exc._grpc_details = exc.details
                 if use_full_traceback:
