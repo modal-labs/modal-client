6e1955bb25851ad4e37658709c3aba5e5f6e1a24
Michael Waskom <michael@modal.com>
2025-12-17 17:48:09 +0000

    Add GRPCError conversion

--- modal/_utils/task_command_router_client.py
+++ modal/_utils/task_command_router_client.py
@@ -523,13 +523,15 @@ class TaskCommandRouterClient:
                     raise ConnectionError(str(e))
 
     async def mount_directory(self, request: sr_pb2.TaskMountDirectoryRequest):
-        return await call_with_retries_on_transient_errors(
-            lambda: self._call_with_auth_retry(self._stub.TaskMountDirectory, request)
-        )
+        with grpc_error_converter():
+            return await call_with_retries_on_transient_errors(
+                lambda: self._call_with_auth_retry(self._stub.TaskMountDirectory, request)
+            )
 
     async def snapshot_directory(
         self, request: sr_pb2.TaskSnapshotDirectoryRequest
     ) -> sr_pb2.TaskSnapshotDirectoryResponse:
-        return await call_with_retries_on_transient_errors(
-            lambda: self._call_with_auth_retry(self._stub.TaskSnapshotDirectory, request)
-        )
+        with grpc_error_converter():
+            return await call_with_retries_on_transient_errors(
+                lambda: self._call_with_auth_retry(self._stub.TaskSnapshotDirectory, request)
+            )
