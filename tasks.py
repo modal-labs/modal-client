d2cb3f40eaa4f561f4a22b1a6bc89ba9c78eed07
claudia <34072934+molocule@users.noreply.github.com>
2026-01-26 11:20:14 -0500

    add server to pyright allowlist (#32794)
    GitOrigin-RevId: f057629a337e4093903dc0c42e01da48af4b3237

--- tasks.py
+++ tasks.py
@@ -213,6 +213,7 @@ def type_check(ctx):
         "modal/_functions.py",
         "modal/_runtime/asgi.py",
         "modal/_runtime/user_code_imports.py",
+        "modal/_server.py",
         "modal/_utils/__init__.py",
         "modal/_utils/async_utils.py",
         "modal/_utils/grpc_testing.py",
