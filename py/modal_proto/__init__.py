a43ea1d397edc05ec6b22b947b000211efbc5ba3
Michael Waskom <mwaskom@users.noreply.github.com>
2026-07-31 16:12:43 -0400

    Discourage direct usage of the gRPC API (#50627)
    GitOrigin-RevId: 6d972985a7f4404beaf0c556ff381bf29aebab2e

--- py/modal_proto/__init__.py
+++ py/modal_proto/__init__.py
@@ -1,4 +1,11 @@
 # Copyright Modal Labs 2024
+"""Modal gRPC types for Python.
+
+Warning: direct usage of Modal's gRPC API is discouraged,
+and no support or compatibility guarantees are provided.
+Message fields or entire RPCs may be changed or removed
+without notice. We recommend using official SDKs instead.
+"""
 from pkgutil import extend_path
 
 __path__ = extend_path(__path__, __name__)  # type: ignore[has-type]
