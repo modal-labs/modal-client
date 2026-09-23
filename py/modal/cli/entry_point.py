abf1343ac2797bafc8a91226da6c90322b4682a8
Connor Adams <connor@modal.com>
2026-09-23 15:27:46 +0000

    Remove the hidden cluster CLI commands (#61109)
    GitOrigin-RevId: 39bcb45bebbf1f8e8503a7e932a5a799d092619c

--- py/modal/cli/entry_point.py
+++ py/modal/cli/entry_point.py
@@ -12,7 +12,6 @@ from ._help import ModalCommand, ModalGroup, ModalProfileOption
 from .app import app_cli
 from .billing import billing_cli
 from .changelog import changelog
-from .cluster import cluster_cli
 from .config import config_cli
 from .container import container_cli
 from .curl import curl
@@ -116,8 +115,6 @@ entrypoint_cli.add_command(container_cli, panel="Deployments")
 entrypoint_cli.add_command(endpoint_cli, panel="Deployments")
 entrypoint_cli.add_command(function_cli, panel="Deployments")
 entrypoint_cli.add_command(server_cli, panel="Deployments")
-# cluster is hidden while multi-node is in beta/experimental
-entrypoint_cli.add_command(cluster_cli, panel="Deployments", hidden=True)
 
 entrypoint_cli.add_command(image_cli, panel="Storage")
 entrypoint_cli.add_command(dict_cli, panel="Storage")
